95b650c208e1d8112c9034fcdfaadd0690cd2d0c
hackerdmitry <hacker.dmitry@gmail.com>
2023-10-01 11:46:26 +0500

    Sample

---
+++ LeetCode/Sample/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Sample
+{
+    public class Solution
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ LeetCode/Sample/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.Sample
+{
+    [TestFixture(TestName = "Sample")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(1, 2, 3)]
+        public void Test(int input1, int input2, int output)
+        {
+            var solution = new Solution();
+            var actual = solution.Sum(input1, input2);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
