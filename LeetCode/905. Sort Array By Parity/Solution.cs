6642ad0e3a6601b4010b93f4c7890fef28a781d9
hackerdmitry <hacker.dmitry@gmail.com>
2023-10-01 11:47:24 +0500

    905. Sort Array By Parity

---
+++ LeetCode/905. Sort Array By Parity/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace LeetCode._905._Sort_Array_By_Parity
+{
+    public class Solution
+    {
+        public int[] SortArrayByParity(int[] nums)
+        {
+            var orderedNums = new int[nums.Length];
+
+            var evenNumsCount = nums.Count(x => x % 2 == 0);
+
+            var oddIndex = evenNumsCount;
+            var evenIndex = 0;
+
+            foreach (var num in nums)
+            {
+                if (num % 2 == 0)
+                    orderedNums[evenIndex++] = num;
+                else
+                    orderedNums[oddIndex++] = num;
+            }
+
+            return orderedNums;
+        }
+    }
+}
---
+++ LeetCode/905. Sort Array By Parity/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._905._Sort_Array_By_Parity
+{
+    [TestFixture(TestName = "905. Sort Array By Parity")]
+    public class Tests
+    {
+        [TestCase(new[] {3, 1, 2, 4}, new[] {2, 4, 3, 1})]
+        [TestCase(new[] {0}, new[] {0})]
+        public void Test(int[] input, int[] output)
+        {
+            var solution = new Solution();
+            var actual = solution.SortArrayByParity(input);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
