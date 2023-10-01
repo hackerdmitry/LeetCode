5124111c1669c7698454db652b11626870ec4136
hackerdmitry <hacker.dmitry@gmail.com>
2023-10-01 11:47:02 +0500

    456. 132 Pattern

---
+++ LeetCode/456. 132 Pattern/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._456._132_Pattern
+{
+    public class Solution
+    {
+        public bool Find132pattern(int[] nums)
+        {
+            var min = nums[0];
+            var periods = new LinkedList<(int, int?)>();
+            periods.AddFirst((min, null));
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                var num = nums[i];
+
+                if (num < min)
+                {
+                    min = num;
+                    if (periods.Last!.Value.Item2.HasValue)
+                        periods.AddLast((min, null));
+                    else
+                        periods.Last.Value = (min, null);
+                }
+                else
+                {
+                    var period = periods.First;
+                    while (period != null)
+                    {
+                        if (!period.Value.Item2.HasValue && num > period.Value.Item1 ||
+                            num > period.Value.Item2)
+                        {
+                            period.Value = (period.Value.Item1, num);
+                            if (period.Previous != null &&
+                                period.Value.Item1 <= period.Previous.Value.Item1 &&
+                                period.Value.Item2 >= period.Previous.Value.Item2)
+                                periods.Remove(period.Previous);
+                        }
+                        if (period.Value.Item1 < num && num < period.Value.Item2)
+                            return true;
+
+                        period = period.Next;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode/456. 132 Pattern/Tests.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace LeetCode._456._132_Pattern
+{
+    [TestFixture(TestName = "456. 132 Pattern")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(new[] {1, 2, 3, 4}, false)]
+        [TestCase(new[] {3, 1, 4, 2}, true)]
+        [TestCase(new[] {-1, 3, 2, 0}, true)]
+        [TestCase(new[] {1, 1, 2, 1, 1, 1, 1, 1}, false)]
+        [TestCase(new[] {1, 1, 7, 1, 1, 1, 2, 1}, true)]
+        [TestCase(new[] {3, 1, 4, 0, 2}, true)]
+        [TestCase(new[] {3, 1, 4, 0, 1}, false)]
+        [TestCase(new[] {3, 2, 4, 0, 2, 1}, true)]
+        [TestCase(new[] {1, 3, 2, 4, 3, 5, 4, 6, 5, 7, 6, 8, 9}, true)]
+        [TestCase(new[] {1, 3, 2, 4, 3, 5, 4, 6, 5, 7, 6, 8, 2}, true)]
+        [TestCase(new[] {14, 16, 12, 14, 10, 12, 8, 10, 6, 8, 4, 6, 16}, false)]
+        [TestCase(new[] {14, 16, 12, 14, 10, 12, 8, 10, 6, 8, 4, 6, 15}, true)]
+        [TestCase(new[] {14, 16, 12, 14, 10, 12, 8, 10, 6, 8, 4, 6, 11}, true)]
+        [TestCase(new[] {14, 16, 12, 14, 10, 12, 8, 10, 6, 8, 4, 6, 5}, true)]
+        [TestCaseSource(nameof(LongInput))]
+        public void Test(int[] input, bool output)
+        {
+            var solution = new Solution();
+            var actual = solution.Find132pattern(input);
+            Assert.AreEqual(output, actual);
+        }
+
+        private static IEnumerable LongInput()
+        {
+            var sr = new StreamReader("./456. 132 Pattern/LongInput.txt");
+            var input = sr.ReadToEnd().Split(",").Select(int.Parse).ToArray();
+            yield return new object[] {input, false};
+        }
+    }
+}
