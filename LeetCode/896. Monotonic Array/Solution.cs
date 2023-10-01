4f72350d4e3cfd555df5ae7bb33cee4893d9fda0
hackerdmitry <hacker.dmitry@gmail.com>
2023-10-01 11:47:20 +0500

    896. Monotonic Array

---
+++ LeetCode/896. Monotonic Array/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode._896._Monotonic_Array
+{
+    public class Solution
+    {
+        public bool IsMonotonic(int[] nums)
+        {
+            var isIncrease = (bool?) null;
+            for (var i = 1; i < nums.Length; i++)
+            {
+                var cur = nums[i];
+                var prev = nums[i - 1];
+                if (cur != prev)
+                {
+                    if (isIncrease.HasValue)
+                    {
+                        if (prev > cur ^ !isIncrease.Value)
+                            return false;
+                    }
+                    else isIncrease = prev < cur;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode/896. Monotonic Array/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._896._Monotonic_Array
+{
+    [TestFixture(TestName = "896. Monotonic Array")]
+    public class Tests
+    {
+        [TestCase(new[] {1, 2, 2, 3}, true)]
+        [TestCase(new[] {6, 5, 4, 4}, true)]
+        [TestCase(new[] {1, 3, 2}, false)]
+        public void Test(int[] input, bool output)
+        {
+            var solution = new Solution();
+            var actual = solution.IsMonotonic(input);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
