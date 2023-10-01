60e872cd381bbf0a366468c9e5ca2a158c68325d
hackerdmitry <hacker.dmitry@gmail.com>
2023-10-01 11:47:14 +0500

    880. Decoded String at Index

---
+++ LeetCode/880. Decoded String at Index/Solution.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._880._Decoded_String_at_Index
+{
+    public class Solution
+    {
+        public string DecodeAtIndex(string s, int k)
+        {
+            var words = new List<(string Word, int Digit, int Repeat, long Sum)>();
+
+            var lastDigitPos = -1;
+            for (var i = 0; i < s.Length; i++)
+            {
+                var c = s[i];
+                if (char.IsDigit(c))
+                {
+                    var digit = c - '0';
+                    var startIndex = lastDigitPos + 1;
+                    var length = i - lastDigitPos - 1;
+                    lastDigitPos = i;
+                    AddWord(s, startIndex, length, words, digit);
+                    if (words[^1].Sum >= int.MaxValue)
+                        break;
+                }
+            }
+
+            if (lastDigitPos != s.Length - 1 && words.Count == 0 || words[^1].Sum < int.MaxValue)
+            {
+                var startIndex = lastDigitPos + 1;
+                var length = s.Length - lastDigitPos - 1;
+                AddWord(s, startIndex, length, words, 1);
+            }
+
+            k--;
+            while (true)
+            {
+                for (int i = 0; i < words.Count; i++)
+                {
+                    var word = words[i];
+                    if (k < word.Sum)
+                    {
+                        k %= word.Repeat;
+
+                        if (i == 0 || k >= words[i - 1].Sum)
+                        {
+                            if (i > 0)
+                                k -= (int) words[i - 1].Sum;
+                            return word.Word[k % word.Word.Length].ToString();
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void AddWord(
+            string s, int startIndex, int length,
+            List<(string Word, int Digit, int Repeat, long Sum)> words, int digit)
+        {
+            var word = s.Substring(startIndex, length);
+
+            if (word.Length == 0)
+            {
+                words[^1] = (
+                    words[^1].Word,
+                    words[^1].Digit,
+                    words[^1].Repeat,
+                    words[^1].Sum * digit);
+                return;
+            }
+
+            var prevSum = words.Count == 0 ? 0 : words[^1].Sum;
+            var repeat = prevSum + word.Length;
+            var sum = repeat * digit;
+            words.Add((word, digit, (int) repeat, sum));
+        }
+    }
+}
---
+++ LeetCode/880. Decoded String at Index/Tests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._880._Decoded_String_at_Index
+{
+    [TestFixture(TestName = "880. Decoded String at Index")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase("leet2code3", 10, "o")]
+        [TestCase("ha22", 5, "h")]
+        [TestCase("a2345678999999999999999", 1, "a")]
+        [TestCase("leet2code3", 29, "l")]
+        [TestCase("leet2code3", 24, "e")]
+        [TestCase("leet2code3", 23, "d")]
+        [TestCase("a1b2c3", 15, "c")]
+        [TestCase("a1b2c3", 12, "b")]
+        [TestCase("abc", 1, "a")]
+        [TestCase("abc", 3, "c")]
+        [TestCase("a2c", 3, "c")]
+        [TestCase("a4vsb832i3zxouzx6vu6", 31179, "b")]
+        [TestCase("a4vsb832i3zxouzx6vu6", 1, "a")]
+        [TestCase("a4vsb832i3zxouzx6vu6", 5, "v")]
+        [TestCase("a4vsb832i3zxouzx6vu6", 337, "i")]
+        [TestCase("a4vsb832i3zxouzx6vu6", 1012, "z")]
+        [TestCase("a4vsb832i3zxouzx6vu6", 6103, "v")]
+        [TestCase("a4vsb832i3zxouzx6vu6", 36624, "u")]
+        public void Test(string input1, int input2, string output)
+        {
+            var solution = new Solution();
+            var actual = solution.DecodeAtIndex(input1, input2);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
