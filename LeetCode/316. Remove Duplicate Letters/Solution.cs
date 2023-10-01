43059d52b316f8abc96605b3d0899dbc992c93f5
hackerdmitry <hacker.dmitry@gmail.com>
2023-10-01 11:46:47 +0500

    316. Remove Duplicate Letters

---
+++ LeetCode/316. Remove Duplicate Letters/Solution.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode._316._Remove_Duplicate_Letters
+{
+    public class Solution
+    {
+        public string RemoveDuplicateLetters(string s)
+        {
+            var input = s;
+
+            var letterPositions = input
+                .Select((x, i) => (x, i))
+                .GroupBy(x => x.x, x => x.i)
+                .OrderBy(x => x.Key)
+                .ToDictionary(
+                    x => x.Key,
+                    x => x.Select(y => y).ToArray()
+                );
+            var keys = letterPositions.Keys.ToArray();
+
+            return GetMinimalLex(
+                input.ToArray(),
+                keys,
+                letterPositions
+            );
+        }
+
+        private string GetMinimalLex(char[] word, char[] letters, Dictionary<char, int[]> letterPositions)
+        {
+            var letterLastPositions = new Dictionary<char, int>(letters.Length);
+
+            var position = word.Length - 1;
+            foreach (var letter in word.Reverse())
+            {
+                letterLastPositions.TryAdd(letter, position--);
+            }
+
+            var linkWord = new LinkedList<char>();
+            var leftLetters = letters.ToHashSet();
+
+            var lastPos = -1;
+            while (leftLetters.Count > 0)
+            {
+                foreach (var letter in letters)
+                {
+                    if (leftLetters.Contains(letter))
+                    {
+                        var firstLetterPosition = letterPositions[letter].First(x => x > lastPos);
+                        if (leftLetters.All(x => letterLastPositions[x] >= firstLetterPosition))
+                        {
+                            linkWord.AddLast(letter);
+                            leftLetters.Remove(letter);
+                            lastPos = firstLetterPosition;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            var sb = new StringBuilder();
+            foreach (var c in linkWord)
+                sb.Append(c);
+            return sb.ToString();
+        }
+    }
+}
---
+++ LeetCode/316. Remove Duplicate Letters/Tests.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+
+namespace LeetCode._316._Remove_Duplicate_Letters
+{
+    [TestFixture(TestName = "316. Remove Duplicate Letters")]
+    public class Tests
+    {
+        [TestCase("a", "a")]
+        [TestCase("ba", "ba")]
+        [TestCase("bcabc", "abc")]
+        [TestCase("cbacdcbc", "acdb")]
+        [TestCase("ecbacba", "eacb")]
+        [TestCase("abcbcd", "abcd")]
+        [TestCase("acbcbd", "abcd")]
+        [TestCase("acdbcd", "abcd")]
+        [TestCase("cdbcd", "bcd")]
+        // ------------b-feg---k-uyj---orn-d----i-qszp--c---a--w-
+        // --------0---------1---------2---------3---------4----- <- 45
+        [TestCase("rusrbofeggbbkyuyjsrzornpdguwzizqszpbicdquakqws", "bfegkuyjorndiqszpcaw")]
+        [TestCase("acdebcde", "abcde")]
+        [TestCase("acdebcde", "abcde")]
+        [TestCase("acdebced", "abced")]
+        public void Test(string input, string output)
+        {
+            var solution = new Solution();
+            var actual = solution.RemoveDuplicateLetters(input);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
