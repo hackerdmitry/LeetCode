4a7e157b1aaecbafff27097f1102c2b1619816e9
hackerdmitry <hacker.dmitry@gmail.com>
2023-10-01 11:47:08 +0500

    557. Reverse Words in a String III

---
+++ LeetCode/557. Reverse Words in a String III/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace LeetCode._557._Reverse_Words_in_a_String_III
+{
+    public class Solution
+    {
+        public string ReverseWords(string s)
+        {
+            return string.Join(' ', s.Split(' ').Select(word =>
+            {
+                var sb = new StringBuilder();
+                foreach (var letter in word.Reverse())
+                    sb.Append(letter);
+                return sb;
+            }));
+        }
+    }
+}
---
+++ LeetCode/557. Reverse Words in a String III/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._557._Reverse_Words_in_a_String_III
+{
+    [TestFixture(TestName = "557. Reverse Words in a String III")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase("Let's take LeetCode contest", "s'teL ekat edoCteeL tsetnoc")]
+        [TestCase("God Ding", "doG gniD")]
+        public void Test(string input, string output)
+        {
+            var solution = new Solution();
+            var actual = solution.ReverseWords(input);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
