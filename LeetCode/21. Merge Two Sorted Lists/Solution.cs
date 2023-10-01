899ac96553c41d6aced6a6a1457007bd74ec1dc7
hackerdmitry <hacker.dmitry@gmail.com>
2023-10-01 12:25:26 +0500

    21. Merge Two Sorted Lists

---
+++ LeetCode/21. Merge Two Sorted Lists/Solution.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode._21._Merge_Two_Sorted_Lists
+{
+    public class Solution
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            var elem1 = list1;
+            var elem2 = list2;
+
+            var reversedResult = (ListNode)null;
+            while (elem1 != null || elem2 != null)
+            {
+                if (elem1 == null)
+                {
+                    reversedResult = new ListNode(elem2.val, reversedResult);
+                    elem2 = elem2.next;
+                }
+                else if (elem2 == null)
+                {
+                    reversedResult = new ListNode(elem1.val, reversedResult);
+                    elem1 = elem1.next;
+                }
+                else
+                {
+                    if (elem1.val < elem2.val)
+                    {
+                        reversedResult = new ListNode(elem1.val, reversedResult);
+                        elem1 = elem1.next;
+                    }
+                    else
+                    {
+                        reversedResult = new ListNode(elem2.val, reversedResult);
+                        elem2 = elem2.next;
+                    }
+                }
+            }
+
+            var result = (ListNode) null;
+            while (reversedResult != null)
+            {
+                result = new ListNode(reversedResult.val, result);
+                reversedResult = reversedResult.next;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/21. Merge Two Sorted Lists/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace LeetCode._21._Merge_Two_Sorted_Lists
+{
+    [TestFixture(TestName = "21. Merge Two Sorted Lists")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(new[] {1, 2, 4}, new[] {1, 3, 4}, new[] {1, 1, 2, 3, 4, 4})]
+        [TestCase(new int[0], new int[0], new int[0])]
+        [TestCase(new int[0], new[] {0}, new[] {0})]
+        public void Test(int[] input1, int[] input2, int[] output)
+        {
+            var solution = new Solution();
+
+            var list1 = input1.Reverse().Aggregate((ListNode) null, (nextNode, curValue) => new ListNode(curValue, nextNode));
+            var list2 = input2.Reverse().Aggregate((ListNode) null, (nextNode, curValue) => new ListNode(curValue, nextNode));
+
+            var result = solution.MergeTwoLists(list1, list2);
+
+            var actual = new List<int>();
+            while (result != null)
+            {
+                actual.Add(result.val);
+                result = result.next;
+            }
+
+            Console.WriteLine(string.Join(", ", output));
+            Console.WriteLine(string.Join(", ", actual));
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
