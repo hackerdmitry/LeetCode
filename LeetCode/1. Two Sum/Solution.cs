5da65699bfab5ec95ccce7c0d97adc38b90de458
hackerdmitry <hacker.dmitry@gmail.com>
2023-10-01 11:46:38 +0500

    1. Two Sum

---
+++ LeetCode/1. Two Sum/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode._1._Two_Sum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var numPositions = nums
+                .Select((x, i) => (x, i))
+                .GroupBy(x => x.x, x => x.i)
+                .ToDictionary(x => x.Key, x => x.ToArray());
+            foreach (var numPosition in numPositions)
+            {
+                if (target % 2 == 0 &&
+                    target / 2 == numPosition.Key)
+                {
+                    if (numPosition.Value.Length == 2)
+                        return numPosition.Value;
+                }
+                else if (numPositions.ContainsKey(target - numPosition.Key))
+                    return new[] {numPosition.Value.First(), numPositions[target - numPosition.Key].First()};
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LeetCode/1. Two Sum/Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCode._1._Two_Sum
+{
+    [TestFixture(TestName = "1. Two Sum")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(new[] {2, 7, 11, 15}, 9, new[] {0, 1})]
+        [TestCase(new[] {3, 2, 4}, 6, new[] {1, 2})]
+        [TestCase(new[] {3, 3}, 6, new[] {0, 1})]
+        public void Test(int[] input1, int input2, int[] output)
+        {
+            var solution = new Solution();
+            var actual = solution.TwoSum(input1, input2);
+            Console.WriteLine(string.Join(", ", actual));
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
