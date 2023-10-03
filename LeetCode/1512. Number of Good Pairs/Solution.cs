0f93d27abafe5d96ec8f5eaadb6da5e8c3854610
hackerdmitry <d.butov@skbkontur.ru>
2023-10-03 10:36:32 +0500

    1512. Number of Good Pairs

---
+++ LeetCode/1512. Number of Good Pairs/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._1512._Number_of_Good_Pairs
+{
+    public class Solution
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            var dict = new Dictionary<int, int>();
+            foreach (var num in nums)
+            {
+                if (dict.ContainsKey(num))
+                    dict[num]++;
+                else
+                    dict[num] = 1;
+            }
+
+            var sum = 0;
+            foreach (var (_, value) in dict)
+            {
+                if (value > 1)
+                    sum += value * (value - 1) / 2;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ LeetCode/1512. Number of Good Pairs/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1512._Number_of_Good_Pairs
+{
+    [TestFixture(TestName = "1512. Number of Good Pairs")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(new[] { 1, 2, 3, 1, 1, 3 }, 4)]
+        [TestCase(new[] { 1, 1, 1, 1 }, 6)]
+        [TestCase(new[] { 1, 2, 3 }, 0)]
+        public void Test(int[] input, int output)
+        {
+            var solution = new Solution();
+            var actual = solution.NumIdenticalPairs(input);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
