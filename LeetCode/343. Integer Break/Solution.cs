0d3b252964104a7fbf9793613185f215796fbc86
hackerdmitry <d.butov@skbkontur.ru>
2023-10-06 10:29:51 +0500

    343. Integer Break

---
+++ LeetCode/343. Integer Break/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode._343._Integer_Break
+{
+    public class Solution
+    {
+        public int IntegerBreak(int n)
+        {
+            var maxProduct = 0;
+            for (var k = 2; k <= n; k++)
+            {
+                var mod = n % k;
+                var div = n / k;
+
+                var product = div;
+                for (var i = 0; i < k - mod - 1; i++)
+                    product *= div;
+                for (var i = 0; i < mod; i++)
+                    product *= div + 1;
+                if (maxProduct < product)
+                    maxProduct = product;
+            }
+
+            return maxProduct;
+        }
+    }
+}
---
+++ LeetCode/343. Integer Break/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._343._Integer_Break
+{
+    [TestFixture(TestName = "343. Integer Break")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(4, 4)]
+        [TestCase(10, 36)]
+        public void Test(int input, int output)
+        {
+            var solution = new Solution();
+            var actual = solution.IntegerBreak(input);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
