d942d003ed2b9c512bd20bb61cc1280b690c1e92
hackerdmitry <d.butov@skbkontur.ru>
2023-10-06 10:20:17 +0500

    229. Majority Element II

---
+++ LeetCode/229. Majority Element II/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._229._Majority_Element_II
+{
+    public class Solution
+    {
+        public IList<int> MajorityElement(int[] nums)
+        {
+            var result = new List<int>();
+            var hashSet = new HashSet<int>();
+            var dict = new Dictionary<int, int>();
+            var limit = nums.Length / 3;
+            foreach (var num in nums)
+            {
+                if (dict.ContainsKey(num))
+                    dict[num]++;
+                else
+                    dict[num] = 1;
+
+                if (dict[num] > limit && !hashSet.Contains(num))
+                {
+                    hashSet.Add(num);
+                    result.Add(num);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/229. Majority Element II/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._229._Majority_Element_II
+{
+    [TestFixture(TestName = "229. Majority Element II")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(new[] { 3, 2, 3 }, new[] { 3 })]
+        [TestCase(new[] { 1 }, new[] { 1 })]
+        [TestCase(new[] { 1, 2 }, new[] { 1, 2 })]
+        public void Test(int[] input, int[] output)
+        {
+            var solution = new Solution();
+            var actual = solution.MajorityElement(input);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
