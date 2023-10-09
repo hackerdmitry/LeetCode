29b867c73eef35f79230acba900f4fa78773803a
hackerdmitry <d.butov@skbkontur.ru>
2023-10-09 09:52:47 +0500

    34. Find First and Last Position of Element in Sorted Array

---
+++ LeetCode/34. Find First and Last Position of Element in Sorted Array/Solution.cs
@@ -0,0 +1,64 @@
+﻿namespace LeetCode._34._Find_First_and_Last_Position_of_Element_in_Sorted_Array
+{
+    public class Solution
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            var result = new[] { -1, -1 };
+            if (nums.Length == 0)
+                return result;
+
+            var left = 0;
+            var right = nums.Length - 1;
+
+            var middle = 0;
+            while (left != right)
+            {
+                middle = (left + right) / 2;
+                var current = nums[middle];
+                if (current < target)
+                    left = middle;
+                else
+                    right = middle;
+
+                if (right - left == 1)
+                {
+                    if (nums[left] == target)
+                        middle = left;
+                    else
+                        middle = right;
+                    break;
+                }
+            }
+
+            if (nums[middle] == target)
+                result[0] = middle;
+            else
+                return result;
+
+            left = 0;
+            right = nums.Length - 1;
+            while (left != right)
+            {
+                middle = (left + right) / 2;
+                var current = nums[middle];
+                if (current <= target)
+                    left = middle;
+                else
+                    right = middle;
+
+                if (right - left == 1)
+                {
+                    if (nums[right] == target)
+                        middle = right;
+                    else
+                        middle = left;
+                    break;
+                }
+            }
+
+            result[1] = middle;
+            return result;
+        }
+    }
+}
---
+++ LeetCode/34. Find First and Last Position of Element in Sorted Array/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCode._34._Find_First_and_Last_Position_of_Element_in_Sorted_Array
+{
+    [TestFixture(TestName = "34. Find First and Last Position of Element in Sorted Array")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(new[] { 5, 7, 7, 8, 8, 10 }, 8, new[] { 3, 4 })]
+        [TestCase(new[] { 5, 7, 7, 8, 8, 10 }, 6, new[] { -1, -1 })]
+        [TestCase(new int[0], 0, new[] { -1, -1 })]
+        public void Test(int[] input1, int input2, int[] output)
+        {
+            var solution = new Solution();
+            var actual = solution.SearchRange(input1, input2);
+            Console.WriteLine("expected: " + string.Join(", ", output));
+            Console.WriteLine("actual: " + string.Join(", ", actual));
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
