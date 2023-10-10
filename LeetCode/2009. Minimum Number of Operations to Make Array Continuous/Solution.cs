208d8b4ffe9b738d502b5ded2438a1ebfef7132d
hackerdmitry <d.butov@skbkontur.ru>
2023-10-10 11:09:34 +0500

    2009. Minimum Number of Operations to Make Array Continuous

---
+++ LeetCode/2009. Minimum Number of Operations to Make Array Continuous/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode._2009._Minimum_Number_of_Operations_to_Make_Array_Continuous
+{
+    public class Solution
+    {
+        public int MinOperations(int[] nums)
+        {
+            Array.Sort(nums);
+
+            for (var i = 1; i < nums.Length; i++)
+                if (nums[i] == nums[i - 1])
+                    nums[i - 1] = int.MaxValue;
+
+            Array.Sort(nums);
+
+            var minPermutations = nums.Length - 1;
+            var lastNum = nums[^1];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var num = nums[i];
+
+                if (lastNum - num < minPermutations)
+                    break;
+
+                var minIndex = SearchMinIndex(nums, num + nums.Length - 1);
+                var curPermutations = nums.Length - 1 - minIndex + i;
+                if (curPermutations < minPermutations)
+                    minPermutations = curPermutations;
+            }
+
+            return minPermutations;
+        }
+
+        private int SearchMinIndex(int[] nums, int minNum)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+
+            while (left < right)
+            {
+                if (right - 1 == left)
+                    return nums[right] <= minNum ? right : left;
+
+                var middle = (left + right) / 2;
+                var curNum = nums[middle];
+                if (curNum <= minNum)
+                    left = middle;
+                else
+                    right = middle;
+            }
+
+            return right;
+        }
+    }
+}
---
+++ LeetCode/2009. Minimum Number of Operations to Make Array Continuous/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2009._Minimum_Number_of_Operations_to_Make_Array_Continuous
+{
+    [TestFixture(TestName = "2009. Minimum Number of Operations to Make Array Continuous")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(new[]{4,2,5,3}, 0)]
+        [TestCase(new[]{1,2,3,5,6}, 1)]
+        [TestCase(new[]{1,10,100,1000}, 3)]
+        [TestCase(new[]{8,5,9,9,8,4}, 2)]
+        [TestCase(new[]{1,1,1,1}, 3)]
+        public void Test(int[] input, int output)
+        {
+            var solution = new Solution();
+            var actual = solution.MinOperations(input);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
