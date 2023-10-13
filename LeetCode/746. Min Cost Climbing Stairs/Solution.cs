e88c87436327c15c4158e095bb4388cfbea1fe72
hackerdmitry <d.butov@skbkontur.ru>
2023-10-13 08:54:45 +0500

    746. Min Cost Climbing Stairs

---
+++ LeetCode/746. Min Cost Climbing Stairs/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LeetCode._746._Min_Cost_Climbing_Stairs
+{
+    public class Solution
+    {
+        public int MinCostClimbingStairs(int[] cost)
+        {
+            var minCosts = new int[cost.Length];
+            minCosts[0] = cost[0];
+            minCosts[1] = cost[1];
+
+            for (var i = 2; i < minCosts.Length; i++)
+                minCosts[i] = Math.Min(minCosts[i - 1], minCosts[i - 2]) + cost[i];
+
+            return Math.Min(minCosts[^1], minCosts[^2]);
+        }
+    }
+}
---
+++ LeetCode/746. Min Cost Climbing Stairs/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._746._Min_Cost_Climbing_Stairs
+{
+    [TestFixture(TestName = "746. Min Cost Climbing Stairs")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(new[] { 10, 15, 20 }, 15)]
+        [TestCase(new[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 }, 6)]
+        [TestCase(new[] { 999, 10, 15, 20 }, 25)]
+        public void Test(int[] input, int output)
+        {
+            var solution = new Solution();
+            var actual = solution.MinCostClimbingStairs(input);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
