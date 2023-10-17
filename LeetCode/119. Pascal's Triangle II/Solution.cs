55b5979407943766a64500b0cd0574897fdd1408
hackerdmitry <d.butov@skbkontur.ru>
2023-10-17 08:38:58 +0500

    119. Pascal's Triangle II

---
+++ LeetCode/119. Pascal's Triangle II/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._119._Pascal_s_Triangle_II
+{
+    public class Solution
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            var rows = new List<int[]> { new[] { 1 } };
+            for (var i = 1; i < rowIndex + 1; i++)
+            {
+                rows.Add(new int[i + 1]);
+                rows[i][0] = 1;
+                rows[i][i] = 1;
+                for (var j = 1; j < i; j++)
+                {
+                    rows[i][j] = rows[i - 1][j - 1] + rows[i - 1][j];
+                }
+            }
+
+            return rows[rowIndex];
+        }
+    }
+}
---
+++ LeetCode/119. Pascal's Triangle II/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._119._Pascal_s_Triangle_II
+{
+    [TestFixture(TestName = "119. Pascal's Triangle II")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(3, new[]{1,3,3,1})]
+        [TestCase(0, new[]{1})]
+        [TestCase(1, new[]{1,1})]
+        public void Test(int input, int[] output)
+        {
+            var solution = new Solution();
+            var actual = solution.GetRow(input);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
