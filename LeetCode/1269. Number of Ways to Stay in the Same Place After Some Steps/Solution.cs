b22839b33701051529fad9f1e27193823f339a92
hackerdmitry <hacker.dmitry@gmail.com>
2023-10-19 21:23:20 +0500

    1269. Number of Ways to Stay in the Same Place After Some Steps

---
+++ LeetCode/1269. Number of Ways to Stay in the Same Place After Some Steps/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace LeetCode._1269._Number_of_Ways_to_Stay_in_the_Same_Place_After_Some_Steps
+{
+    public class Solution
+    {
+        const int module = 1_000_000_007;
+
+        public int NumWays(int steps, int arrLen)
+        {
+            var ways = new List<int[]> {new int[steps + 1]};
+            ways[0][0] = 1;
+
+            for (int i = 0; i < steps; i++)
+            {
+                var maxJ = Math.Min(i + 2, arrLen);
+                if (ways.Count < maxJ)
+                    ways.Add(new int[steps + 1]);
+
+                for (int j = 0; j < maxJ; j++)
+                {
+                    var result = ways[j][i];
+                    if (j != 0)
+                        result = (result + ways[j - 1][i]) % module;
+                    if (j != maxJ - 1)
+                        result = (result + ways[j + 1][i]) % module;
+                    ways[j][i + 1] = result;
+                }
+            }
+
+            return ways[0][steps];
+        }
+    }
+}
---
+++ LeetCode/1269. Number of Ways to Stay in the Same Place After Some Steps/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1269._Number_of_Ways_to_Stay_in_the_Same_Place_After_Some_Steps
+{
+    [TestFixture(TestName = "1269. Number of Ways to Stay in the Same Place After Some Steps")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(3, 2, 4)]
+        [TestCase(2, 4, 2)]
+        [TestCase(4, 2, 8)]
+        [TestCase(4, 4, 9)]
+        public void Test(int input1, int input2, int output)
+        {
+            var solution = new Solution();
+            var actual = solution.NumWays(input1, input2);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
