ed3f56ddfa3f5711430cd7be660d8f0bddd98503
hackerdmitry <hacker.dmitry@gmail.com>
2023-10-19 21:23:00 +0500

    844. Backspace String Compare

---
+++ LeetCode/844. Backspace String Compare/Solution.cs
@@ -0,0 +1,61 @@
+﻿namespace LeetCode._844._Backspace_String_Compare
+{
+    public class Solution
+    {
+        public bool BackspaceCompare(string s, string t)
+        {
+            var i = s.Length - 1;
+            var j = t.Length - 1;
+
+            while (i >= 0 || j >= 0)
+            {
+                var sJump = 1;
+                if (i >= 0 && s[i] == '#')
+                {
+                    i--;
+
+                    while (i >= 0 && s[i] == '#' || sJump > 0)
+                    {
+                        if (i < 0)
+                            break;
+                        if (s[i] == '#')
+                            sJump++;
+                        else
+                            sJump--;
+                        i--;
+                    }
+                }
+
+                var tJump = 1;
+                if (j >= 0 && t[j] == '#')
+                {
+                    j--;
+
+                    while (j >= 0 && t[j] == '#' || tJump > 0)
+                    {
+                        if (j < 0)
+                            break;
+                        if (t[j] == '#')
+                            tJump++;
+                        else
+                            tJump--;
+                        j--;
+                    }
+                }
+
+                if (i < 0 && j < 0)
+                    break;
+
+                if (i >= 0 && j >= 0 && s[i] != t[j] ||
+                    i >= 0 && j < 0 && s[i] != '#' ||
+                    j >= 0 && i < 0 && t[j] != '#')
+                    return false;
+
+                i--;
+                j--;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode/844. Backspace String Compare/Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._844._Backspace_String_Compare
+{
+    [TestFixture(TestName = "844. Backspace String Compare")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase("ab#c", "ad#c", true)]
+        [TestCase("ad#c", "ab#c", true)]
+        [TestCase("ab##", "c#d#", true)]
+        [TestCase("a#c", "b", false)]
+        [TestCase("a##c", "#a#c", true)]
+        [TestCase("bxj##tw", "bxo#j##tw", true)]
+        [TestCase("bxj##tw", "bxj###tw", false)]
+        [TestCase("nzp#o#g", "b#nzp#o#g", true)]
+        [TestCase("nzp#o#g", "ииnzp#o#g", false)]
+        public void Test(string input1, string input2, bool output)
+        {
+            var solution = new Solution();
+            var actual = solution.BackspaceCompare(input1, input2);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
