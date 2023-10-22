c95a7bc0d3b36970d10b6ca6e9b685894f279cb4
hackerdmitry <hacker.dmitry@gmail.com>
2023-10-23 00:35:07 +0500

    1793. Maximum Score of a Good Subarray

---
+++ LeetCode/1793. Maximum Score of a Good Subarray/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace LeetCode._1793._Maximum_Score_of_a_Good_Subarray
+{
+    public class Solution
+    {
+        public int MaximumScore(int[] nums, int k)
+        {
+            var a = new int[k + 1];
+            var b = new int[nums.Length - k];
+
+            var minA = int.MaxValue;
+            for (int i = k; i >= 0; i--)
+            {
+                if (nums[i] < minA)
+                    minA = nums[i];
+
+                a[i] = minA;
+            }
+
+            var minB = int.MaxValue;
+            for (int i = k; i < nums.Length; i++)
+            {
+                if (nums[i] < minB)
+                    minB = nums[i];
+
+                b[i - k] = minB;
+            }
+
+            var aIndex = 0;
+            var bIndex = nums.Length - k - 1;
+            var maxScore = Math.Min(a[aIndex], b[bIndex]) * (bIndex + k + 1);
+
+            while (aIndex != k || bIndex != 0)
+            {
+                var min = Math.Min(a[aIndex], b[bIndex]);
+                var score = min * (bIndex + k - aIndex + 1);
+                if (score > maxScore)
+                    maxScore = score;
+
+                if (aIndex == k)
+                    bIndex--;
+                else if (bIndex == 0)
+                    aIndex++;
+                else
+                {
+                    if (a[aIndex] < b[bIndex])
+                        aIndex++;
+                    else
+                        bIndex--;
+                }
+            }
+
+            return maxScore;
+        }
+    }
+}
---
+++ LeetCode/1793. Maximum Score of a Good Subarray/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1793._Maximum_Score_of_a_Good_Subarray
+{
+    [TestFixture(TestName = "1793. Maximum Score of a Good Subarray")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(new[] {1, 4, 3, 7, 4, 5}, 3, 15)]
+        [TestCase(new[] {5, 5, 4, 5, 4, 1, 1, 1}, 0, 20)]
+        [TestCase(new[] {6569, 9667, 3148, 7698, 1622, 2194, 793, 9041, 1670, 1872}, 5, 9732)]
+        [TestCase(new[] {8182,1273,9847,6230,52,1467,6062,726,4852,4507,2460,2041,500,1025,5524}, 8, 9014)]
+        [TestCase(new[] {5}, 0, 5)]
+        public void Test(int[] input1, int input2, int output)
+        {
+            var solution = new Solution();
+            var actual = solution.MaximumScore(input1, input2);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
