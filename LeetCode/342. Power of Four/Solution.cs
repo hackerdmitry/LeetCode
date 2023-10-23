1bf6956dd948a406d8c6af62168e734ca0a87406
hackerdmitry <d.butov@skbkontur.ru>
2023-10-23 10:51:02 +0500

    342. Power of Four

---
+++ LeetCode/342. Power of Four/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode._342._Power_of_Four
+{
+    public class Solution
+    {
+        public bool IsPowerOfFour(int n)
+        {
+            while (n > 1)
+            {
+                if (n % 4 != 0)
+                    return false;
+
+                n /= 4;
+            }
+
+            return n == 1;
+        }
+    }
+}
---
+++ LeetCode/342. Power of Four/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._342._Power_of_Four
+{
+    [TestFixture(TestName = "342. Power of Four")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(16, true)]
+        [TestCase(5, false)]
+        [TestCase(1, true)]
+        [TestCase(-64, false)]
+        public void Test(int input, bool output)
+        {
+            var solution = new Solution();
+            var actual = solution.IsPowerOfFour(input);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
