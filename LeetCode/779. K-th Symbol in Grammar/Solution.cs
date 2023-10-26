5df338bd8bf2db44a4be3d9dbb3cb46b51d1445d
hackerdmitry <d.butov@skbkontur.ru>
2023-10-26 10:19:02 +0500

    779. K-th Symbol in Grammar

---
+++ LeetCode/779. K-th Symbol in Grammar/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode._779._K_th_Symbol_in_Grammar
+{
+    public class Solution
+    {
+        public int KthGrammar(int n, int k)
+        {
+            var inverted = false;
+
+            var len = 1;
+            for (var i = 1; i < n; i++)
+                len *= 2;
+
+            for (var i = 1; i < n; i++)
+            {
+                if (k > len / 2)
+                {
+                    inverted = !inverted;
+                    k -= len / 2;
+                }
+
+                len /= 2;
+            }
+
+            return k == 1 ^ inverted ? 0 : 1;
+        }
+    }
+}
---
+++ LeetCode/779. K-th Symbol in Grammar/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._779._K_th_Symbol_in_Grammar
+{
+    [TestFixture(TestName = "779. K-th Symbol in Grammar")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(1, 1, 0)]
+        [TestCase(2, 1, 0)]
+        [TestCase(2, 2, 1)]
+        public void Test(int input1, int input2, int output)
+        {
+            var solution = new Solution();
+            var actual = solution.KthGrammar(input1, input2);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
