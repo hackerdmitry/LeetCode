e78768552b60a62ff2ba98e5e5385e2a1e37d0f0
hackerdmitry <d.butov@skbkontur.ru>
2023-10-27 12:20:39 +0500

    5. Longest Palindromic Substring

---
+++ LeetCode/5. Longest Palindromic Substring/Solution.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode._5._Longest_Palindromic_Substring
+{
+    public class Solution
+    {
+        public string LongestPalindrome(string s)
+        {
+            var start = -1;
+            var length = -1;
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                for (var j = 0; i - j >= 0 && i + j < s.Length; j++)
+                {
+                    if (s[i - j] == s[i + j])
+                    {
+                        if (length < j * 2 + 1)
+                        {
+                            start = i - j;
+                            length = j * 2 + 1;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            for (var i = 0; i < s.Length - 1; i++)
+            {
+                for (var j = 0; i - j >= 0 && i + j + 1 < s.Length; j++)
+                {
+                    if (s[i - j] == s[i + j + 1])
+                    {
+                        if (length < (j + 1) * 2)
+                        {
+                            start = i - j;
+                            length = (j + 1) * 2;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return s.Substring(start, length);
+        }
+    }
+}
---
+++ LeetCode/5. Longest Palindromic Substring/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._5._Longest_Palindromic_Substring
+{
+    [TestFixture(TestName = "5. Longest Palindromic Substring")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase("babad", "bab")]
+        [TestCase("cbbd", "bb")]
+        public void Test(string input, string output)
+        {
+            var solution = new Solution();
+            var actual = solution.LongestPalindrome(input);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
