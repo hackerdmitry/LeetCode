fc0d6a7c1343be6228052a9434ac1c824331fe11
hackerdmitry <hacker.dmitry@gmail.com>
2023-10-29 14:25:28 +0500

    458. Poor Pigs

---
+++ LeetCode/458. Poor Pigs/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode._458._Poor_Pigs
+{
+    public class Solution
+    {
+        public int PoorPigs(int buckets, int minutesToDie, int minutesToTest)
+        {
+            var rounds = minutesToTest / minutesToDie + 1;
+            var bucketsByRound = 1;
+
+            for (var pigs = 0; ; pigs++)
+            {
+                if (bucketsByRound >= buckets)
+                    return pigs;
+
+                bucketsByRound *= rounds;
+            }
+
+        }
+    }
+}
---
+++ LeetCode/458. Poor Pigs/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._458._Poor_Pigs
+{
+    [TestFixture(TestName = "458. Poor Pigs")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(4, 15, 15, 2)]
+        [TestCase(4, 15, 30, 2)]
+        [TestCase(4, 15, 45, 1)]
+        [TestCase(1000, 15, 60, 5)]
+        [TestCase(27, 1, 2, 3)]
+        [TestCase(1, 1, 1, 0)]
+        public void Test(int input1, int input2, int input3, int output)
+        {
+            var solution = new Solution();
+            var actual = solution.PoorPigs(input1, input2, input3);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
