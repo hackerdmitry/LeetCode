7098544f0d2ec9a670b9cd1b2e851c8746e28540
hackerdmitry <hacker.dmitry@gmail.com>
2023-10-31 22:36:09 +0500

    1356. Sort Integers by The Number of 1 Bits

---
+++ LeetCode/1356. Sort Integers by The Number of 1 Bits/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._1356._Sort_Integers_by_The_Number_of_1_Bits
+{
+    public class Solution
+    {
+        public int[] SortByBits(int[] arr)
+        {
+            var comparer = new SortHelper();
+            return arr.OrderBy(x => x, comparer).ToArray();
+        }
+
+        private class SortHelper : IComparer<int>
+        {
+            public int Compare(int a, int b)
+            {
+                var aBits = CountBits(a);
+                var bBits = CountBits(b);
+
+                if (aBits < bBits)
+                    return -1;
+                if (aBits > bBits)
+                    return 1;
+
+                return a < b
+                    ? -1
+                    : a > b ? 1 : 0;
+            }
+
+            static int CountBits(int a)
+            {
+                var countBits = 0;
+                while (a != 0)
+                {
+                    if (a % 2 != 0)
+                        countBits++;
+                    a /= 2;
+                }
+
+                return countBits;
+            }
+        }
+    }
+}
---
+++ LeetCode/1356. Sort Integers by The Number of 1 Bits/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1356._Sort_Integers_by_The_Number_of_1_Bits
+{
+    [TestFixture(TestName = "1356. Sort Integers by The Number of 1 Bits")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(new[]{0,1,2,3,4,5,6,7,8}, new[]{0,1,2,4,8,3,5,6,7})]
+        [TestCase(new[]{1024,512,256,128,64,32,16,8,4,2,1}, new[]{1,2,4,8,16,32,64,128,256,512,1024})]
+        public void Test(int[] input, int[] output)
+        {
+            var solution = new Solution();
+            var actual = solution.SortByBits(input);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
