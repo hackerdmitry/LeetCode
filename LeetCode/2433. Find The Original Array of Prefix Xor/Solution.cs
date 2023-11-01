6ff5184e057eac9160176c737487e50419fa16e7
hackerdmitry <hacker.dmitry@gmail.com>
2023-11-02 00:53:14 +0500

    2433. Find The Original Array of Prefix Xor

---
+++ LeetCode/2433. Find The Original Array of Prefix Xor/Solution.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode._2433._Find_The_Original_Array_of_Prefix_Xor
+{
+    public class Solution
+    {
+        public int[] FindArray(int[] pref)
+        {
+            for (var i = pref.Length - 1; i >= 1; i--)
+                pref[i] ^= pref[i - 1];
+
+            return pref;
+        }
+    }
+}
---
+++ LeetCode/2433. Find The Original Array of Prefix Xor/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2433._Find_The_Original_Array_of_Prefix_Xor
+{
+    [TestFixture(TestName = "2433. Find The Original Array of Prefix Xor")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(new[]{5,2,0,3,1}, new[]{5,7,2,3,2})]
+        [TestCase(new[]{13}, new[]{13})]
+        public void Test(int[] input, int[] output)
+        {
+            var solution = new Solution();
+            var actual = solution.FindArray(input);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
