41a3ac87e2e363207205b5ac625a0942f39fdabe
hackerdmitry <d.butov@skbkontur.ru>
2023-11-03 20:37:48 +0500

    1441. Build an Array With Stack Operations

---
+++ LeetCode/1441. Build an Array With Stack Operations/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._1441._Build_an_Array_With_Stack_Operations;
+
+public class Solution
+{
+    public IList<string> BuildArray(int[] target, int n)
+    {
+        var result = new List<string>();
+
+        var targetIndex = 0;
+        for (var i = 1; i <= n && targetIndex < target.Length; i++)
+        {
+            result.Add("Push");
+            if (i == target[targetIndex])
+                targetIndex++;
+            else
+                result.Add("Pop");
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/1441. Build an Array With Stack Operations/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1441._Build_an_Array_With_Stack_Operations;
+
+[TestFixture(TestName = "1441. Build an Array With Stack Operations")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{1,3}, 3, new[]{"Push","Push","Pop","Push"})]
+    [TestCase(new[]{1,2,3}, 3, new[]{"Push","Push","Push"})]
+    [TestCase(new[]{1,2}, 4, new[]{"Push","Push"})]
+    public void Test(int[] input1, int input2, string[] output)
+    {
+        var solution = new Solution();
+        var actual = solution.BuildArray(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
