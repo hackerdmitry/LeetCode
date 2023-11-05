2e9915c5dcb48b3468221fe5d21e5eedda174452
hackerdmitry <hacker.dmitry@gmail.com>
2023-11-05 10:20:54 +0500

    1535. Find the Winner of an Array Game

---
+++ LeetCode/1535. Find the Winner of an Array Game/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode._1535._Find_the_Winner_of_an_Array_Game;
+
+public class Solution
+{
+    public int GetWinner(int[] arr, int k)
+    {
+        var maxIndex = 0;
+        var startK = 0;
+
+        for (var i = 1; i < arr.Length; i++)
+        {
+            if (arr[maxIndex] < arr[i])
+            {
+                maxIndex = i;
+                startK = i - 1;
+            }
+
+            if (i - startK == k)
+                break;
+        }
+
+        return arr[maxIndex];
+    }
+}
---
+++ LeetCode/1535. Find the Winner of an Array Game/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1535._Find_the_Winner_of_an_Array_Game;
+
+[TestFixture(TestName = "1535. Find the Winner of an Array Game")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{2,1,3,5,4,6,7}, 2, 5)]
+    [TestCase(new[]{3,2,1}, 10, 3)]
+    [TestCase(new[]{3,2,1,5,4,6,7}, 2, 3)]
+    [TestCase(new[]{3,2,1,7,4,6,5}, 3, 7)]
+    public void Test(int[] input1, int input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.GetWinner(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
