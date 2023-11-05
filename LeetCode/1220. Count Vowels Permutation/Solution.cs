391af76f39348729af78eb8008f362f9a859a0bf
hackerdmitry <hacker.dmitry@gmail.com>
2023-11-05 10:07:56 +0500

    1220. Count Vowels Permutation

---
+++ LeetCode/1220. Count Vowels Permutation/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace LeetCode._1220._Count_Vowels_Permutation
+{
+    public class Solution
+    {
+        private const int modulo = 1_000_000_007;
+
+        public int CountVowelPermutation(int n)
+        {
+            var vowels = new int[n, 5];
+            for (var i = 0; i < 5; i++)
+                vowels[0, i] = 1;
+
+            for (var i = 1; i < n; i++)
+            {
+                vowels[i, 0] = ((vowels[i - 1, 1] + vowels[i - 1, 2]) % modulo + vowels[i - 1, 4]) % modulo;
+                vowels[i, 1] = (vowels[i - 1, 0] + vowels[i - 1, 2]) % modulo;
+                vowels[i, 2] = (vowels[i - 1, 1] + vowels[i - 1, 3]) % modulo;
+                vowels[i, 3] = vowels[i - 1, 2];
+                vowels[i, 4] = (vowels[i - 1, 2] + vowels[i - 1, 3]) % modulo;
+            }
+
+            return (int)(Enumerable.Range(0, 5).Sum(i => (long)vowels[n - 1, i]) % modulo);
+        }
+    }
+}
---
+++ LeetCode/1220. Count Vowels Permutation/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1220._Count_Vowels_Permutation
+{
+    [TestFixture(TestName = "1220. Count Vowels Permutation")]
+    public class Tests
+    {
+        [Timeout(1000)]
+        [TestCase(1, 5)]
+        [TestCase(2, 10)]
+        [TestCase(5, 68)]
+        [TestCase(144, 18208803)]
+        public void Test(int input, int output)
+        {
+            var solution = new Solution();
+            var actual = solution.CountVowelPermutation(input);
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
