933ac0f8a4ba66eacae39d656310d6c4e8d9667b
hackerdmitry <hacker.dmitry@gmail.com>
2023-11-06 19:47:34 +0500

    2265. Count Nodes Equal to Average of Subtree

---
+++ LeetCode/2265. Count Nodes Equal to Average of Subtree/Solution.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.__TreeNode;
+
+namespace LeetCode._2265._Count_Nodes_Equal_to_Average_of_Subtree;
+
+public class Solution
+{
+    public int AverageOfSubtree(TreeNode root)
+    {
+        return CountAverages(root).CountAverages;
+    }
+
+    private (int Count, int Sum, int CountAverages) CountAverages(TreeNode treeNode)
+    {
+        var count = 1;
+        var sum = treeNode.val;
+        var countAverages = 0;
+
+        if (treeNode.left != null)
+        {
+            var (countLeft, sumLeft, countAveragesLeft) = CountAverages(treeNode.left);
+            count += countLeft;
+            sum += sumLeft;
+            countAverages += countAveragesLeft;
+        }
+
+        if (treeNode.right != null)
+        {
+            var (countRight, sumRight, countAveragesRight) = CountAverages(treeNode.right);
+            count += countRight;
+            sum += sumRight;
+            countAverages += countAveragesRight;
+        }
+
+        if (sum / count == treeNode.val)
+            countAverages++;
+
+        return (count, sum, countAverages);
+    }
+}
---
+++ LeetCode/2265. Count Nodes Equal to Average of Subtree/Tests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using LeetCode.__TreeNode;
+using NUnit.Framework;
+
+namespace LeetCode._2265._Count_Nodes_Equal_to_Average_of_Subtree;
+
+[TestFixture(TestName = "2265. Count Nodes Equal to Average of Subtree")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int?[] input, int output)
+    {
+        var solution = new Solution();
+        var treeRoot = TreeNode.CreateFromArray(input);
+        var actual = solution.AverageOfSubtree(treeRoot);
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        yield return new object[]
+        {
+            new int?[]{4,8,5,0,1,null,6},
+            5
+        };
+        yield return new object[]
+        {
+            new int?[]{1},
+            1
+        };
+    }
+}
