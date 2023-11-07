4d8a3d26b8bdd594e1e98082626a90cf23025578
hackerdmitry <hacker.dmitry@gmail.com>
2023-11-07 23:40:57 +0500

    1921. Eliminate Maximum Number of Monsters

---
+++ LeetCode/1921. Eliminate Maximum Number of Monsters/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode._1921._Eliminate_Maximum_Number_of_Monsters;
+
+public class Solution
+{
+    public int EliminateMaximum(int[] dist, int[] speed)
+    {
+        var countOfSteps = new int[dist.Length];
+        for (int i = 0; i < dist.Length; i++)
+        {
+            countOfSteps[i] = dist[i] / speed[i];
+            if (dist[i] % speed[i] != 0)
+                countOfSteps[i]++;
+        }
+
+        Array.Sort(countOfSteps);
+
+        for (int i = 0; i < dist.Length; i++)
+        {
+            if (countOfSteps[i] <= i)
+                return i;
+        }
+
+        return dist.Length;
+    }
+}
---
+++ LeetCode/1921. Eliminate Maximum Number of Monsters/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1921._Eliminate_Maximum_Number_of_Monsters;
+
+[TestFixture(TestName = "1921. Eliminate Maximum Number of Monsters")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{1,3,4},  new[]{1,1,1}, 3)]
+    [TestCase(new[]{1,1,2,3},  new[]{1,1,1,1}, 1)]
+    [TestCase(new[]{3,2,4},  new[]{5,3,2}, 1)]
+    public void Test(int[] input1, int[] input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.EliminateMaximum(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
