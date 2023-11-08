fa091a84323808c80cba212ee13408f82bdae08a
hackerdmitry <hacker.dmitry@gmail.com>
2023-11-08 22:24:18 +0500

    2849. Determine if a Cell Is Reachable at a Given Time

---
+++ LeetCode/2849. Determine if a Cell Is Reachable at a Given Time/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LeetCode._2849._Determine_if_a_Cell_Is_Reachable_at_a_Given_Time;
+
+public class Solution
+{
+    public bool IsReachableAtTime(int sx, int sy, int fx, int fy, int t)
+    {
+        var hor = Math.Abs(sx - fx);
+        var ver = Math.Abs(sy - fy);
+
+        var max = Math.Max(hor, ver);
+
+        return max == 0 ? t != 1 : t >= max;
+    }
+}
---
+++ LeetCode/2849. Determine if a Cell Is Reachable at a Given Time/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2849._Determine_if_a_Cell_Is_Reachable_at_a_Given_Time;
+
+[TestFixture(TestName = "2849. Determine if a Cell Is Reachable at a Given Time")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(2, 4, 7, 7, 6, true)]
+    [TestCase(3, 1, 7, 3, 3, false)]
+    [TestCase(1, 2, 1, 2, 1, false)]
+    [TestCase(1, 1, 1, 1, 3, true)]
+    public void Test(int input1, int input2, int input3, int input4, int input5, bool output)
+    {
+        var solution = new Solution();
+        var actual = solution.IsReachableAtTime(input1, input2, input3, input4, input5);
+        Assert.AreEqual(output, actual);
+    }
+}
