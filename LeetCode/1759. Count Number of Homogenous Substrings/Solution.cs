d354db501f4f0e94d4cbe024633c8e197367edef
hackerdmitry <hacker.dmitry@gmail.com>
2023-11-09 21:01:28 +0500

    1759. Count Number of Homogenous Substrings

---
+++ LeetCode/1759. Count Number of Homogenous Substrings/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode._1759._Count_Number_of_Homogenous_Substrings;
+
+public class Solution
+{
+    public int CountHomogenous(string s)
+    {
+        const int module = 1_000_000_007;
+
+        var sum = 1;
+        var lastChar = s[0];
+        var lastSame = 0;
+
+        for (var i = 1; i < s.Length; i++)
+        {
+            if (lastChar == s[i])
+            {
+                sum += i - lastSame + 1;
+                sum %= module;
+            }
+            else
+            {
+                sum++;
+                lastSame = i;
+                lastChar = s[i];
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ LeetCode/1759. Count Number of Homogenous Substrings/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1759._Count_Number_of_Homogenous_Substrings;
+
+[TestFixture(TestName = "1759. Count Number of Homogenous Substrings")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("abbcccaa", 13)]
+    [TestCase("xy", 2)]
+    [TestCase("zzzzz", 15)]
+    public void Test(string input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.CountHomogenous(input);
+        Assert.AreEqual(output, actual);
+    }
+}
