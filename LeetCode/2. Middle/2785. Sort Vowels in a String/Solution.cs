05b80dc8c8d5d926873c1cbb215efa6a129eeb7f
hackerdmitry <hacker.dmitry@gmail.com>
2023-11-13 21:49:01 +0500

    2785. Sort Vowels in a String

---
+++ LeetCode/2. Middle/2785. Sort Vowels in a String/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2._Middle._2785._Sort_Vowels_in_a_String;
+
+public class Solution
+{
+    public string SortVowels(string s)
+    {
+        var vowels = new HashSet<char> {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+
+        var t = new char[s.Length];
+        var orderedVowels = s.Where(x => vowels.Contains(x)).OrderBy(x => x).ToArray();
+        var orderedVowelsIndex = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            t[i] = vowels.Contains(s[i]) ? orderedVowels[orderedVowelsIndex++] : s[i];
+        }
+
+        return new string(t);
+    }
+}
---
+++ LeetCode/2. Middle/2785. Sort Vowels in a String/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._2785._Sort_Vowels_in_a_String;
+
+[TestFixture(TestName = "2785. Sort Vowels in a String")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("lEetcOde", "lEOtcede")]
+    [TestCase("lYmpH", "lYmpH")]
+    public void Test(string input, string output)
+    {
+        var solution = new Solution();
+        var actual = solution.SortVowels(input);
+        Assert.AreEqual(output, actual);
+    }
+}
