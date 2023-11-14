5fa37c94f767ff943e3028fc2fa0660f98b258cd
hackerdmitry <d.butov@skbkontur.ru>
2023-11-14 10:48:15 +0500

    1930. Unique Length-3 Palindromic Subsequences

---
+++ LeetCode/2. Middle/1930. Unique Length-3 Palindromic Subsequences/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace LeetCode._2._Middle._1930._Unique_Length_3_Palindromic_Subsequences;
+
+public class Solution
+{
+    public int CountPalindromicSubsequence(string s)
+    {
+        var positions = new Dictionary<int, List<int>>();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            var c = s[i];
+            if (positions.ContainsKey(c))
+                positions[c].Add(i);
+            else
+                positions[c] = new List<int> { i };
+        }
+
+        var checks = new bool[26, 26];
+        var result = 0;
+        foreach (var (letter, letterPositions) in positions)
+        {
+            if (letterPositions.Count < 2)
+                continue;
+
+            var startPos = letterPositions[0];
+            var endPos = letterPositions[^1];
+
+            for (var i = startPos + 1; i < endPos; i++)
+            {
+                if (!checks[letter - 'a', s[i] - 'a'])
+                {
+                    checks[letter - 'a', s[i] - 'a'] = true;
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/2. Middle/1930. Unique Length-3 Palindromic Subsequences/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1930._Unique_Length_3_Palindromic_Subsequences;
+
+[TestFixture(TestName = "1930. Unique Length-3 Palindromic Subsequences")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("aabca", 3)]
+    [TestCase("adc", 0)]
+    [TestCase("bbcbaba", 4)]
+    public void Test(string input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.CountPalindromicSubsequence(input);
+        Assert.AreEqual(output, actual);
+    }
+}
