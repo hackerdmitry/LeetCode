bbf41c3b3d3c76a3e5983b0ab1f926ae93f3ac73
hackerdmitry <hacker.dmitry@gmail.com>
2023-11-16 04:11:47 +0500

    1846. Maximum Element After Decreasing and Rearranging

---
+++ LeetCode/2. Middle/1846. Maximum Element After Decreasing and Rearranging/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace LeetCode._2._Middle._1846._Maximum_Element_After_Decreasing_and_Rearranging;
+
+public class Solution
+{
+    public int MaximumElementAfterDecrementingAndRearranging(int[] arr)
+    {
+        arr = arr.OrderBy(x => x).ToArray();
+        if (arr[0] > 1)
+            arr[0] = 1;
+
+        for (var i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] - arr[i - 1] > 1)
+            {
+                arr[i] = arr[i - 1] + 1;
+            }
+        }
+
+        return arr[^1];
+    }
+}
---
+++ LeetCode/2. Middle/1846. Maximum Element After Decreasing and Rearranging/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1846._Maximum_Element_After_Decreasing_and_Rearranging;
+
+[TestFixture(TestName = "1846. Maximum Element After Decreasing and Rearranging")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{2,2,1,2,1}, 2)]
+    [TestCase(new[]{100,1,1000}, 3)]
+    [TestCase(new[]{1,2,3,4,5}, 5)]
+    public void Test(int[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MaximumElementAfterDecrementingAndRearranging(input);
+        Assert.AreEqual(output, actual);
+    }
+}
