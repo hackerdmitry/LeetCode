5dfbab3bd5ce1f24c60df0f3223b8b58dc3870d6
hackerdmitry <d.butov@skbkontur.ru>
2023-11-16 09:56:32 +0500

    1980. Find Unique Binary String

---
+++ LeetCode/2. Middle/1980. Find Unique Binary String/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace LeetCode._2._Middle._1980._Find_Unique_Binary_String;
+
+public class Solution
+{
+    public string FindDifferentBinaryString(string[] nums)
+    {
+        var numsSet = nums.Select(ToDecimalInt).ToHashSet();
+        var n = nums[0].Length;
+        for (var i = 0; ; i++)
+        {
+            if (!numsSet.Contains(i))
+                return ToBinaryString(i, n);
+        }
+    }
+
+    private int ToDecimalInt(string num)
+    {
+        var result = 0;
+        var factor = 1;
+        for (var i = num.Length - 1; i >= 0; i--)
+        {
+            if (num[i] == '1')
+                result += factor;
+            factor *= 2;
+        }
+
+        return result;
+    }
+
+    private string ToBinaryString(int num, int n)
+    {
+        var result = new char[n];
+        for (var i = result.Length - 1; i >= 0; i--)
+        {
+            result[i] = num % 2 == 0 ? '0' : '1';
+            num /= 2;
+        }
+
+        return new string(result);
+    }
+}
---
+++ LeetCode/2. Middle/1980. Find Unique Binary String/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1980._Find_Unique_Binary_String;
+
+[TestFixture(TestName = "1980. Find Unique Binary String")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{"01","10"}, "00")]
+    [TestCase(new[]{"00","01"}, "10")]
+    [TestCase(new[]{"111","011","001"}, "000")]
+    public void Test(string[] input, string output)
+    {
+        var solution = new Solution();
+        var actual = solution.FindDifferentBinaryString(input);
+        Assert.AreEqual(output, actual);
+    }
+}
