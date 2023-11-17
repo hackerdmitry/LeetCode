b886519885a26fd6bc07538204585226df824ecd
hackerdmitry <d.butov@skbkontur.ru>
2023-11-17 10:23:22 +0500

    1877. Minimize Maximum Pair Sum in Array

---
+++ LeetCode/2. Middle/1877. Minimize Maximum Pair Sum in Array/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCode._2._Middle._1877._Minimize_Maximum_Pair_Sum_in_Array;
+
+public class Solution
+{
+    public int MinPairSum(int[] nums)
+    {
+        Array.Sort(nums);
+
+        var sum = -1;
+        for (var i = 0; i < nums.Length / 2; i++)
+        {
+            var localSum = nums[i] + nums[nums.Length - 1 - i];
+            if (localSum > sum)
+                sum = localSum;
+        }
+
+        return sum;
+    }
+}
---
+++ LeetCode/2. Middle/1877. Minimize Maximum Pair Sum in Array/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1877._Minimize_Maximum_Pair_Sum_in_Array;
+
+[TestFixture(TestName = "1877. Minimize Maximum Pair Sum in Array")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{3,5,2,3}, 7)]
+    [TestCase(new[]{3,5,4,2,4,6}, 8)]
+    [TestCase(new[]{3,3,2,3}, 6)]
+    public void Test(int[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MinPairSum(input);
+        Assert.AreEqual(output, actual);
+    }
+}
