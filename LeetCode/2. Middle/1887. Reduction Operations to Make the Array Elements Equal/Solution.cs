69f47f35e4ee482febe1c25d8b1a5f92cfe696d2
hackerdmitry <hacker.dmitry@gmail.com>
2023-11-19 14:00:01 +0500

    1887. Reduction Operations to Make the Array Elements Equal

---
+++ LeetCode/2. Middle/1887. Reduction Operations to Make the Array Elements Equal/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2._Middle._1887._Reduction_Operations_to_Make_the_Array_Elements_Equal;
+
+public class Solution
+{
+    public int ReductionOperations(int[] nums)
+    {
+        var dictNums = new Dictionary<int, int>();
+        foreach (var num in nums)
+        {
+            if (dictNums.ContainsKey(num))
+                dictNums[num]++;
+            else
+                dictNums[num] = 1;
+        }
+
+        var orderedKeys = dictNums.Keys.OrderByDescending(x => x).ToArray();
+
+        var result = 0;
+        var prevCount = 0;
+
+        for (var i = 0; i < orderedKeys.Length - 1; i++)
+        {
+            var maxNum = orderedKeys[i];
+            var count = dictNums[maxNum] + prevCount;
+            result += count;
+            prevCount = count;
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/2. Middle/1887. Reduction Operations to Make the Array Elements Equal/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1887._Reduction_Operations_to_Make_the_Array_Elements_Equal;
+
+[TestFixture(TestName = "1887. Reduction Operations to Make the Array Elements Equal")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{5,1,3}, 3)]
+    [TestCase(new[]{1,1,1}, 0)]
+    [TestCase(new[]{1,1,2,2,3}, 4)]
+    public void Test(int[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.ReductionOperations(input);
+        Assert.AreEqual(output, actual);
+    }
+}
