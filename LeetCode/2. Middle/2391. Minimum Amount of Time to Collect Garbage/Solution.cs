9187faea596c30ff24affdd9799dfe8b304f97ec
hackerdmitry <d.butov@skbkontur.ru>
2023-11-20 09:20:17 +0500

    2391. Minimum Amount of Time to Collect Garbage

---
+++ LeetCode/2. Middle/2391. Minimum Amount of Time to Collect Garbage/Solution.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCode._2._Middle._2391._Minimum_Amount_of_Time_to_Collect_Garbage;
+
+public class Solution
+{
+    public int GarbageCollection(string[] garbage, int[] travel)
+    {
+        var p = 0;
+        var g = 0;
+        var m = 0;
+
+        var pSum = 0;
+        var gSum = 0;
+        var mSum = 0;
+
+        var sum = 0;
+
+        for (var i = 0; i < garbage.Length; i++)
+        {
+            var pFinded = false;
+            var gFinded = false;
+            var mFinded = false;
+
+            for (var j = 0; j < garbage[i].Length; j++)
+            {
+                switch (garbage[i][j])
+                {
+                    case 'P':
+                        if (!pFinded)
+                        {
+                            pFinded = true;
+                            pSum = sum;
+                        }
+                        p++;
+                        break;
+                    case 'G':
+                        if (!gFinded)
+                        {
+                            gFinded = true;
+                            gSum = sum;
+                        }
+                        g++;
+                        break;
+                    case 'M':
+                        if (!mFinded)
+                        {
+                            mFinded = true;
+                            mSum = sum;
+                        }
+                        m++;
+                        break;
+                }
+            }
+
+            if (i < travel.Length)
+                sum += travel[i];
+        }
+
+        return pSum + gSum + mSum + p + g + m;
+    }
+}
---
+++ LeetCode/2. Middle/2391. Minimum Amount of Time to Collect Garbage/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._2391._Minimum_Amount_of_Time_to_Collect_Garbage;
+
+[TestFixture(TestName = "2391. Minimum Amount of Time to Collect Garbage")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{"G","P","GP","GG"}, new[]{2,4,3}, 21)]
+    [TestCase(new[]{"MMM","PGM","GP"}, new[]{3,10}, 37)]
+    public void Test(string[] input1, int[] input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.GarbageCollection(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
