ee08b915e5227eeb0bed39f597337dd0a892e664
hackerdmitry <d.butov@skbkontur.ru>
2023-11-21 11:14:54 +0500

    1814. Count Nice Pairs in an Array

---
+++ LeetCode/2. Middle/1814. Count Nice Pairs in an Array/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._2._Middle._1814._Count_Nice_Pairs_in_an_Array;
+
+public class Solution
+{
+    private const int module = 1_000_000_007;
+
+    public int CountNicePairs(int[] nums)
+    {
+        var dictDiffs = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            var diff = num - GetRev(num);
+            if (dictDiffs.ContainsKey(diff))
+                dictDiffs[diff]++;
+            else
+                dictDiffs[diff] = 1;
+        }
+
+        var sum = 0L;
+        foreach (var (_, count) in dictDiffs)
+        {
+            sum += (long)count * (count - 1) / 2;
+            sum %= module;
+        }
+
+        return (int)sum;
+    }
+
+    private int GetRev(int num)
+    {
+        var revNum = 0;
+        while (num > 0)
+        {
+            revNum = revNum * 10 + num % 10;
+            num /= 10;
+        }
+
+        return revNum;
+    }
+}
---
+++ LeetCode/2. Middle/1814. Count Nice Pairs in an Array/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1814._Count_Nice_Pairs_in_an_Array;
+
+[TestFixture(TestName = "1814. Count Nice Pairs in an Array")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{42,11,1,97}, 2)]
+    [TestCase(new[]{13,10,35,24,76}, 4)]
+    public void Test(int[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.CountNicePairs(input);
+        Assert.AreEqual(output, actual);
+    }
+}
