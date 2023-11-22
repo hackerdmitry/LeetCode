f0100cdc8602ad05c05158a4b4966f154987c133
hackerdmitry <d.butov@skbkontur.ru>
2023-11-22 09:51:09 +0500

    1424. Diagonal Traverse II

---
+++ LeetCode/2. Middle/1424. Diagonal Traverse II/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._2._Middle._1424._Diagonal_Traverse_II;
+
+public class Solution
+{
+    public int[] FindDiagonalOrder(IList<IList<int>> nums)
+    {
+        var preparedNums = new LinkedList<(LinkedListNode<int>, LinkedList<int>)>();
+        foreach (var rowNums in nums)
+        {
+            var rowPreparedNums = new LinkedList<int>(rowNums);
+            preparedNums.AddLast((rowPreparedNums.First, rowPreparedNums));
+        }
+
+        var result = new List<int>();
+
+        var start = preparedNums.First;
+        while (preparedNums.Count > 0)
+        {
+            var next = start.Next;
+            var current = start;
+
+            while (current != null)
+            {
+                var (curNode, rowPreparedNums) = current.Value;
+                result.Add(curNode.Value);
+                var previous = current.Previous;
+                if (curNode.Next == null)
+                {
+                    if (start == current)
+                        start = start.Previous;
+                    preparedNums.Remove(current.Value);
+                }
+                else
+                    current.Value = (curNode.Next, rowPreparedNums);
+                current = previous;
+            }
+
+            if (next != null)
+                start = next;
+        }
+
+        return result.ToArray();
+    }
+}
---
+++ LeetCode/2. Middle/1424. Diagonal Traverse II/Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1424._Diagonal_Traverse_II;
+
+[TestFixture(TestName = "1424. Diagonal Traverse II")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int[][] input, int[] output)
+    {
+        var solution = new Solution();
+        Console.WriteLine($"output: [{string.Join(',', output)}]");
+        var actual = solution.FindDiagonalOrder(input);
+        Console.WriteLine($"actual: [{string.Join(',', actual)}]");
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        var testCasesString = @"
+[[1,2,3],[4,5,6],[7,8,9]]
+[1,4,2,7,5,3,8,6,9]
+
+[[1,2,3,4,5],[6,7],[8],[9,10,11],[12,13,14,15,16]]
+[1,6,2,8,7,3,9,4,12,10,5,13,11,14,15,16]
+
+[[14,12,19,16,9],[13,14,15,8,11],[11,13,1]]
+[14,13,12,11,14,19,13,15,16,1,8,9,11]
+".Trim();
+
+        var testCases = testCasesString.Split(
+            Environment.NewLine + Environment.NewLine,
+            StringSplitOptions.RemoveEmptyEntries);
+        foreach (var testCase in testCases)
+        {
+            var lines = testCase.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            var input = lines[0].ParseIntArray2d();
+            var output = lines[1].ParseIntArray();
+            yield return new object[] { input, output };
+        }
+    }
+}
