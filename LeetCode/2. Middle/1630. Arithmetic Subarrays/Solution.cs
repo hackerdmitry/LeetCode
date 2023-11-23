ec69446f5672c4cb37a3a44992e7533da124b4c2
hackerdmitry <d.butov@skbkontur.ru>
2023-11-23 11:08:49 +0500

    1630. Arithmetic Subarrays

---
+++ LeetCode/2. Middle/1630. Arithmetic Subarrays/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2._Middle._1630._Arithmetic_Subarrays;
+
+public class Solution
+{
+    public IList<bool> CheckArithmeticSubarrays(int[] nums, int[] l, int[] r)
+    {
+        var result = new bool[l.Length];
+
+        for (var i = 0; i < l.Length; i++)
+        {
+            var ll = l[i];
+            var rr = r[i];
+
+            var subsequence = Enumerable.Range(ll, rr - ll + 1).Select(j => nums[j]).OrderBy(x => x);
+            var diff = -1;
+            var prevNum = -1;
+            var j = 0;
+            result[i] = true;
+            foreach (var num in subsequence)
+            {
+                if (j != 0)
+                    if (j == 1)
+                        diff = num - prevNum;
+                    else if (num - prevNum != diff)
+                    {
+                        result[i] = false;
+                        break;
+                    }
+
+                prevNum = num;
+                j++;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/2. Middle/1630. Arithmetic Subarrays/Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1630._Arithmetic_Subarrays;
+
+[TestFixture(TestName = "1630. Arithmetic Subarrays")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(
+        new[] { 4, 6, 5, 9, 3, 7 },
+        new[] { 0, 0, 2 },
+        new[] { 2, 3, 5 },
+        new[] { true, false, true })]
+    [TestCase(
+        new[] { -12, -9, -3, -12, -6, 15, 20, -25, -20, -15, -10 },
+        new[] { 0, 1, 6, 4, 8, 7 },
+        new[] { 4, 4, 9, 7, 9, 10 },
+        new[] { false, true, false, false, true, true })]
+    public void Test(int[] input1, int[] input2, int[] input3, bool[] output)
+    {
+        var solution = new Solution();
+        Console.WriteLine($"output: [{string.Join(',', output)}]");
+        var actual = solution.CheckArithmeticSubarrays(input1, input2, input3);
+        Console.WriteLine($"actual: [{string.Join(',', actual)}]");
+        Assert.AreEqual(output, actual);
+    }
+}
