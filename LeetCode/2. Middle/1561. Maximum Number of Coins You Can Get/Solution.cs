16ec3b9778130fcbd3e6c00b5a0dfa88f5f4caf5
hackerdmitry <d.butov@skbkontur.ru>
2023-11-24 09:37:53 +0500

    1561. Maximum Number of Coins You Can Get

---
+++ LeetCode/2. Middle/1561. Maximum Number of Coins You Can Get/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LeetCode._2._Middle._1561._Maximum_Number_of_Coins_You_Can_Get;
+
+public class Solution
+{
+    public int MaxCoins(int[] piles)
+    {
+        Array.Sort(piles);
+        var sum = 0;
+        for (int i = piles.Length - 2, c = 0; c < piles.Length / 3; i -= 2, c++)
+            sum += piles[i];
+
+        return sum;
+    }
+}
---
+++ LeetCode/2. Middle/1561. Maximum Number of Coins You Can Get/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1561._Maximum_Number_of_Coins_You_Can_Get;
+
+[TestFixture(TestName = "1561. Maximum Number of Coins You Can Get")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{2,4,1,2,7,8}, 9)]
+    [TestCase(new[]{2,4,5}, 4)]
+    [TestCase(new[]{9,8,7,6,5,1,2,3,4}, 18)]
+    public void Test(int[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MaxCoins(input);
+        Assert.AreEqual(output, actual);
+    }
+}
