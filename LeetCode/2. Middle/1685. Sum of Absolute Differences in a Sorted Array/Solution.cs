0d12e8c2ee74c27b606d00c1fdb6a326429f1993
hackerdmitry <hacker.dmitry@gmail.com>
2023-11-25 15:21:02 +0500

    1685. Sum of Absolute Differences in a Sorted Array

---
+++ LeetCode/2. Middle/1685. Sum of Absolute Differences in a Sorted Array/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace LeetCode._2._Middle._1685._Sum_of_Absolute_Differences_in_a_Sorted_Array;
+
+public class Solution
+{
+    public int[] GetSumAbsoluteDifferences(int[] nums)
+    {
+        var leftSum = 0;
+        var rightSum = nums.Sum();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var num = nums[i];
+            rightSum -= num;
+            nums[i] = num * i - leftSum + rightSum - (nums.Length - 1 - i) * num;
+            leftSum += num;
+        }
+
+        return nums;
+    }
+}
---
+++ LeetCode/2. Middle/1685. Sum of Absolute Differences in a Sorted Array/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1685._Sum_of_Absolute_Differences_in_a_Sorted_Array;
+
+[TestFixture(TestName = "1685. Sum of Absolute Differences in a Sorted Array")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{2,3,5}, new[]{4,3,5})]
+    [TestCase(new[]{1,4,6,8,10}, new[]{24,15,13,15,21})]
+    public void Test(int[] input, int[] output)
+    {
+        var solution = new Solution();
+        Console.WriteLine($"output: [{string.Join(',', output)}]");
+        var actual = solution.GetSumAbsoluteDifferences(input);
+        Console.WriteLine($"actual: [{string.Join(',', actual)}]");
+        Assert.AreEqual(output, actual);
+    }
+}
