80f15915c44458554299f33b8cc658057e936327
hackerdmitry <hacker.dmitry@gmail.com>
2023-11-26 23:58:55 +0500

    1727. Largest Submatrix With Rearrangements

---
+++ LeetCode/2. Middle/1727. Largest Submatrix With Rearrangements/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace LeetCode._2._Middle._1727._Largest_Submatrix_With_Rearrangements;
+
+public class Solution
+{
+    public int LargestSubmatrix(int[][] matrix)
+    {
+        var m = matrix.Length;
+        var n = matrix[0].Length;
+
+        for (var i = 0; i < n; i++)
+            for (var j = 1; j < m; j++)
+            {
+                if (matrix[j][i] == 0)
+                    continue;
+                matrix[j][i] = matrix[j - 1][i] + 1;
+            }
+
+        var result = 0;
+        for (var i = 0; i < m; i++)
+        {
+            var localMax = matrix[i].OrderByDescending(x => x).Select((x, c) => x * (c + 1)).Max();
+            if (localMax > result)
+                result = localMax;
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/2. Middle/1727. Largest Submatrix With Rearrangements/Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1727._Largest_Submatrix_With_Rearrangements;
+
+[TestFixture(TestName = "1727. Largest Submatrix With Rearrangements")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int number, int[][] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.LargestSubmatrix(input);
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        var testCasesString = @"
+[[0,0,1],[1,1,1],[1,0,1]]
+4
+
+[[1,0,1,0,1]]
+3
+
+[[1,1,0],[1,0,1]]
+2
+
+[[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,1],[0,1,1,0,1,1,1,1,0,1,1,0,0,1,0,1,1,1,1,0,1,1,1,1,1,1]]
+34
+".Trim();
+
+        var testCases = testCasesString.Split(
+            Environment.NewLine + Environment.NewLine,
+            StringSplitOptions.RemoveEmptyEntries);
+        var index = 1;
+        foreach (var testCase in testCases)
+        {
+            var lines = testCase.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            var input = lines[0].ParseIntArray2d();
+            var output = lines[1].ParseInt();
+            yield return new object[] { index++, input, output };
+        }
+    }
+}
