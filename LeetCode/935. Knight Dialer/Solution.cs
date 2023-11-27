cda9d8f3803063d30c9a832e3b5842b091e43d96
hackerdmitry <d.butov@skbkontur.ru>
2023-11-27 09:33:45 +0500

    935. Knight Dialer

---
+++ LeetCode/935. Knight Dialer/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._935._Knight_Dialer;
+
+public class Solution
+{
+    private const int module = 1_000_000_007;
+
+    public int KnightDialer(int n)
+    {
+        var prev = new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+        var cur = new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+        var possibleSteps = new Dictionary<int, int[]>
+        {
+            [0] = new[] { 4, 6 },
+            [1] = new[] { 6, 8 },
+            [2] = new[] { 7, 9 },
+            [3] = new[] { 4, 8 },
+            [4] = new[] { 3, 9, 0 },
+            [5] = Array.Empty<int>(),
+            [6] = new[] { 1, 7, 0 },
+            [7] = new[] { 2, 6 },
+            [8] = new[] { 1, 3 },
+            [9] = new[] { 2, 4 },
+        };
+
+        for (var i = 1; i < n; i++)
+        {
+            for (var j = 0; j < cur.Length; j++)
+                cur[j] = (int)(possibleSteps[j].Sum(x => (long)prev[x]) % module);
+            for (var j = 0; j < prev.Length; j++)
+                prev[j] = cur[j];
+        }
+
+        return (int)(cur.Select(x => (long)x).Sum() % module);
+    }
+}
---
+++ LeetCode/935. Knight Dialer/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._935._Knight_Dialer;
+
+[TestFixture(TestName = "935. Knight Dialer")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(1, 10)]
+    [TestCase(2, 20)]
+    [TestCase(3131, 136006598)]
+    public void Test(int input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.KnightDialer(input);
+        Assert.AreEqual(output, actual);
+    }
+}
