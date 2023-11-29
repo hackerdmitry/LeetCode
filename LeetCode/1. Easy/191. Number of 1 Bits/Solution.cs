fe447b6425315d8261e44b3dffffa7a6959003ce
hackerdmitry <d.butov@skbkontur.ru>
2023-11-29 09:16:07 +0500

    191. Number of 1 Bits

---
+++ LeetCode/1. Easy/191. Number of 1 Bits/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode._1._Easy._191._Number_of_1_Bits;
+
+public class Solution
+{
+    public int HammingWeight(uint n)
+    {
+        var sum = 0;
+        while (n != 0)
+        {
+            sum += (int)(n - (n >> 1 << 1));
+            n >>= 1;
+        }
+
+        return sum;
+    }
+}
---
+++ LeetCode/1. Easy/191. Number of 1 Bits/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using NUnit.Framework;
+
+namespace LeetCode._1._Easy._191._Number_of_1_Bits;
+
+[TestFixture(TestName = "191. Number of 1 Bits")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(0b00000000000000000000000000001011U, 3)]
+    [TestCase(0b00000000000000000000000010000000U, 1)]
+    [TestCase(0b11111111111111111111111111111101U, 31)]
+    public void Test(uint input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.HammingWeight(input);
+        Assert.AreEqual(output, actual);
+    }
+}
