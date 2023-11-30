d8de650c6bf664c1d06ec4b6b0eb0f96a7a23c56
hackerdmitry <d.butov@skbkontur.ru>
2023-11-30 13:27:07 +0500

    1611. Minimum One Bit Operations to Make Integers Zero

---
+++ LeetCode/3. Hard/1611. Minimum One Bit Operations to Make Integers Zero/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode._3._Hard._1611._Minimum_One_Bit_Operations_to_Make_Integers_Zero;
+
+public class Solution
+{
+    public int MinimumOneBitOperations(int n)
+    {
+        var k = 0;
+        var k2 = 1;
+
+        while (k < n)
+        {
+            k = (k << 1) + 1;
+            k2 <<= 1;
+        }
+        k2 >>= 1;
+        n ^= k2;
+
+        var result = k;
+        var posSign = false;
+        for (; n > 0; k >>= 1, k2 >>= 1)
+            if (k2 <= n)
+            {
+                result += posSign ? k : -k;
+                n ^= k2;
+                posSign = !posSign;
+            }
+
+        return result;
+    }
+}
---
+++ LeetCode/3. Hard/1611. Minimum One Bit Operations to Make Integers Zero/Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._3._Hard._1611._Minimum_One_Bit_Operations_to_Make_Integers_Zero;
+
+[TestFixture(TestName = "1611. Minimum One Bit Operations to Make Integers Zero")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(0, 0)]
+    [TestCase(1, 1)]
+    [TestCase(3, 2)]
+    [TestCase(6, 4)]
+    [TestCase(11, 13)]
+    [TestCase(15, 10)]
+    [TestCase(24, 16)]
+    [TestCase(20, 24)]
+    [TestCase(90, 108)]
+    [TestCase(333, 393)]
+    public void Test(int input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MinimumOneBitOperations(input);
+        Assert.AreEqual(output, actual);
+    }
+}
