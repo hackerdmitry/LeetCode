9b9ea211b3f27089c975cb107b94d7f7b5c4a9b0
hackerdmitry <d.butov@skbkontur.ru>
2023-12-01 09:10:44 +0500

    1662. Check If Two String Arrays are Equivalent

---
+++ LeetCode/1. Easy/1662. Check If Two String Arrays are Equivalent/Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCode._1._Easy._1662._Check_If_Two_String_Arrays_are_Equivalent;
+
+public class Solution
+{
+    public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+    {
+        return string.Join("", word1) == string.Join("", word2);
+    }
+}
---
+++ LeetCode/1. Easy/1662. Check If Two String Arrays are Equivalent/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1662._Check_If_Two_String_Arrays_are_Equivalent;
+
+[TestFixture(TestName = "1662. Check If Two String Arrays are Equivalent")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{"ab", "c"}, new[]{"a", "bc"}, true)]
+    [TestCase(new[]{"a", "cb"},new[]{"ab", "c"}, false)]
+    [TestCase(new[]{"abc", "d", "defg"}, new[]{"abcddefg"}, true)]
+    public void Test(string[] input1, string[] input2, bool output)
+    {
+        var solution = new Solution();
+        var actual = solution.ArrayStringsAreEqual(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
