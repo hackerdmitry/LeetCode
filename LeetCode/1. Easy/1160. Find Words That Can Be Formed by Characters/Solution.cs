ac1b8edc7336aee31626496ee76de8a08553665b
hackerdmitry <hacker.dmitry@gmail.com>
2023-12-02 17:39:24 +0500

    1160. Find Words That Can Be Formed by Characters

---
+++ LeetCode/1. Easy/1160. Find Words That Can Be Formed by Characters/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace LeetCode._1._Easy._1160._Find_Words_That_Can_Be_Formed_by_Characters;
+
+public class Solution
+{
+    public int CountCharacters(string[] words, string chars)
+    {
+        var charsDict = chars.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+        var result = 0;
+        var alphabet = new int[26];
+        foreach (var word in words)
+        {
+            for (var i = 0; i < word.Length; i++)
+                alphabet[word[i] - 'a']++;
+
+            var isFit = true;
+            for (var i = 0; i < alphabet.Length; i++)
+            {
+                var letter = (char) ('a' + i);
+                if (alphabet[i] > 0 && (!charsDict.ContainsKey(letter) || charsDict[letter] < alphabet[i]))
+                    isFit = false;
+                alphabet[i] = 0;
+            }
+
+            if (isFit)
+                result += word.Length;
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/1. Easy/1160. Find Words That Can Be Formed by Characters/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1160._Find_Words_That_Can_Be_Formed_by_Characters;
+
+[TestFixture(TestName = "1160. Find Words That Can Be Formed by Characters")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{"cat","bt","hat","tree"}, "atach", 6)]
+    [TestCase(new[]{"hello","world","leetcode"}, "welldonehoneyr", 10)]
+    public void Test(string[] input1, string input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.CountCharacters(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
