8d61e941e0faec8513de393b5e239d4304205860
hackerdmitry <hacker.dmitry@gmail.com>
2023-12-03 13:49:01 +0500

    1266. Minimum Time Visiting All Points

---
+++ LeetCode/1. Easy/1266. Minimum Time Visiting All Points/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode._1._Easy._1266._Minimum_Time_Visiting_All_Points;
+
+public class Solution
+{
+    public int MinTimeToVisitAllPoints(int[][] points)
+    {
+        var result = 0;
+        for (var i = 1; i < points.Length; i++)
+        {
+            var prevPoint = points[i - 1];
+            var curPoint = points[i];
+
+            var hor = Math.Abs(prevPoint[0] - curPoint[0]);
+            var ver = Math.Abs(prevPoint[1] - curPoint[1]);
+
+            result += hor > ver ? hor : ver;
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/1. Easy/1266. Minimum Time Visiting All Points/Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1266._Minimum_Time_Visiting_All_Points;
+
+[TestFixture(TestName = "1266. Minimum Time Visiting All Points")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int[][] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MinTimeToVisitAllPoints(input);
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        var testCasesString = @"
+[[1,1],[3,4],[-1,0]]
+7
+
+[[3,2],[-2,2]]
+5
+".Trim();
+
+        var testCases = testCasesString.Split(
+            Environment.NewLine + Environment.NewLine,
+            StringSplitOptions.RemoveEmptyEntries);
+        foreach (var testCase in testCases)
+        {
+            var lines = testCase.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            var input = lines[0].ParseIntArray2d();
+            var output = lines[1].ParseInt();
+            yield return new object[] { input, output };
+        }
+    }
+}
