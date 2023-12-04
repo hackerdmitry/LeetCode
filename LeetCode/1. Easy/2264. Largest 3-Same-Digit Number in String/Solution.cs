379b68d56d3ad4d1329e7da5aac5c1d758030dbf
hackerdmitry <d.butov@skbkontur.ru>
2023-12-04 09:35:10 +0500

    2264. Largest 3-Same-Digit Number in String

---
+++ LeetCode/1. Easy/2264. Largest 3-Same-Digit Number in String/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._1._Easy._2264._Largest_3_Same_Digit_Number_in_String;
+
+public class Solution
+{
+    public string LargestGoodInteger(string num)
+    {
+        var maxDigit = -1;
+
+        for (var i = 2; i < num.Length; i++)
+            if (num[i - 2] == num[i - 1] && num[i - 1] == num[i] && num[i] - '0' > maxDigit)
+                maxDigit = num[i] - '0';
+
+        return maxDigit == -1 ? "" : new string((char)(maxDigit + '0'), 3);
+    }
+}
---
+++ LeetCode/1. Easy/2264. Largest 3-Same-Digit Number in String/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._2264._Largest_3_Same_Digit_Number_in_String;
+
+[TestFixture(TestName = "2264. Largest 3-Same-Digit Number in String")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("6777133339", "777")]
+    [TestCase("2300019", "000")]
+    [TestCase("42352338", "")]
+    public void Test(string input, string output)
+    {
+        var solution = new Solution();
+        var actual = solution.LargestGoodInteger(input);
+        Assert.AreEqual(output, actual);
+    }
+}
