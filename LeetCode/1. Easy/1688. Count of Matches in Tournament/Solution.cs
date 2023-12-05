5596295a347ca8ef942c292fadd76d4052fb3428
hackerdmitry <d.butov@skbkontur.ru>
2023-12-05 10:36:59 +0500

    1688. Count of Matches in Tournament

---
+++ LeetCode/1. Easy/1688. Count of Matches in Tournament/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode._1._Easy._1688._Count_of_Matches_in_Tournament;
+
+public class Solution
+{
+    public int NumberOfMatches(int n)
+    {
+        var result = 0;
+        while (n != 1)
+        {
+            result += n % 2 == 0
+                ? n / 2
+                : n / 2 + 1;
+            n /= 2;
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/1. Easy/1688. Count of Matches in Tournament/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1688._Count_of_Matches_in_Tournament;
+
+[TestFixture(TestName = "1688. Count of Matches in Tournament")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(7, 6)]
+    [TestCase(14, 13)]
+    public void Test(int input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.NumberOfMatches(input);
+        Assert.AreEqual(output, actual);
+    }
+}
