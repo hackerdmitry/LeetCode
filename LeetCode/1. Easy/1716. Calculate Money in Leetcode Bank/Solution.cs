e3c82da6206220d18a4b0a1d9c15a56c2cd16fdb
hackerdmitry <d.butov@skbkontur.ru>
2023-12-06 09:16:49 +0500

    1716. Calculate Money in Leetcode Bank

---
+++ LeetCode/1. Easy/1716. Calculate Money in Leetcode Bank/Solution.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace LeetCode._1._Easy._1716._Calculate_Money_in_Leetcode_Bank;
+
+public class Solution
+{
+    public int TotalMoney(int n)
+    {
+        return Enumerable.Range(0, n).Sum(x => x / 7 + x % 7 + 1);
+    }
+}
---
+++ LeetCode/1. Easy/1716. Calculate Money in Leetcode Bank/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1716._Calculate_Money_in_Leetcode_Bank;
+
+[TestFixture(TestName = "1716. Calculate Money in Leetcode Bank")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(4, 10)]
+    [TestCase(10, 37)]
+    [TestCase(20, 96)]
+    public void Test(int input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.TotalMoney(input);
+        Assert.AreEqual(output, actual);
+    }
+}
