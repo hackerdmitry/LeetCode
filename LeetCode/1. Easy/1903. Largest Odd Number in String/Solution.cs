19729b2709962c76e2e5ff234d5a88f97debda44
hackerdmitry <d.butov@skbkontur.ru>
2023-12-07 09:13:43 +0500

    1903. Largest Odd Number in String

---
+++ LeetCode/1. Easy/1903. Largest Odd Number in String/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace LeetCode._1._Easy._1903._Largest_Odd_Number_in_String;
+
+public class Solution
+{
+    public string LargestOddNumber(string num)
+    {
+        var length = num
+            .Select((x, i) => (x, i + 1))
+            .Reverse()
+            .FirstOrDefault<(char x, int i)>(x => (x.x - '0') % 2 == 1, ('\0', 0)).i;
+        return num[..length];
+    }
+}
---
+++ LeetCode/1. Easy/1903. Largest Odd Number in String/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1903._Largest_Odd_Number_in_String;
+
+[TestFixture(TestName = "1903. Largest Odd Number in String")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("52", "5")]
+    [TestCase("4206", "")]
+    [TestCase("35427", "35427")]
+    public void Test(string input, string output)
+    {
+        var solution = new Solution();
+        var actual = solution.LargestOddNumber(input);
+        Assert.AreEqual(output, actual);
+    }
+}
