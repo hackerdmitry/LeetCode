085c4fcbbf40218d8fa21aaf51b0d2c3682c376c
hackerdmitry <d.butov@skbkontur.ru>
2023-12-08 09:34:00 +0500

    606. Construct String from Binary Tree

---
+++ LeetCode/1. Easy/606. Construct String from Binary Tree/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using LeetCode.__TreeNode;
+
+namespace LeetCode._1._Easy._606._Construct_String_from_Binary_Tree;
+
+public class Solution
+{
+    public string Tree2str(TreeNode root)
+    {
+        return Tree2StringBuilder(root).ToString();
+    }
+
+    private StringBuilder Tree2StringBuilder(TreeNode node)
+    {
+        if (node == null)
+            return new StringBuilder();
+
+        var result = new StringBuilder(node.val.ToString());
+
+        if (node.right != null)
+        {
+            AppendWrappedParenthesis(result, Tree2StringBuilder(node.left));
+            AppendWrappedParenthesis(result, Tree2StringBuilder(node.right));
+        }
+        else if (node.left != null)
+            AppendWrappedParenthesis(result, Tree2StringBuilder(node.left));
+
+        return result;
+    }
+
+    private StringBuilder AppendWrappedParenthesis(StringBuilder stringBuilder, StringBuilder val) =>
+        stringBuilder
+            .Append('(')
+            .Append(val)
+            .Append(')');
+}
---
+++ LeetCode/1. Easy/606. Construct String from Binary Tree/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using LeetCode.__TreeNode;
+using NUnit.Framework;
+
+namespace LeetCode._1._Easy._606._Construct_String_from_Binary_Tree;
+
+[TestFixture(TestName = "606. Construct String from Binary Tree")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int?[] input, string output)
+    {
+        var solution = new Solution();
+
+        var treeRoot = TreeNode.CreateFromArray(input);
+        var actual = solution.Tree2str(treeRoot);
+
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        yield return new object[]
+        {
+            new int?[]{1,2,3,4},
+            "1(2(4))(3)",
+        };
+        yield return new object[]
+        {
+            new int?[]{1,2,3,null,4},
+            "1(2()(4))(3)",
+        };
+    }
+}
