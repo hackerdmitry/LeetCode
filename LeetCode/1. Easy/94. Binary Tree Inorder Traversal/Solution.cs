4ee0545e2ec0070cefab49693a4cbc923b5d094f
hackerdmitry <hacker.dmitry@gmail.com>
2023-12-09 12:10:05 +0500

    94. Binary Tree Inorder Traversal

---
+++ LeetCode/1. Easy/94. Binary Tree Inorder Traversal/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using LeetCode.__TreeNode;
+
+namespace LeetCode._1._Easy._94._Binary_Tree_Inorder_Traversal;
+
+public class Solution
+{
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        var result = new List<int>();
+        if (root == null)
+            return result;
+
+        var stack = new Stack<(TreeNode, int)>();
+        stack.Push((root, 1));
+
+        while (stack.Count > 0)
+        {
+            var (node, state) = stack.Pop();
+
+            if (state == 1)
+            {
+                state++;
+                if (node.left != null)
+                {
+                    stack.Push((node, 2));
+                    stack.Push((node.left, 1));
+                    continue;
+                }
+            }
+
+            if (state == 2)
+            {
+                state++;
+                result.Add(node.val);
+            }
+
+            if (state == 3 && node.right != null)
+                stack.Push((node.right, 1));
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/1. Easy/94. Binary Tree Inorder Traversal/Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using LeetCode.__TreeNode;
+using NUnit.Framework;
+
+namespace LeetCode._1._Easy._94._Binary_Tree_Inorder_Traversal;
+
+[TestFixture(TestName = "94. Binary Tree Inorder Traversal")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int?[] input, int[] output)
+    {
+        var solution = new Solution();
+
+        var treeRoot = TreeNode.CreateFromArray(input);
+        Console.WriteLine("expected: " + string.Join(", ", output));
+        var actual = solution.InorderTraversal(treeRoot);
+        Console.WriteLine("actual: " + string.Join(", ", actual));
+
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        yield return new object[]
+        {
+            new int?[]{1,null,2,3},
+            new[]{1,3,2}
+        };
+        yield return new object[]
+        {
+            Array.Empty<int?>(),
+            Array.Empty<int>(),
+        };
+        yield return new object[]
+        {
+            new int?[]{1},
+            new[]{1}
+        };
+        yield return new object[]
+        {
+            new int?[]{3,1,2},
+            new[]{1,3,2}
+        };
+    }
+}
