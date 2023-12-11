8c8c2954ed2f16cab0a55810de2a9c0564e540f7
hackerdmitry <d.butov@skbkontur.ru>
2023-12-11 09:25:09 +0500

    1287. Element Appearing More Than 25% In Sorted Array

---
+++ LeetCode/1. Easy/1287. Element Appearing More Than 25% In Sorted Array/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode._1._Easy._1287._Element_Appearing_More_Than_25__In_Sorted_Array;
+
+public class Solution
+{
+    public int FindSpecialInteger(int[] arr)
+    {
+        var moreThan25 = Math.Ceiling(arr.Length * 0.25);
+        if (arr.Length % 4 == 0)
+            moreThan25++;
+
+        var lastSameIndex = 0;
+        for (var i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] != arr[lastSameIndex])
+                lastSameIndex = i;
+
+            if (i - lastSameIndex + 1 >= moreThan25)
+                break;
+        }
+
+        return arr[lastSameIndex];
+    }
+}
---
+++ LeetCode/1. Easy/1287. Element Appearing More Than 25% In Sorted Array/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1287._Element_Appearing_More_Than_25__In_Sorted_Array;
+
+[TestFixture(TestName = "1287. Element Appearing More Than 25% In Sorted Array")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{1,2,2,6,6,6,6,7,10}, 6)]
+    [TestCase(new[]{1,1}, 1)]
+    [TestCase(new[]{1}, 1)]
+    [TestCase(new[]{1,2,3,3}, 3)]
+    [TestCase(new[]{1,2,2,2,2,2,3,3,3,3,3,3,4,4,4,4,5,5,5,5}, 3)]
+    public void Test(int[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.FindSpecialInteger(input);
+        Assert.AreEqual(output, actual);
+    }
+}
