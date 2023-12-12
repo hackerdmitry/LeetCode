353204b1c9abf8f6c64a14b0ed0c87e67b8762bb
hackerdmitry <d.butov@skbkontur.ru>
2023-12-12 18:47:55 +0500

    1464. Maximum Product of Two Elements in an Array

---
+++ LeetCode/1. Easy/1464. Maximum Product of Two Elements in an Array/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode._1._Easy._1464._Maximum_Product_of_Two_Elements_in_an_Array;
+
+public class Solution
+{
+    public int MaxProduct(int[] nums)
+    {
+        var max1 = -1;
+        var max2 = -1;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (max1 < nums[i])
+            {
+                max2 = max1;
+                max1 = nums[i];
+            }
+            else if (max2 < nums[i])
+                max2 = nums[i];
+        }
+
+        return (max1 - 1) * (max2 - 1);
+    }
+}
---
+++ LeetCode/1. Easy/1464. Maximum Product of Two Elements in an Array/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1464._Maximum_Product_of_Two_Elements_in_an_Array;
+
+[TestFixture(TestName = "1464. Maximum Product of Two Elements in an Array")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{3,4,5,2}, 12)]
+    [TestCase(new[]{3,5,5,2}, 16)]
+    [TestCase(new[]{1,5,4,5}, 16)]
+    [TestCase(new[]{3,7}, 12)]
+    public void Test(int[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MaxProduct(input);
+        Assert.AreEqual(output, actual);
+    }
+}
