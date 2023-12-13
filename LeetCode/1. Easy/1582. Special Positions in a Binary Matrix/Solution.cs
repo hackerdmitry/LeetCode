1064a8173a15f4634c1f6f7e0e6bfe01361d1280
hackerdmitry <d.butov@skbkontur.ru>
2023-12-13 10:46:18 +0500

    1582. Special Positions in a Binary Matrix

---
+++ LeetCode/1. Easy/1582. Special Positions in a Binary Matrix/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode._1._Easy._1582._Special_Positions_in_a_Binary_Matrix;
+
+public class Solution
+{
+    public int NumSpecial(int[][] mat)
+    {
+        var result = 0;
+
+        for (var i = 0; i < mat.Length; i++)
+        {
+            for (var j = 0; j < mat[0].Length; j++)
+            {
+                if (mat[i][j] == 1)
+                {
+                    for (var y = 0; y < mat.Length; y++)
+                    {
+                        if (i == y)
+                            continue;
+                        if (mat[y][j] == 1)
+                            goto next;
+                    }
+                    for (var x = 0; x < mat[0].Length; x++)
+                    {
+                        if (j == x)
+                            continue;
+                        if (mat[i][x] == 1)
+                            goto next;
+                    }
+                    result++;
+                }
+
+                next: ;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/1. Easy/1582. Special Positions in a Binary Matrix/Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1582._Special_Positions_in_a_Binary_Matrix;
+
+[TestFixture(TestName = "1582. Special Positions in a Binary Matrix")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int[][] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.NumSpecial(input);
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        var testCasesString = @"
+[[1,0,0],[0,0,1],[1,0,0]]
+1
+
+[[1,0,0],[0,1,0],[0,0,1]]
+3
+
+[[1,0,0],[1,0,0],[1,0,0]]
+0
+
+[[0,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,1],[0,0,0,0,0,0,1,0],[0,0,0,0,0,0,0,1]]
+2
+".Trim();
+
+        var testCases = testCasesString.Split(
+            Environment.NewLine + Environment.NewLine,
+            StringSplitOptions.RemoveEmptyEntries);
+        foreach (var testCase in testCases)
+        {
+            var lines = testCase.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            var input = lines[0].ParseIntArray2d();
+            var output = lines[1].ParseInt();
+            yield return new object[] { input, output };
+        }
+    }
+}
