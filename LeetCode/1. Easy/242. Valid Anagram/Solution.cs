69e0bf2689ff0bdf5ec09b7783afad0983b7e529
hackerdmitry <hacker.dmitry@gmail.com>
2023-12-16 19:45:44 +0500

    242. Valid Anagram

---
+++ LeetCode/1. Easy/242. Valid Anagram/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode._1._Easy._242._Valid_Anagram;
+
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+            return false;
+
+        var anagramS = new int[26];
+        var anagramT = new int[26];
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            anagramS[s[i] - 'a']++;
+            anagramT[t[i] - 'a']++;
+        }
+
+        for (var i = 0; i < 26; i++)
+            if (anagramS[i] != anagramT[i])
+                return false;
+
+        return true;
+    }
+}
---
+++ LeetCode/1. Easy/242. Valid Anagram/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._242._Valid_Anagram;
+
+[TestFixture(TestName = "242. Valid Anagram")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("anagram", "nagaram", true)]
+    [TestCase("rat", "car", false)]
+    public void Test(string input1, string input2, bool output)
+    {
+        var solution = new Solution();
+        var actual = solution.IsAnagram(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
