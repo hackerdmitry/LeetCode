38c3a32f049671d0ae30b17f3490c45d5f271222
hackerdmitry <hacker.dmitry@gmail.com>
2023-12-18 20:54:08 +0500

    1913. Maximum Product Difference Between Two Pairs

---
+++ LeetCode/1. Easy/1913. Maximum Product Difference Between Two Pairs/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode._1._Easy._1913._Maximum_Product_Difference_Between_Two_Pairs;
+
+public class Solution
+{
+    public int MaxProductDifference(int[] nums)
+    {
+        var max1 = int.MinValue;
+        var max2 = int.MinValue;
+
+        var min1 = int.MaxValue;
+        var min2 = int.MaxValue;
+
+        foreach (var num in nums)
+        {
+            if (num > max1)
+            {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2)
+                max2 = num;
+
+            if (num < min1)
+            {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2)
+                min2 = num;
+        }
+
+        return max1 * max2 - min1 * min2;
+    }
+}
---
+++ LeetCode/1. Easy/1913. Maximum Product Difference Between Two Pairs/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1913._Maximum_Product_Difference_Between_Two_Pairs;
+
+[TestFixture(TestName = "1913. Maximum Product Difference Between Two Pairs")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{5,6,2,7,4}, 34)]
+    [TestCase(new[]{4,2,5,9,7,4,8}, 64)]
+    public void Test(int[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MaxProductDifference(input);
+        Assert.AreEqual(output, actual);
+    }
+}
