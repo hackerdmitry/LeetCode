7b3a532951a06cd1c7b1d5c87156fa216dc99fac
hackerdmitry <hacker.dmitry@gmail.com>
2023-12-19 20:53:07 +0500

    661. Image Smoother

---
+++ LeetCode/1. Easy/661. Image Smoother/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode._1._Easy._661._Image_Smoother;
+
+public class Solution
+{
+    public int[][] ImageSmoother(int[][] img)
+    {
+        var result = new int[img.Length][];
+
+        for (var i = 0; i < img.Length; i++)
+        {
+            result[i] = new int[img[i].Length];
+
+            for (var j = 0; j < img[i].Length; j++)
+            {
+                var count = 0;
+                for (var x = Math.Max(i - 1, 0); x < Math.Min(i + 2, img.Length); x++)
+                {
+                    for (var y = Math.Max(j - 1, 0); y < Math.Min(j + 2, img[i].Length); y++)
+                    {
+                        count++;
+                        result[i][j] += img[x][y];
+                    }
+                }
+
+                result[i][j] /= count;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/1. Easy/661. Image Smoother/Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._1._Easy._661._Image_Smoother;
+
+[TestFixture(TestName = "661. Image Smoother")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int[][] input, int[][] output)
+    {
+        var solution = new Solution();
+        var actual = solution.ImageSmoother(input);
+        Console.WriteLine($"output:\n{string.Join(",\n", output.Select(x => $"[{string.Join(',', x)}]"))}");
+        Console.WriteLine($"actual:\n{string.Join(",\n", actual.Select(x => $"[{string.Join(',', x)}]"))}");
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        var testCasesString = @"
+[[1,1,1],[1,0,1],[1,1,1]]
+[[0,0,0],[0,0,0],[0,0,0]]
+
+[[100,200,100],[200,50,200],[100,200,100]]
+[[137,141,137],[141,138,141],[137,141,137]]
+
+[[2,3,4],[5,6,7],[8,9,10],[11,12,13],[14,15,16]]
+[[4,4,5],[5,6,6],[8,9,9],[11,12,12],[13,13,14]]
+".Trim();
+
+        var testCases = testCasesString.Split(
+            Environment.NewLine + Environment.NewLine,
+            StringSplitOptions.RemoveEmptyEntries);
+        foreach (var testCase in testCases)
+        {
+            var lines = testCase.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            var input = lines[0].ParseIntArray2d();
+            var output = lines[1].ParseIntArray2d();
+            yield return new object[] { input, output };
+        }
+    }
+}
