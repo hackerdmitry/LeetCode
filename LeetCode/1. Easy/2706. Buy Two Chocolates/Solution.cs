82d6688be667eeae66dc335f7b6178bcd7d82781
hackerdmitry <hacker.dmitry@gmail.com>
2023-12-20 15:01:50 +0500

    2706. Buy Two Chocolates

---
+++ LeetCode/1. Easy/2706. Buy Two Chocolates/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode._1._Easy._2706._Buy_Two_Chocolates;
+
+public class Solution
+{
+    public int BuyChoco(int[] prices, int money)
+    {
+        var min1 = int.MaxValue;
+        var min2 = int.MaxValue;
+
+        foreach (var price in prices)
+        {
+            if (price < min1)
+            {
+                min2 = min1;
+                min1 = price;
+            }
+            else if (price < min2) min2 = price;
+        }
+
+        return min1 + min2 > money ? money : money - min1 - min2;
+    }
+}
---
+++ LeetCode/1. Easy/2706. Buy Two Chocolates/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._2706._Buy_Two_Chocolates;
+
+[TestFixture(TestName = "2706. Buy Two Chocolates")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{1,2,2}, 3, 0)]
+    [TestCase(new[]{3,2,3}, 3, 3)]
+    public void Test(int[] input1, int input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.BuyChoco(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
