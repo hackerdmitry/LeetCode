9329fa2bfebd118dc54805e2ccb65deba41a67df
hackerdmitry <hacker.dmitry@gmail.com>
2023-12-21 16:46:03 +0500

    1637. Widest Vertical Area Between Two Points Containing No Points

---
+++ LeetCode/2. Middle/1637. Widest Vertical Area Between Two Points Containing No Points/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2._Middle._1637._Widest_Vertical_Area_Between_Two_Points_Containing_No_Points;
+
+public class Solution
+{
+    public int MaxWidthOfVerticalArea(int[][] points)
+    {
+        var result = int.MinValue;
+        var startPoint = int.MaxValue;
+        foreach (var point in points.Select(x => x[0]).OrderBy(x => x))
+        {
+            if (startPoint != int.MaxValue)
+                result = Math.Max(point - startPoint, result);
+            startPoint = point;
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/2. Middle/1637. Widest Vertical Area Between Two Points Containing No Points/Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1637._Widest_Vertical_Area_Between_Two_Points_Containing_No_Points;
+
+[TestFixture(TestName = "1637. Widest Vertical Area Between Two Points Containing No Points")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int[][] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MaxWidthOfVerticalArea(input);
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        var testCasesString = @"
+[[8,7],[9,9],[7,4],[9,7]]
+1
+
+[[3,1],[9,0],[1,0],[1,4],[5,3],[8,8]]
+3
+".Trim();
+
+        var testCases = testCasesString.Split(
+            Environment.NewLine + Environment.NewLine,
+            StringSplitOptions.RemoveEmptyEntries);
+        foreach (var testCase in testCases)
+        {
+            var lines = testCase.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            var input = lines[0].ParseIntArray2d();
+            var output = lines[1].ParseInt();
+            yield return new object[] { input, output };
+        }
+    }
+}
