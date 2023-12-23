800a4390e3c4f9ef4ec77fb22a8c0d03e9cb2dcb
hackerdmitry <hacker.dmitry@gmail.com>
2023-12-24 02:12:54 +0500

    1496. Path Crossing

---
+++ LeetCode/1. Easy/1496. Path Crossing/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._1._Easy._1496._Path_Crossing;
+
+public class Solution
+{
+    public bool IsPathCrossing(string path)
+    {
+        var lastPoint = (0, 0);
+        var hash = new HashSet<(int, int)> { lastPoint };
+        foreach (var direction in path)
+        {
+            var directionPoint = direction switch
+            {
+                'N' => (1, 0),
+                'S' => (-1, 0),
+                'W' => (0, -1),
+                'E' => (0, 1),
+            };
+            lastPoint = (lastPoint.Item1 + directionPoint.Item1,
+                         lastPoint.Item2 + directionPoint.Item2);
+            if (!hash.Add(lastPoint))
+                return true;
+        }
+
+        return false;
+    }
+}
---
+++ LeetCode/1. Easy/1496. Path Crossing/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1496._Path_Crossing;
+
+[TestFixture(TestName = "1496. Path Crossing")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("NES", false)]
+    [TestCase("NESWW", true)]
+    public void Test(string input, bool output)
+    {
+        var solution = new Solution();
+        var actual = solution.IsPathCrossing(input);
+        Assert.AreEqual(output, actual);
+    }
+}
