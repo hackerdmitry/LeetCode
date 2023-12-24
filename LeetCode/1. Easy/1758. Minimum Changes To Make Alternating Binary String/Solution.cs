9aa7a6725d7106ccf20d3df59d82368e155570d4
hackerdmitry <hacker.dmitry@gmail.com>
2023-12-24 18:52:36 +0500

    1758. Minimum Changes To Make Alternating Binary String

---
+++ LeetCode/1. Easy/1758. Minimum Changes To Make Alternating Binary String/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCode._1._Easy._1758._Minimum_Changes_To_Make_Alternating_Binary_String;
+
+public class Solution
+{
+    public int MinOperations(string s)
+    {
+        var v1 = 0;
+        var v2 = 0;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (i % 2 == 0 ^ s[i] == '0')
+                v1++;
+            else
+                v2++;
+        }
+
+        return Math.Min(v1, v2);
+    }
+}
---
+++ LeetCode/1. Easy/1758. Minimum Changes To Make Alternating Binary String/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1758._Minimum_Changes_To_Make_Alternating_Binary_String;
+
+[TestFixture(TestName = "1758. Minimum Changes To Make Alternating Binary String")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("0100", 1)]
+    [TestCase("10", 0)]
+    [TestCase("1111", 2)]
+    public void Test(string input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MinOperations(input);
+        Assert.AreEqual(output, actual);
+    }
+}
