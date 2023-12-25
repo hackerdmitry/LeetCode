254723e2416ff4743fbeddf47822b1e0f581125c
hackerdmitry <d.butov@skbkontur.ru>
2023-12-25 09:31:43 +0500

    91. Decode Ways

---
+++ LeetCode/2. Middle/91. Decode Ways/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode._2._Middle._91._Decode_Ways;
+
+public class Solution
+{
+    public int NumDecodings(string s)
+    {
+        if (s[0] == '0')
+            return 0;
+
+        var groups = new int[s.Length];
+        groups[0] = 1;
+
+        for (var i = 1; i < s.Length; i++)
+        {
+            if (s[i] != '0')
+                groups[i] = groups[i - 1];
+
+            if (s[i-1] != '0')
+            {
+                var twoDigit = (s[i - 1] - '0') * 10 + s[i] - '0';
+                if (twoDigit <= 26)
+                    groups[i] += i > 1 ? groups[i - 2] : 1;
+            }
+        }
+
+        return groups[^1];
+    }
+}
---
+++ LeetCode/2. Middle/91. Decode Ways/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._91._Decode_Ways;
+
+[TestFixture(TestName = "91. Decode Ways")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("12", 2)]
+    [TestCase("226", 3)]
+    [TestCase("06", 0)]
+    [TestCase("11106", 2)]
+    public void Test(string input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.NumDecodings(input);
+        Assert.AreEqual(output, actual);
+    }
+}
