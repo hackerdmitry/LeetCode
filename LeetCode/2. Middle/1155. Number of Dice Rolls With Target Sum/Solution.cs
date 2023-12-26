191efea5ecb189053e527ad9a2ac62fc96fa08f0
hackerdmitry <d.butov@skbkontur.ru>
2023-12-26 13:13:58 +0500

    1155. Number of Dice Rolls With Target Sum

---
+++ LeetCode/2. Middle/1155. Number of Dice Rolls With Target Sum/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCode._2._Middle._1155._Number_of_Dice_Rolls_With_Target_Sum;
+
+public class Solution
+{
+    private const int module = 1_000_000_007;
+
+    public int NumRollsToTarget(int n, int k, int target)
+    {
+        if (n * k < target)
+            return 0;
+
+        var n1 = new int[target];
+        for (var i = Math.Max(0, target - k); i < target; i++)
+            n1[i] = 1;
+
+        var lastN1 = target - 1;
+        var queue = new Queue<int>(k * 2 - 1);
+
+        for (var i = 1; i < n; i++)
+        {
+            var sum = (long)n1[lastN1];
+            queue.Enqueue(n1[lastN1]);
+            n1[lastN1] = 0;
+
+            for (var j = lastN1 - 1; queue.Count > 0 && j >= 0; j--)
+            {
+                var prevValue = n1[j];
+                n1[j] = (int)(sum % module);
+
+                if (queue.Count == k)
+                    sum -= queue.Dequeue();
+
+                if (n1[j] != 0)
+                {
+                    sum += prevValue;
+                    queue.Enqueue(prevValue);
+                }
+            }
+
+            queue.Clear();
+            lastN1--;
+        }
+
+        return n1[0];
+    }
+}
---
+++ LeetCode/2. Middle/1155. Number of Dice Rolls With Target Sum/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1155._Number_of_Dice_Rolls_With_Target_Sum;
+
+[TestFixture(TestName = "1155. Number of Dice Rolls With Target Sum")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(1, 6, 3, 1)]
+    [TestCase(2, 6, 7, 6)]
+    [TestCase(30, 30, 500, 222616187)]
+    [TestCase(4, 6, 18, 80)]
+    public void Test(int input1, int input2, int input3, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.NumRollsToTarget(input1, input2, input3);
+        Assert.AreEqual(output, actual);
+    }
+}
