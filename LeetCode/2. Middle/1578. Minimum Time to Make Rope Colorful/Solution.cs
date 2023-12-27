523361649f56706266eaf948dbb36ac34e3e034f
hackerdmitry <d.butov@skbkontur.ru>
2023-12-27 09:58:54 +0500

    1578. Minimum Time to Make Rope Colorful

---
+++ LeetCode/2. Middle/1578. Minimum Time to Make Rope Colorful/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode._2._Middle._1578._Minimum_Time_to_Make_Rope_Colorful;
+
+public class Solution
+{
+    public int MinCost(string colors, int[] neededTime)
+    {
+        var result = 0;
+        for (var i = 1; i < colors.Length; i++)
+            if (colors[i] == colors[i - 1])
+            {
+                var sumTime = neededTime[i] + neededTime[i - 1];
+                var maxTime = Math.Max(neededTime[i], neededTime[i - 1]);
+                var lastJ = i + 1;
+                for (var j = i + 1; j < colors.Length && colors[j] == colors[i]; j++)
+                {
+                    maxTime = Math.Max(maxTime, neededTime[j]);
+                    sumTime += neededTime[j];
+                    lastJ = j;
+                }
+
+                result += sumTime - maxTime;
+                i = lastJ;
+            }
+
+        return result;
+    }
+}
---
+++ LeetCode/2. Middle/1578. Minimum Time to Make Rope Colorful/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1578._Minimum_Time_to_Make_Rope_Colorful;
+
+[TestFixture(TestName = "1578. Minimum Time to Make Rope Colorful")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("abaac", new[]{1,2,3,4,5}, 3)]
+    [TestCase("abc", new[]{1,2,3}, 0)]
+    [TestCase("aabaa", new[]{1,2,3,4,1}, 2)]
+    [TestCase("bbbaaa", new[]{4,9,3,8,8,9}, 23)]
+    public void Test(string input1, int[] input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MinCost(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
