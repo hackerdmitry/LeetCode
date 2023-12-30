67c6c63f195d455d770d6cd83801e856c74a60d8
hackerdmitry <hacker.dmitry@gmail.com>
2023-12-30 23:04:00 +0500

    1897. Redistribute Characters to Make All Strings Equal

---
+++ LeetCode/1. Easy/1897. Redistribute Characters to Make All Strings Equal/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace LeetCode._1._Easy._1897._Redistribute_Characters_to_Make_All_Strings_Equal;
+
+public class Solution
+{
+    public bool MakeEqual(string[] words)
+    {
+        var letters = new int[26];
+        foreach (var word in words)
+            foreach (var letter in word)
+                letters[letter - 'a']++;
+
+        for (var i = 0; i < 26; i++)
+            if (letters[i] != 0 && letters[i] % words.Length != 0)
+                return false;
+
+        return true;
+    }
+}
---
+++ LeetCode/1. Easy/1897. Redistribute Characters to Make All Strings Equal/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1897._Redistribute_Characters_to_Make_All_Strings_Equal;
+
+[TestFixture(TestName = "1897. Redistribute Characters to Make All Strings Equal")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{"abc","aabc","bc"}, true)]
+    [TestCase(new[]{"ab","a"}, false)]
+    [TestCase(new[]{"a","a","a"}, true)]
+    [TestCase(new[]{"a","b"}, false)]
+    public void Test(string[] input, bool output)
+    {
+        var solution = new Solution();
+        var actual = solution.MakeEqual(input);
+        Assert.AreEqual(output, actual);
+    }
+}
