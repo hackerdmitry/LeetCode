ac8c386e8aa456101683ac1fc1a9c22eb53a9418
hackerdmitry <hacker.dmitry@gmail.com>
2023-12-31 11:31:59 +0500

    1624. Largest Substring Between Two Equal Characters

---
+++ LeetCode/1. Easy/1624. Largest Substring Between Two Equal Characters/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode._1._Easy._1624._Largest_Substring_Between_Two_Equal_Characters;
+
+public class Solution
+{
+    public int MaxLengthBetweenEqualCharacters(string s)
+    {
+        var lettersStart = new int[26];
+        var lettersEnd = new int[26];
+
+        for (var i = 0; i < s.Length; i++)
+            if (lettersStart[s[i] - 'a'] == 0)
+                lettersStart[s[i] - 'a'] = i + 1;
+
+        for (int i = s.Length - 1; i >= 0; i--)
+            if (lettersEnd[s[i] - 'a'] == 0)
+                lettersEnd[s[i] - 'a'] = i + 1;
+
+        var maxLength = -1;
+        for (var i = 0; i < 26; i++)
+            if (lettersEnd[i] != lettersStart[i])
+                maxLength = Math.Max(lettersEnd[i] - lettersStart[i] - 1, maxLength);
+
+        return maxLength;
+    }
+}
---
+++ LeetCode/1. Easy/1624. Largest Substring Between Two Equal Characters/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1624._Largest_Substring_Between_Two_Equal_Characters;
+
+[TestFixture(TestName = "1624. Largest Substring Between Two Equal Characters")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("aa", 0)]
+    [TestCase("abca", 2)]
+    [TestCase("cbzxy", -1)]
+    [TestCase("mgntdygtxrvxjnwksqhxuxtrv", 18)]
+    public void Test(string input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MaxLengthBetweenEqualCharacters(input);
+        Assert.AreEqual(output, actual);
+    }
+}
