c8ccac847f82a82a5e40f3be732d2205a89f334d
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-01 20:42:17 +0500

    455. Assign Cookies

---
+++ LeetCode/1. Easy/455. Assign Cookies/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace LeetCode._1._Easy._455._Assign_Cookies;
+
+public class Solution
+{
+    public int FindContentChildren(int[] g, int[] s)
+    {
+        g = g.OrderBy(x => x).ToArray();
+        s = s.OrderBy(x => x).ToArray();
+
+        var result = 0;
+
+        var si = 0;
+        for (var gi = 0; gi < g.Length; gi++)
+        {
+            while (si < s.Length && s[si] < g[gi])
+                si++;
+
+            if (si == s.Length)
+                break;
+
+            result++;
+            si++;
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/1. Easy/455. Assign Cookies/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._455._Assign_Cookies;
+
+[TestFixture(TestName = "455. Assign Cookies")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{1,2,3}, new[]{1,1}, 1)]
+    [TestCase(new[]{1,2}, new[]{1,2,3}, 2)]
+    [TestCase(new[]{1,2,3}, new[]{3}, 1)]
+    public void Test(int[] input1, int[] input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.FindContentChildren(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
