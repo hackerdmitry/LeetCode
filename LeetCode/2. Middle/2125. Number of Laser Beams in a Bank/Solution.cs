3407e6678733cdb0692e5356ee76729e993a1fb8
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-03 13:20:09 +0500

    2125. Number of Laser Beams in a Bank

---
+++ LeetCode/2. Middle/2125. Number of Laser Beams in a Bank/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2._Middle._2125._Number_of_Laser_Beams_in_a_Bank;
+
+public class Solution
+{
+    public int NumberOfBeams(string[] bank)
+    {
+        var signalList = new List<int>();
+        foreach (var row in bank)
+        {
+            var count = row.Count(x => x == '1');
+            if (count != 0)
+                signalList.Add(count);
+        }
+
+        var result = signalList.Count > 1 ? signalList[0] * signalList[1] : 0;
+        for (var i = 2; i < signalList.Count; i++)
+            result += signalList[i] * signalList[i - 1];
+
+        return result;
+    }
+}
---
+++ LeetCode/2. Middle/2125. Number of Laser Beams in a Bank/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._2125._Number_of_Laser_Beams_in_a_Bank;
+
+[TestFixture(TestName = "2125. Number of Laser Beams in a Bank")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{"011001","000000","010100","001000"}, 8)]
+    [TestCase(new[]{"000","111","000"}, 0)]
+    public void Test(string[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.NumberOfBeams(input);
+        Assert.AreEqual(output, actual);
+    }
+}
