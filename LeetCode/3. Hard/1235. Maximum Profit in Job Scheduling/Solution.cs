73f9df742c549c6831100860617ad69949cc997d
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-06 22:25:09 +0500

    1235. Maximum Profit in Job Scheduling

---
+++ LeetCode/3. Hard/1235. Maximum Profit in Job Scheduling/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode._3._Hard._1235._Maximum_Profit_in_Job_Scheduling;
+
+public class Solution
+{
+    public int JobScheduling(int[] startTime, int[] endTime, int[] profit)
+    {
+        var length = startTime.Length;
+
+        var bricks = Enumerable.Range(0, length)
+            .Select(i => new Brick(startTime[i], endTime[i], profit[i]))
+            .OrderBy(x => x.EndTime)
+            .ToArray();
+        var bestProfits = new BestProfit[length];
+
+        var bestProfit = 0;
+        for (var i = 0; i < length; i++)
+        {
+            var brick = bricks[i];
+            var maxProfit = FindMaxProfit(brick.StartTime, i, bestProfits);
+            bestProfits[i] = new BestProfit(brick.EndTime, Math.Max(maxProfit + brick.Profit, bestProfit));
+            if (bestProfits[i].Profit > bestProfit)
+                bestProfit = bestProfits[i].Profit;
+        }
+
+        return bestProfit;
+    }
+
+    private int FindMaxProfit(int startTime, int endIndex, BestProfit[] bestProfits)
+    {
+        var maxProfit = 0;
+        var left = 0;
+        var right = endIndex;
+
+        while (left < right)
+        {
+            var middle = (left + right) / 2;
+
+            if (bestProfits[middle].StartTime <= startTime &&
+                bestProfits[middle].Profit > maxProfit)
+                maxProfit = bestProfits[middle].Profit;
+
+            if (bestProfits[middle].StartTime > startTime)
+                right = middle;
+            else if (left == middle)
+                left++;
+            else
+                left = middle;
+        }
+
+        return maxProfit;
+    }
+
+    private record Brick(int StartTime, int EndTime, int Profit);
+
+    private record BestProfit(int StartTime, int Profit);
+}
---
+++ LeetCode/3. Hard/1235. Maximum Profit in Job Scheduling/Tests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._3._Hard._1235._Maximum_Profit_in_Job_Scheduling;
+
+[TestFixture(TestName = "1235. Maximum Profit in Job Scheduling")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int[] input1, int[] input2, int[] input3, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.JobScheduling(input1, input2, input3);
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        var testCasesString = @"
+[1,2,3,3]
+[3,4,5,6]
+[50,10,40,70]
+120
+
+[1,2,3,4,6]
+[3,5,10,6,9]
+[20,20,100,70,60]
+150
+
+[1,1,1]
+[2,3,4]
+[5,6,4]
+6
+
+[24,24,7,2,1,13,6,14,18,24]
+[27,27,20,7,14,22,20,24,19,27]
+[6,1,4,2,3,6,5,6,9,8]
+20
+".Trim();
+
+        var testCases = testCasesString.Split(
+            Environment.NewLine + Environment.NewLine,
+            StringSplitOptions.RemoveEmptyEntries);
+        foreach (var testCase in testCases)
+        {
+            var lines = testCase.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            var input1 = lines[0].ParseIntArray();
+            var input2 = lines[1].ParseIntArray();
+            var input3 = lines[2].ParseIntArray();
+            var output = lines[3].ParseInt();
+            yield return new object[] { input1, input2, input3, output };
+        }
+    }
+}
