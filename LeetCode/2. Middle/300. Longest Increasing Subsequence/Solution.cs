e7944144bf3537cc19f36e78f849450c3a54cd7a
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-06 21:41:26 +0500

    300. Longest Increasing Subsequence

---
+++ LeetCode/2. Middle/300. Longest Increasing Subsequence/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode._2._Middle._300._Longest_Increasing_Subsequence;
+
+public class Solution
+{
+    public int LengthOfLIS(int[] nums)
+    {
+        var counts = new int[nums.Length];
+        var result = 0;
+
+        for (var i = 0; i < counts.Length; i++)
+        {
+            var max = -1;
+            for (var j = 0; j < i; j++)
+                if (nums[j] < nums[i] && counts[j] > max)
+                    max = counts[j];
+
+            if (max == -1)
+                counts[i] = 1;
+            else
+                counts[i] = max + 1;
+
+            result = Math.Max(counts[i], result);
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/2. Middle/300. Longest Increasing Subsequence/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._300._Longest_Increasing_Subsequence;
+
+[TestFixture(TestName = "300. Longest Increasing Subsequence")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{10,9,2,5,3,7,101,18}, 4)]
+    [TestCase(new[]{0,1,0,3,2,3}, 4)]
+    [TestCase(new[]{7,7,7,7,7,7,7}, 1)]
+    public void Test(int[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.LengthOfLIS(input);
+        Assert.AreEqual(output, actual);
+    }
+}
