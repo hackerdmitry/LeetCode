0ddfebe99a0a3d416f29cfbc34ead2e78ffd2f2c
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-09 17:23:59 +0500

    938. Range Sum of BST

---
+++ LeetCode/1. Easy/938. Range Sum of BST/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LeetCode.__TreeNode;
+
+namespace LeetCode._1._Easy._938._Range_Sum_of_BST;
+
+public class Solution
+{
+    public int RangeSumBST(TreeNode root, int low, int high)
+    {
+        var sum = 0;
+
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            if (low <= node.val && node.val <= high)
+                sum += node.val;
+            if (node.left != null)
+                queue.Enqueue(node.left);
+            if (node.right != null)
+                queue.Enqueue(node.right);
+        }
+
+        return sum;
+    }
+}
---
+++ LeetCode/1. Easy/938. Range Sum of BST/Tests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using LeetCode.__TreeNode;
+using NUnit.Framework;
+
+namespace LeetCode._1._Easy._938._Range_Sum_of_BST;
+
+[TestFixture(TestName = "938. Range Sum of BST")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int?[] input1, int input2, int input3, int output)
+    {
+        var solution = new Solution();
+        var treeRoot = TreeNode.CreateFromArray(input1);
+        var actual = solution.RangeSumBST(treeRoot, input2, input3);
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        yield return new object[]
+        {
+            new int?[]{10,5,15,3,7,null,18}, 7, 15,
+            32,
+        };
+        yield return new object[]
+        {
+            new int?[]{10,5,15,3,7,13,18,1,null,6}, 6, 10,
+            23,
+        };
+    }
+}
