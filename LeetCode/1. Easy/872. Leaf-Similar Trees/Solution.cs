ab5632b1f29e4681c5743d7596960cbe9f96e6c6
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-09 17:34:48 +0500

    872. Leaf-Similar Trees

---
+++ LeetCode/1. Easy/872. Leaf-Similar Trees/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeetCode.__TreeNode;
+
+namespace LeetCode._1._Easy._872._Leaf_Similar_Trees;
+
+public class Solution
+{
+    public bool LeafSimilar(TreeNode root1, TreeNode root2)
+    {
+        var leafListRoot1 = new List<int>();
+        FillLeafList(root1, leafListRoot1);
+        var leafListRoot2 = new List<int>();
+        FillLeafList(root2, leafListRoot2);
+
+        return leafListRoot1.Count == leafListRoot2.Count &&
+               Enumerable.Range(0, leafListRoot1.Count).All(i => leafListRoot1[i] == leafListRoot2[i]);
+    }
+
+    private void FillLeafList(TreeNode node, List<int> leafList)
+    {
+        if (node.left == null && node.right == null)
+        {
+            leafList.Add(node.val);
+            return;
+        }
+
+        if (node.left != null)
+            FillLeafList(node.left, leafList);
+
+        if (node.right != null)
+            FillLeafList(node.right, leafList);
+    }
+}
---
+++ LeetCode/1. Easy/872. Leaf-Similar Trees/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using LeetCode.__TreeNode;
+using NUnit.Framework;
+
+namespace LeetCode._1._Easy._872._Leaf_Similar_Trees;
+
+[TestFixture(TestName = "872. Leaf-Similar Trees")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int?[] input1, int?[] input2, bool output)
+    {
+        var solution = new Solution();
+        var treeRoot1 = TreeNode.CreateFromArray(input1);
+        var treeRoot2 = TreeNode.CreateFromArray(input2);
+        var actual = solution.LeafSimilar(treeRoot1, treeRoot2);
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        yield return new object[]
+        {
+            new int?[]{3,5,1,6,2,9,8,null,null,7,4},
+            new int?[]{3,5,1,6,7,4,2,null,null,null,null,null,null,9,8},
+            true,
+        };
+        yield return new object[]
+        {
+            new int?[]{1,2,3},
+            new int?[]{1,3,2},
+            false,
+        };
+    }
+}
