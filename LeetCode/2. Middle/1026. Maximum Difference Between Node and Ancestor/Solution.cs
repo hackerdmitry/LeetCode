867ad0f54ecbeeb53d2fd99bb9b90f4367a4ef03
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-11 21:29:38 +0500

    1026. Maximum Difference Between Node and Ancestor

---
+++ LeetCode/2. Middle/1026. Maximum Difference Between Node and Ancestor/Solution.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.__TreeNode;
+
+namespace LeetCode._2._Middle._1026._Maximum_Difference_Between_Node_and_Ancestor;
+
+public class Solution
+{
+    public int MaxAncestorDiff(TreeNode root)
+    {
+        var (min, max) = FindMaxMinInNode(root, int.MaxValue, int.MinValue);
+        return max - min;
+    }
+
+    private (int, int) FindMaxMinInNode(TreeNode node, int min, int max)
+    {
+        if (node.val < min)
+            min = node.val;
+
+        if (node.val > max)
+            max = node.val;
+
+        var newMinMax = (min, max);
+
+        if (node.left != null)
+        {
+            var tempMinMax = FindMaxMinInNode(node.left, min, max);
+            if (tempMinMax.Item2 - tempMinMax.Item1 > newMinMax.Item2 - newMinMax.Item1)
+                newMinMax = tempMinMax;
+        }
+
+        if (node.right != null)
+        {
+            var tempMinMax = FindMaxMinInNode(node.right, min, max);
+            if (tempMinMax.Item2 - tempMinMax.Item1 > newMinMax.Item2 - newMinMax.Item1)
+                newMinMax = tempMinMax;
+        }
+
+        return newMinMax;
+    }
+}
---
+++ LeetCode/2. Middle/1026. Maximum Difference Between Node and Ancestor/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using LeetCode.__TreeNode;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1026._Maximum_Difference_Between_Node_and_Ancestor;
+
+[TestFixture(TestName = "1026. Maximum Difference Between Node and Ancestor")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int?[] input, int output)
+    {
+        var solution = new Solution();
+
+        var treeRoot = TreeNode.CreateFromArray(input);
+        var actual = solution.MaxAncestorDiff(treeRoot);
+
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        yield return new object[]
+        {
+            new int?[]{8,3,10,1,6,null,14,null,null,4,7,13},
+            7,
+        };
+        yield return new object[]
+        {
+            new int?[]{1,null,2,null,0,3},
+            3,
+        };
+    }
+}
