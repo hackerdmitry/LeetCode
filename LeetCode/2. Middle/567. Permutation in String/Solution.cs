7188f1f56ec932d3840f09b8d406cf264cfb84b1
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-12 15:23:09 +0500

    560. Subarray Sum Equals K

---
+++ LeetCode/2. Middle/567. Permutation in String/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2._Middle._567._Permutation_in_String;
+
+public class Solution
+{
+    public bool CheckInclusion(string s1, string s2)
+    {
+        var letters = s1.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+        var permutation = new Queue<char>();
+
+        for (var i = 0; i < s2.Length; i++)
+        {
+            var curLetter = s2[i];
+            if (letters.ContainsKey(curLetter))
+            {
+                 if (letters[curLetter] == 0)
+                     ClearPermutation(letters, permutation, curLetter);
+                 permutation.Enqueue(curLetter);
+                 letters[curLetter]--;
+            }
+            else
+                ClearPermutation(letters, permutation);
+
+            if (permutation.Count == s1.Length)
+                return true;
+        }
+
+        return false;
+    }
+
+    private void ClearPermutation(Dictionary<char, int> letters, Queue<char> permutation, char? stopLetter = null)
+    {
+        while (permutation.Count > 0)
+        {
+            var letter = permutation.Dequeue();
+            letters[letter]++;
+            if (letter == stopLetter)
+                break;
+        }
+    }
+}
---
+++ LeetCode/2. Middle/567. Permutation in String/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._567._Permutation_in_String;
+
+[TestFixture(TestName = "560. Subarray Sum Equals K")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("ab", "eidbaooo", true)]
+    [TestCase("ab", "eidboaoo", false)]
+    public void Test(string input1, string input2, bool output)
+    {
+        var solution = new Solution();
+        var actual = solution.CheckInclusion(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
