6de6384e7d5c0ed77b2abaac97f327474bae577c
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-12 15:50:39 +0500

    605. Can Place Flowers

---
+++ LeetCode/1. Easy/605. Can Place Flowers/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode._1._Easy._605._Can_Place_Flowers;
+
+public class Solution
+{
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        var possibleCount = 0;
+
+        if (flowerbed.Length == 1 && flowerbed[0] == 0)
+        {
+            flowerbed[0] = 1;
+            possibleCount++;
+        }
+
+        if (flowerbed.Length > 1)
+        {
+            if (flowerbed[0] == 0 && flowerbed[1] == 0)
+            {
+                flowerbed[0] = 1;
+                possibleCount++;
+            }
+
+            if (flowerbed[^1] == 0 && flowerbed[^2] == 0)
+            {
+                flowerbed[^1] = 1;
+                possibleCount++;
+            }
+        }
+
+        for (var i = 1; i < flowerbed.Length - 1; i++)
+        {
+            if (flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0)
+            {
+                possibleCount++;
+                flowerbed[i] = 1;
+            }
+        }
+
+        return possibleCount >= n;
+    }
+}
---
+++ LeetCode/1. Easy/605. Can Place Flowers/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._605._Can_Place_Flowers;
+
+[TestFixture(TestName = "605. Can Place Flowers")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{1,0,0,0,1}, 1, true)]
+    [TestCase(new[]{1,0,0,0,1}, 2, false)]
+    [TestCase(new[]{0,0,1,0,1}, 1, true)]
+    [TestCase(new[]{1,0,0,0,0,1}, 2, false)]
+    [TestCase(new[]{0}, 1, true)]
+    public void Test(int[] input1, int input2, bool output)
+    {
+        var solution = new Solution();
+        var actual = solution.CanPlaceFlowers(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
