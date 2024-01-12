14e23d73399dfefa4805c7cefc24966b5822927a
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-12 16:02:28 +0500

    1704. Determine if String Halves Are Alike

---
+++ LeetCode/1. Easy/1704. Determine if String Halves Are Alike/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._1._Easy._1704._Determine_if_String_Halves_Are_Alike;
+
+public class Solution
+{
+    public bool HalvesAreAlike(string s)
+    {
+        var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+        var a = Enumerable.Range(0, s.Length / 2).Count(i => vowels.Contains(s[i]));
+        var b = Enumerable.Range(s.Length / 2, s.Length / 2).Count(i => vowels.Contains(s[i]));
+        return a == b;
+    }
+}
---
+++ LeetCode/1. Easy/1704. Determine if String Halves Are Alike/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1704._Determine_if_String_Halves_Are_Alike;
+
+[TestFixture(TestName = "1704. Determine if String Halves Are Alike")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("book", true)]
+    [TestCase("textbook", false)]
+    public void Test(string input, bool output)
+    {
+        var solution = new Solution();
+        var actual = solution.HalvesAreAlike(input);
+        Assert.AreEqual(output, actual);
+    }
+}
