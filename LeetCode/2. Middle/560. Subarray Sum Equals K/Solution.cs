318b6c38706832598bef6a2f400c8c01a3b2f444
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-12 15:08:24 +0500

    560. Subarray Sum Equals K

---
+++ LeetCode/2. Middle/560. Subarray Sum Equals K/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2._Middle._560._Subarray_Sum_Equals_K;
+
+public class Solution
+{
+    public int SubarraySum(int[] nums, int k)
+    {
+        var count = 0;
+        var sum = 0;
+        var dict = new Dictionary<int, int> { { 0, 1 } };
+        foreach (var num in nums)
+        {
+            sum += num;
+            if (dict.ContainsKey(sum - k))
+                count += dict[sum - k];
+            if (dict.ContainsKey(sum))
+                dict[sum]++;
+            else
+                dict.Add(sum, 1);
+        }
+
+        return count;
+    }
+}
---
+++ LeetCode/2. Middle/560. Subarray Sum Equals K/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._560._Subarray_Sum_Equals_K;
+
+[TestFixture(TestName = "560. Subarray Sum Equals K")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{1,1,1}, 2, 2)]
+    [TestCase(new[]{1,2,3}, 3, 2)]
+    [TestCase(new[]{1,1,1,1,1}, 3, 3)]
+    [TestCase(new[]{1,-1,0}, 0, 3)]
+    [TestCase(new[]{0,0}, 0, 3)]
+    public void Test(int[] input1, int input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.SubarraySum(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
