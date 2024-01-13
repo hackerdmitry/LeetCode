a19ebbf83ae7788590aad2a6ad9d8dd91b90b857
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-13 16:33:06 +0500

    1347. Minimum Number of Steps to Make Two Strings Anagram

---
+++ LeetCode/2. Middle/1347. Minimum Number of Steps to Make Two Strings Anagram/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode._2._Middle._1347._Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram;
+
+public class Solution
+{
+    public int MinSteps(string s, string t)
+    {
+        var sLetters = s.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+        var tLetters = t.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+        var diffSum = Enumerable.Range('a', 26).Sum(x =>
+        {
+            var c = (char)x;
+            sLetters.TryGetValue(c, out var sCount);
+            tLetters.TryGetValue(c, out var tCount);
+            return Math.Abs(sCount - tCount);
+        });
+
+        return diffSum / 2;
+    }
+}
---
+++ LeetCode/2. Middle/1347. Minimum Number of Steps to Make Two Strings Anagram/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1347._Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram;
+
+[TestFixture(TestName = "1347. Minimum Number of Steps to Make Two Strings Anagram")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("bab", "aba", 1)]
+    [TestCase("leetcode", "practice", 5)]
+    [TestCase("anagram", "mangaar", 0)]
+    public void Test(string input1, string input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MinSteps(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
