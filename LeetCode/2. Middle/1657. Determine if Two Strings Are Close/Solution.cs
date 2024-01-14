19d7817ee8d15ba32c1512323509a19b390eed2d
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-14 21:31:03 +0500

    1657. Determine if Two Strings Are Close

---
+++ LeetCode/2. Middle/1657. Determine if Two Strings Are Close/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode._2._Middle._1657._Determine_if_Two_Strings_Are_Close;
+
+public class Solution
+{
+    public bool CloseStrings(string word1, string word2)
+    {
+        var letters1 = new bool[26];
+        var counts1 = new int[26];
+        foreach (var x in word1.Select(c => c - 'a'))
+        {
+            letters1[x] = true;
+            counts1[x]++;
+        }
+
+        var letters2 = new bool[26];
+        var counts2 = new int[26];
+        foreach (var x in word2.Select(c => c - 'a'))
+        {
+            letters2[x] = true;
+            counts2[x]++;
+        }
+
+        Array.Sort(counts1);
+        Array.Sort(counts2);
+
+        return Enumerable.Range(0, 26).All(i => letters1[i] == letters2[i]) &&
+               Enumerable.Range(0, 26).All(i => counts1[i] == counts2[i]);
+    }
+}
---
+++ LeetCode/2. Middle/1657. Determine if Two Strings Are Close/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1657._Determine_if_Two_Strings_Are_Close;
+
+[TestFixture(TestName = "1657. Determine if Two Strings Are Close")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("abc","bca", true)]
+    [TestCase("a", "aa", false)]
+    [TestCase("cabbba", "abbccc", true)]
+    [TestCase("uau", "ssx", false)]
+    [TestCase("abbzccca", "babzzczc", true)]
+    public void Test(string input1, string input2, bool output)
+    {
+        var solution = new Solution();
+        var actual = solution.CloseStrings(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
