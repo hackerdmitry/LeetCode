4be9361459ba704c4a6ea2ea395aa5ffdcd23f91
hackerdmitry <d.butov@skbkontur.ru>
2024-01-17 18:59:04 +0500

    1207. Unique Number of Occurrences

---
+++ LeetCode/1. Easy/1207. Unique Number of Occurrences/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._1._Easy._1207._Unique_Number_of_Occurrences;
+
+public class Solution
+{
+    public bool UniqueOccurrences(int[] arr)
+    {
+        var dict = new Dictionary<int, int>();
+        foreach (var i in arr)
+        {
+            if (!dict.ContainsKey(i))
+                dict[i] = 1;
+            else
+                dict[i]++;
+        }
+
+        var hash = new HashSet<int>();
+        foreach (var (_, value) in dict)
+            if (!hash.Add(value))
+                return false;
+
+        return true;
+    }
+}
---
+++ LeetCode/1. Easy/1207. Unique Number of Occurrences/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._1207._Unique_Number_of_Occurrences;
+
+[TestFixture(TestName = "1207. Unique Number of Occurrences")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{1,2,2,1,1,3}, true)]
+    [TestCase(new[]{1,2}, false)]
+    [TestCase(new[]{-3,0,1,-3,1,1,1,-3,10,0}, true)]
+    public void Test(int[] input, bool output)
+    {
+        var solution = new Solution();
+        var actual = solution.UniqueOccurrences(input);
+        Assert.AreEqual(output, actual);
+    }
+}
