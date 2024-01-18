0e864bd00460b2b5272c19114cc025dc97b3f92b
hackerdmitry <d.butov@skbkontur.ru>
2024-01-18 16:09:56 +0500

    70. Climbing Stairs

---
+++ LeetCode/1. Easy/70. Climbing Stairs/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode._1._Easy._70._Climbing_Stairs;
+
+public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        var a = 1;
+        var b = 1;
+        var res = 1;
+        while (n > 1)
+        {
+            res = a + b;
+            b = a;
+            a = res;
+            n--;
+        }
+
+        return res;
+    }
+}
---
+++ LeetCode/1. Easy/70. Climbing Stairs/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._70._Climbing_Stairs;
+
+[TestFixture(TestName = "70. Climbing Stairs")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(2, 2)]
+    [TestCase(3, 3)]
+    [TestCase(45, 1836311903)]
+    public void Test(int input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.ClimbStairs(input);
+        Assert.AreEqual(output, actual);
+    }
+}
