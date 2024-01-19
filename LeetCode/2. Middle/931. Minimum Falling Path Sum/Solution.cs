7ca3a32f116f80dcc3e7e310a8774496d3ce38df
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-20 01:32:26 +0500

    931. Minimum Falling Path Sum

---
+++ LeetCode/2. Middle/931. Minimum Falling Path Sum/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode._2._Middle._931._Minimum_Falling_Path_Sum;
+
+public class Solution
+{
+    public int MinFallingPathSum(int[][] matrix)
+    {
+        var dp = new int[matrix[0].Length];
+        for (int i = 0; i < dp.Length; i++)
+            dp[i] = matrix[0][i];
+        var helpDP = new int[dp.Length];
+        for (int j = 1; j < matrix.Length; j++)
+        {
+            for (int i = 0; i < dp.Length; i++)
+                helpDP[i] = dp[i];
+
+            for (int i = 0; i < dp.Length; i++)
+                dp[i] = matrix[j][i] +
+                        (i == 0
+                            ? Math.Min(helpDP[0], helpDP[1])
+                            : i == dp.Length - 1
+                                ? Math.Min(helpDP[^2], helpDP[^1])
+                                : Math.Min(helpDP[i - 1], Math.Min(helpDP[i], helpDP[i + 1])));
+        }
+
+        return dp.Min();
+    }
+}
---
+++ LeetCode/2. Middle/931. Minimum Falling Path Sum/Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._931._Minimum_Falling_Path_Sum;
+
+[TestFixture(TestName = "931. Minimum Falling Path Sum")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int[][] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MinFallingPathSum(input);
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        var testCasesString = @"
+[[2,1,3],[6,5,4],[7,8,9]]
+13
+
+[[-19,57],[-40,-5]]
+-59
+
+[[1,1,1],[2,1,1],[2,2,1],[2,9,9]]
+6
+".Trim();
+
+        var testCases = testCasesString.Split(
+            Environment.NewLine + Environment.NewLine,
+            StringSplitOptions.RemoveEmptyEntries);
+        foreach (var testCase in testCases)
+        {
+            var lines = testCase.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            var input = lines[0].ParseIntArray2d();
+            var output = lines[1].ParseInt();
+            yield return new object[] { input, output };
+        }
+    }
+}
