8ff4771d7514a4d5b585208a9d7b6db9563ec57c
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-21 21:11:59 +0500

    198. House Robber

---
+++ LeetCode/2. Middle/198. House Robber/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode._2._Middle._198._House_Robber;
+
+public class Solution
+{
+    public int Rob(int[] nums)
+    {
+        var dp = new int[nums.Length];
+        if (nums.Length > 0)
+            dp[0] = nums[0];
+        if (nums.Length > 1)
+            dp[1] = nums[1];
+        for (var i = 2; i < nums.Length; i++)
+            dp[i] = nums[i] +
+                    (i == 2
+                        ? dp[0]
+                        : Math.Max(dp[i - 2], dp[i - 3]));
+
+        return Math.Max(dp.LastOrDefault(0), dp.SkipLast(1).LastOrDefault(0));
+    }
+}
---
+++ LeetCode/2. Middle/198. House Robber/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._198._House_Robber;
+
+[TestFixture(TestName = "198. House Robber")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{1,2,3,1}, 4)]
+    [TestCase(new[]{2,7,9,3,1}, 12)]
+    [TestCase(new[]{1}, 1)]
+    [TestCase(new int[0], 0)]
+    public void Test(int[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.Rob(input);
+        Assert.AreEqual(output, actual);
+    }
+}
