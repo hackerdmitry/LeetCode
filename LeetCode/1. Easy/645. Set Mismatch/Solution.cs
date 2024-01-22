765b8ae7474193f156d61fdaa83a8ec48f1bd595
hackerdmitry <d.butov@skbkontur.ru>
2024-01-22 13:34:06 +0500

    645. Set Mismatch

---
+++ LeetCode/1. Easy/645. Set Mismatch/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+
+namespace LeetCode._1._Easy._645._Set_Mismatch;
+
+public class Solution
+{
+    public int[] FindErrorNums(int[] nums)
+    {
+        var duplicated = -1;
+
+        var counter = 0;
+        while (counter < nums.Length)
+        {
+            if (nums[counter] < 0)
+            {
+                counter++;
+                continue;
+            }
+
+            if (nums[counter] == counter + 1)
+            {
+                nums[counter] = -counter - 1;
+                counter++;
+                continue;
+            }
+
+            var index = nums[counter] - 1;
+            while (index != counter)
+            {
+                if (nums[index] == 0)
+                {
+                    nums[index] = -index - 1;
+                    break;
+                }
+
+                if (nums[index] < 0)
+                {
+                    duplicated = index + 1;
+                    break;
+                }
+
+                var temp = index;
+                index = nums[index] - 1;
+                nums[temp] = -temp - 1;
+            }
+
+            if (index == counter)
+                nums[index] = -index - 1;
+            else
+                nums[counter] = 0;
+            counter++;
+        }
+
+        return new[] { duplicated, Enumerable.Range(0, nums.Length).First(x => nums[x] == 0) + 1 };
+    }
+}
---
+++ LeetCode/1. Easy/645. Set Mismatch/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._645._Set_Mismatch;
+
+[TestFixture(TestName = "645. Set Mismatch")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{1,2,2,4}, new[]{2,3})]
+    [TestCase(new[]{1,1}, new[]{1,2})]
+    [TestCase(new[]{5,3,4,5,1}, new[]{5,2})]
+    [TestCase(new[]{8,7,3,5,3,6,1,4}, new[]{3,2})]
+    public void Test(int[] input, int[] output)
+    {
+        var solution = new Solution();
+        var actual = solution.FindErrorNums(input);
+        Assert.AreEqual(output, actual);
+    }
+}
