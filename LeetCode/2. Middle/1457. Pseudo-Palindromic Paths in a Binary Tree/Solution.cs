9e86b195ee99fb493363a82bfdb97f519509cbc5
hackerdmitry <hacker.dmitry@gmail.com>
2024-01-24 20:59:13 +0500

    1457. Pseudo-Palindromic Paths in a Binary Tree

---
+++ LeetCode/2. Middle/1457. Pseudo-Palindromic Paths in a Binary Tree/Solution.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using LeetCode.__TreeNode;
+
+namespace LeetCode._2._Middle._1457._Pseudo_Palindromic_Paths_in_a_Binary_Tree;
+
+public class Solution
+{
+    public int PseudoPalindromicPaths(TreeNode root)
+    {
+        var result = 0;
+
+        var queue = new Queue<(TreeNode, int[], int)>();
+
+        var rootValues = new int[9];
+        rootValues[root.val - 1]++;
+        queue.Enqueue((root, rootValues, 1));
+
+        while (queue.Count != 0)
+        {
+            var (node, values, count) = queue.Dequeue();
+
+            if (node.left != null)
+            {
+                if (node.right != null)
+                {
+                    var copyValues = new int[9];
+                    values.CopyTo(copyValues, 0);
+                    values[node.left.val - 1]++;
+                    copyValues[node.right.val - 1]++;
+                    queue.Enqueue((node.left, values, count + 1));
+                    queue.Enqueue((node.right, copyValues, count + 1));
+                }
+                else
+                {
+                    values[node.left.val - 1]++;
+                    queue.Enqueue((node.left, values, count + 1));
+                }
+            }
+            else
+            {
+                if (node.right != null)
+                {
+                    values[node.right.val - 1]++;
+                    queue.Enqueue((node.right, values, count + 1));
+                }
+                else
+                {
+                    if (IsPalindromic(values, count))
+                        result++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private bool IsPalindromic(int[] values, int count) =>
+        count % 2 == 0
+            ? values.All(x => x % 2 == 0)
+            : values.Count(x => x % 2 != 0) == 1;
+}
---
+++ LeetCode/2. Middle/1457. Pseudo-Palindromic Paths in a Binary Tree/Tests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using LeetCode.__TreeNode;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1457._Pseudo_Palindromic_Paths_in_a_Binary_Tree;
+
+[TestFixture(TestName = "1457. Pseudo-Palindromic Paths in a Binary Tree")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int?[] input, int output)
+    {
+        var solution = new Solution();
+        var treeRoot = TreeNode.CreateFromArray(input);
+        var actual = solution.PseudoPalindromicPaths(treeRoot);
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        yield return new object[]
+        {
+            new int?[]{2,3,1,3,1,null,1},
+            2
+        };
+        yield return new object[]
+        {
+            new int?[]{2,1,1,1,3,null,null,null,null,null,1},
+            1
+        };
+        yield return new object[]
+        {
+            new int?[]{9},
+            1
+        };
+        yield return new object[]
+        {
+            new int?[]{8,8,null,7,7,null,null,2,4,null,8,null,7,null,1},
+            2
+        };
+    }
+}
