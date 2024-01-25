cbe2006d9eebb31383ff75e4d4c104d64f5699cf
hackerdmitry <d.butov@skbkontur.ru>
2024-01-25 13:26:48 +0500

    1143. Longest Common Subsequence

---
+++ LeetCode/2. Middle/1143. Longest Common Subsequence/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LeetCode._2._Middle._1143._Longest_Common_Subsequence;
+
+public class Solution
+{
+    public int LongestCommonSubsequence(string text1, string text2)
+    {
+        var dp = new int[text1.Length];
+
+        foreach (var letter2 in text2)
+        {
+            var isUsed = false;
+            for (var i = 0; i < text1.Length; i++)
+            {
+                var prevDP = i == 0 ? 0 : dp[i - 1];
+
+                var letter1 = text1[i];
+
+                var var1 = !isUsed && letter1 == letter2
+                    ? prevDP + 1
+                    : prevDP;
+                var var2 = dp[i];
+
+                if (var1 > var2)
+                {
+                    isUsed = true;
+                    dp[i] = var1;
+                }
+                else
+                {
+                    isUsed = false;
+                    dp[i] = var2;
+                }
+            }
+        }
+
+        return dp[^1];
+    }
+}
---
+++ LeetCode/2. Middle/1143. Longest Common Subsequence/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1143._Longest_Common_Subsequence;
+
+[TestFixture(TestName = "1143. Longest Common Subsequence")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("abcde", "ace", 3)]
+    [TestCase("abc", "abc", 3)]
+    [TestCase("abc", "def", 0)]
+    [TestCase("acc", "acd", 2)]
+    [TestCase("jgtargjctqvijshexyjcjcre", "pyzazexujqtsjebcnadahobwf", 6)]
+    public void Test(string input1, string input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.LongestCommonSubsequence(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
