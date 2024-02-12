4cde5d996374fdb8b629f6d9f5dabacdd065e9fa
hackerdmitry <d.butov@skbkontur.ru>
2024-02-12 17:56:54 +0500

    169. Majority Element

---
+++ LeetCode/1. Easy/169. Majority Element/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode._1._Easy._169._Majority_Element;
+
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        var elem = nums[0];
+        var count = 1;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (count == 0)
+                elem = nums[i];
+
+            if (elem == nums[i])
+                count++;
+            else
+                count--;
+        }
+
+        return elem;
+    }
+}
---
+++ LeetCode/1. Easy/169. Majority Element/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._169._Majority_Element;
+
+[TestFixture(TestName = "169. Majority Element")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{3,2,3}, 3)]
+    [TestCase(new[]{2,2,1,1,1,2,2}, 2)]
+    public void Test(int[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MajorityElement(input);
+        Assert.AreEqual(output, actual);
+    }
+}
