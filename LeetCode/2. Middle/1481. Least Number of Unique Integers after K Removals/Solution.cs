aa2642039198ad56ed1ad49e99a78dadce474e11
hackerdmitry <d.butov@skbkontur.ru>
2024-02-16 13:17:54 +0500

    1481. Least Number of Unique Integers after K Removals

---
+++ LeetCode/2. Middle/1481. Least Number of Unique Integers after K Removals/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2._Middle._1481._Least_Number_of_Unique_Integers_after_K_Removals;
+
+public class Solution
+{
+    public int FindLeastNumOfUniqueInts(int[] arr, int k)
+    {
+        var dict = new Dictionary<int, int>();
+        foreach (var a in arr)
+            if (dict.ContainsKey(a))
+                dict[a]++;
+            else
+                dict[a] = 1;
+
+        var result = dict.Count;
+        foreach (var (_, count) in dict.OrderBy(x => x.Value))
+        {
+            k -= count;
+            if (k < 0)
+                break;
+
+            result--;
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/2. Middle/1481. Least Number of Unique Integers after K Removals/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1481._Least_Number_of_Unique_Integers_after_K_Removals;
+
+[TestFixture(TestName = "1481. Least Number of Unique Integers after K Removals")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{5,5,4}, 1, 1)]
+    [TestCase(new[]{4,3,1,1,3,3,2}, 3, 2)]
+    public void Test(int[] input1, int input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.FindLeastNumOfUniqueInts(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
