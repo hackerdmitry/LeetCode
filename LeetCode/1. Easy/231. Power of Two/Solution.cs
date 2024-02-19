3b051456385f954affa9ddb803fb8b7fbe1a1875
hackerdmitry <d.butov@skbkontur.ru>
2024-02-19 16:18:03 +0500

    231. Power of Two

---
+++ LeetCode/1. Easy/231. Power of Two/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode._1._Easy._231._Power_of_Two;
+
+public class Solution
+{
+    public bool IsPowerOfTwo(int n)
+    {
+        var k = 1 << 30;
+        while (k > 0)
+        {
+            if (n == k)
+                return true;
+            k >>= 1;
+        }
+
+        return false;
+    }
+}
---
+++ LeetCode/1. Easy/231. Power of Two/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._231._Power_of_Two;
+
+[TestFixture(TestName = "231. Power of Two")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(1, true)]
+    [TestCase(16, true)]
+    [TestCase(3, false)]
+    [TestCase(0, false)]
+    [TestCase(1 << 30, true)]
+    public void Test(int input, bool output)
+    {
+        var solution = new Solution();
+        var actual = solution.IsPowerOfTwo(input);
+        Assert.AreEqual(output, actual);
+    }
+}
