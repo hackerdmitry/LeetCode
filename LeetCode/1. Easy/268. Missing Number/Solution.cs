99f78944229b824ddd8c9904971429e9b03b27d2
hackerdmitry <d.butov@skbkontur.ru>
2024-02-20 15:45:01 +0500

    268. Missing Number

---
+++ LeetCode/1. Easy/268. Missing Number/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode._1._Easy._268._Missing_Number;
+
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        var unknown = nums.Length;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            while (nums[i] != i)
+            {
+                if (nums[i] == nums.Length)
+                {
+                    unknown = i;
+                    nums[i] = -1;
+                    break;
+                }
+
+                (nums[nums[i]], nums[i]) = (nums[i], nums[nums[i]]);
+                if (nums[i] == -1)
+                {
+                    unknown = i;
+                    break;
+                }
+            }
+        }
+
+        return unknown;
+    }
+}
---
+++ LeetCode/1. Easy/268. Missing Number/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._268._Missing_Number;
+
+[TestFixture(TestName = "268. Missing Number")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{3,0,1}, 2)]
+    [TestCase(new[]{0,1}, 2)]
+    [TestCase(new[]{9,6,4,2,3,5,7,0,1}, 8)]
+    [TestCase(new[]{2,0,1}, 3)]
+    public void Test(int[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MissingNumber(input);
+        Assert.AreEqual(output, actual);
+    }
+}
