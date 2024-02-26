bf2d567e235b290040e3d09578817600acdf1638
hackerdmitry <d.butov@skbkontur.ru>
2024-02-26 16:59:44 +0500

    100. Same Tree

---
+++ LeetCode/1. Easy/100. Same Tree/Solution.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.__TreeNode;
+using NUnit.Framework;
+
+namespace LeetCode._1._Easy._100._Same_Tree;
+
+public class Solution
+{
+    public bool IsSameTree(TreeNode p, TreeNode q)
+    {
+        return p == null && q == null || (p != null &&
+                                          q != null &&
+                                          p.val == q.val &&
+                                          (p.left == null) ^ (q.left != null) &&
+                                          (p.left == null || IsSameTree(p.left, q.left)) &&
+                                          (p.right == null) ^ (q.right != null) &&
+                                          (p.right == null || IsSameTree(p.right, q.right)));
+    }
+}
---
+++ LeetCode/1. Easy/100. Same Tree/Tests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using LeetCode.__TreeNode;
+using NUnit.Framework;
+
+namespace LeetCode._1._Easy._100._Same_Tree;
+
+[TestFixture(TestName = "100. Same Tree")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int?[] input1, int?[] input2, bool output)
+    {
+        var solution = new Solution();
+
+        var treeRoot1 = TreeNode.CreateFromArray(input1);
+        var treeRoot2 = TreeNode.CreateFromArray(input2);
+        var actual = solution.IsSameTree(treeRoot1, treeRoot2);
+
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        yield return new object[]
+        {
+            new int?[]{1,2,3},
+            new int?[]{1,2,3},
+            true,
+        };
+        yield return new object[]
+        {
+            new int?[]{1,2},
+            new int?[]{1,null,2},
+            false,
+        };
+        yield return new object[]
+        {
+            new int?[]{1,2,1},
+            new int?[]{1,1,2},
+            false,
+        };
+        yield return new object[]
+        {
+            new int?[]{1},
+            new int?[]{1,null,2},
+            false,
+        };
+    }
+}
