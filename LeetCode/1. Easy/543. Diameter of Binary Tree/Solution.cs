951a8543b1252290abba801882572c8bf237b899
hackerdmitry <d.butov@skbkontur.ru>
2024-03-05 09:36:17 +0500

    543. Diameter of Binary Tree

---
+++ LeetCode/1. Easy/543. Diameter of Binary Tree/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LeetCode.__TreeNode;
+
+namespace LeetCode._1._Easy._543._Diameter_of_Binary_Tree;
+
+public class Solution
+{
+    private List<int> heights = new();
+
+    public int DiameterOfBinaryTree(TreeNode root)
+    {
+        GetHeights(root, 0);
+
+        var enumerator = heights.OrderByDescending(x => x).GetEnumerator();
+        var first = enumerator.MoveNext() ? enumerator.Current : 0;
+        var second = enumerator.MoveNext() ? enumerator.Current : 0;
+
+        return first + second;
+    }
+
+    private void GetHeights(TreeNode node, int curHeight)
+    {
+        if (node.left == null && node.right == null)
+        {
+            heights.Add(curHeight);
+            return;
+        }
+
+        if (node.left != null)
+            GetHeights(node.left, curHeight + 1);
+
+        if (node.right != null)
+            GetHeights(node.right, curHeight + 1);
+    }
+}
---
+++ LeetCode/1. Easy/543. Diameter of Binary Tree/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using LeetCode.__TreeNode;
+using NUnit.Framework;
+
+namespace LeetCode._1._Easy._543._Diameter_of_Binary_Tree;
+
+[TestFixture(TestName = "543. Diameter of Binary Tree")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCaseSource(nameof(Input))]
+    public void Test(int?[] input, int output)
+    {
+        var solution = new Solution();
+
+        var treeRoot = TreeNode.CreateFromArray(input);
+        var actual = solution.DiameterOfBinaryTree(treeRoot);
+
+        Assert.AreEqual(output, actual);
+    }
+
+    private static IEnumerable Input()
+    {
+        yield return new object[]
+        {
+            new int?[]{1,2,3,4,5},
+            3
+        };
+        yield return new object[]
+        {
+            new int?[]{1,2},
+            1
+        };
+    }
+}
