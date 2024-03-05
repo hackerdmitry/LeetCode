bcdf9bc5bbdcf98bd102bd530749a8d95ad51dfa
hackerdmitry <d.butov@skbkontur.ru>
2024-03-05 09:48:02 +0500

    1750. Minimum Length of String After Deleting Similar Ends

---
+++ LeetCode/2. Middle/1750. Minimum Length of String After Deleting Similar Ends/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode._2._Middle._1750._Minimum_Length_of_String_After_Deleting_Similar_Ends;
+
+public class Solution
+{
+    public int MinimumLength(string s)
+    {
+        var left = 0;
+        var right = s.Length - 1;
+
+        while (left < right &&  s[left] == s[right])
+        {
+            do left++; while (left < right && s[left] == s[left - 1]);
+            do right--; while (right > left && s[right] == s[right + 1]);
+        }
+
+        return Math.Max(0, right - left + 1);
+    }
+}
---
+++ LeetCode/2. Middle/1750. Minimum Length of String After Deleting Similar Ends/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1750._Minimum_Length_of_String_After_Deleting_Similar_Ends;
+
+[TestFixture(TestName = "1750. Minimum Length of String After Deleting Similar Ends")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("ca", 2)]
+    [TestCase("cabaabac", 0)]
+    [TestCase("aabccabba", 3)]
+    [TestCase("a", 1)]
+    [TestCase("aaa", 0)]
+    [TestCase("bbbbbbbbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbccbcbcbccbbabbb", 1)]
+    public void Test(string input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MinimumLength(input);
+        Assert.AreEqual(output, actual);
+    }
+}
