eb75aaaa73ae485e06a64dc9d3a6b20aa6ed21f0
hackerdmitry <d.butov@skbkontur.ru>
2024-04-25 10:10:54 +0500

    2370. Longest Ideal Subsequence

---
+++ LeetCode/2. Middle/2370. Longest Ideal Subsequence/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2._Middle._2370._Longest_Ideal_Subsequence;
+
+public class Solution
+{
+    public int LongestIdealString(string s, int k)
+    {
+        var dictionaryAlphabet = new Dictionary<char, int>();
+
+        foreach (var c in s)
+        {
+            var start = (char)Math.Max(c - k, 'a');
+            var end = (char)Math.Min(c + k, 'z');
+
+            var result = 0;
+            for (var i = start; i <= end; i++)
+                if (dictionaryAlphabet.ContainsKey(i) && dictionaryAlphabet[i] > result)
+                    result = dictionaryAlphabet[i];
+            dictionaryAlphabet[c] = result + 1;
+        }
+
+        return dictionaryAlphabet.Max(x => x.Value);
+    }
+}
---
+++ LeetCode/2. Middle/2370. Longest Ideal Subsequence/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._2370._Longest_Ideal_Subsequence;
+
+[TestFixture(TestName = "2370. Longest Ideal Subsequence")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("acfgbd", 2, 4)]
+    [TestCase("abcd", 3, 4)]
+    public void Test(string input1, int input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.LongestIdealString(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
