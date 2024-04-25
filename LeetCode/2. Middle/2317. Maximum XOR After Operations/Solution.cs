7b8d8fa58c84aefcf95c390fd647198ba27b6952
hackerdmitry <d.butov@skbkontur.ru>
2024-04-25 17:39:26 +0500

    2317. Maximum XOR After Operations

---
+++ LeetCode/2. Middle/2317. Maximum XOR After Operations/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+namespace LeetCode._2._Middle._2317._Maximum_XOR_After_Operations;
+
+public class Solution
+{
+    public int MaximumXOR(int[] nums)
+    {
+        var maxNum = nums.Max();
+        if (maxNum == 0)
+            return 0;
+
+        var signsCount = 0;
+        while (maxNum != 0)
+        {
+            maxNum /= 2;
+            signsCount++;
+        }
+
+        var availableOnes = new bool[signsCount];
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var curNum = nums[i];
+            for (var j = 0; j < signsCount && curNum != 0; j++, curNum /= 2)
+                if (curNum % 2 == 1)
+                    availableOnes[j] = true;
+        }
+
+        var result = 0;
+        for (var i = signsCount - 1; i >= 0; i--)
+        {
+            if (availableOnes[i])
+                result++;
+            if (i != 0)
+                result <<= 1;
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/2. Middle/2317. Maximum XOR After Operations/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._2317._Maximum_XOR_After_Operations;
+
+[TestFixture(TestName = "2317. Maximum XOR After Operations")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{3,2,4,6}, 7)]
+    [TestCase(new[]{1,2,3,9,2}, 11)]
+    public void Test(int[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MaximumXOR(input);
+        Assert.AreEqual(output, actual);
+    }
+}
