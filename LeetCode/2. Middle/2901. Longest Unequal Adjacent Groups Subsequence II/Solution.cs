7d5f95f99986e9369033bea70441dfd5571991ae
hackerdmitry <d.butov@skbkontur.ru>
2024-04-25 16:50:45 +0500

    2901. Longest Unequal Adjacent Groups Subsequence II

---
+++ LeetCode/2. Middle/2901. Longest Unequal Adjacent Groups Subsequence II/Solution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2._Middle._2901._Longest_Unequal_Adjacent_Groups_Subsequence_II;
+
+public class Solution
+{
+    public IList<string> GetWordsInLongestSubsequence(string[] words, int[] groups)
+    {
+        var n = words.Length;
+        var cells = new Cell[n];
+        for (var i = 0; i < n; i++)
+        {
+            cells[i] = new Cell { Count = 1, Word = words[i], Group = groups[i], PrevCell = null };
+            for (var j = i - 1; j >= 0; j--)
+            {
+                var isMatch = cells[i].Group != cells[j].Group && SatisfyConditionHammingDistance(cells[i].Word, cells[j].Word);
+                var boost = isMatch ? 1 : 0;
+                if (isMatch && cells[j].Count + boost > cells[i].Count)
+                {
+                    cells[i].Count = cells[j].Count + boost;
+                    cells[i].PrevCell = cells[j];
+                }
+            }
+        }
+
+        var lastCell = cells.MaxBy(x => x.Count);
+        var result = new string[lastCell.Count];
+        while (lastCell != null)
+        {
+            result[lastCell.Count - 1] = lastCell.Word;
+            lastCell = lastCell.PrevCell;
+        }
+
+        return result;
+    }
+
+    private bool SatisfyConditionHammingDistance(string first, string second) =>
+        HammingDistance(first, second) == 1;
+
+    private int? HammingDistance(string first, string second)
+    {
+        if (first.Length != second.Length)
+            return null;
+
+        var differencesCount = 0;
+        for (var i = 0; i < first.Length; i++)
+            if (first[i] != second[i])
+                differencesCount++;
+
+        return differencesCount;
+    }
+
+    private class Cell
+    {
+        public int Count { get; set; }
+        public string Word { get; set; }
+        public int Group { get; set; }
+        public Cell PrevCell { get; set; }
+    }
+}
---
+++ LeetCode/2. Middle/2901. Longest Unequal Adjacent Groups Subsequence II/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._2901._Longest_Unequal_Adjacent_Groups_Subsequence_II;
+
+[TestFixture(TestName = "2901. Longest Unequal Adjacent Groups Subsequence II")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{"bab","dab","cab"}, new[]{1,2,2}, new[]{"bab","dab"})]
+    [TestCase(new[]{"a","b","c","d"}, new[]{1,2,3,4}, new[]{"a","b","c","d"})]
+    [TestCase(new[]{"a","b","c","d"}, new[]{1,2,3,3}, new[]{"a","b","c"})]
+    [TestCase(new[]{"abbbb"}, new[]{1}, new[]{"abbbb"})]
+    [TestCase(new[]{"ccd","bb","ccc"}, new[]{1,1,2}, new[]{"ccd","ccc"})]
+    [TestCase(new[]{"bdb","aaa","ada"}, new[]{2,1,3}, new[]{"aaa","ada"})]
+    [TestCase(new[]{"dcaacc","da","ddcbd","dd"}, new[]{2,3,1,4}, new[]{"da","dd"})]
+    [TestCase(new[]{"aab","cab","ba","dba","daa","bca"}, new[]{4,3,4,6,4,3}, new[]{"aab,cab"})]
+    public void Test(string[] input1, int[] input2, string[] output)
+    {
+        var solution = new Solution();
+        Console.WriteLine($"output: [{string.Join(',', output)}]");
+        var actual = solution.GetWordsInLongestSubsequence(input1, input2);
+        Console.WriteLine($"actual: [{string.Join(',', actual)}]");
+        Assert.AreEqual(output, actual);
+    }
+}
