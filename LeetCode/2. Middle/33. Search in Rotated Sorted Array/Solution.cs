3570c033f6935203ee2ba0e1e140f3d9cf6c57df
hackerdmitry <d.butov@skbkontur.ru>
2024-04-26 10:11:57 +0500

    33. Search in Rotated Sorted Array

---
+++ LeetCode/2. Middle/33. Search in Rotated Sorted Array/Solution.cs
@@ -0,0 +1,68 @@
+﻿namespace LeetCode._2._Middle._33._Search_in_Rotated_Sorted_Array;
+
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        var shift = FindShift(nums);
+        return BinarySearchWithRotatedArray(nums, shift, target);
+    }
+
+    public int BinarySearchWithRotatedArray(int[] nums, int shift, int target)
+    {
+        var n = nums.Length;
+        var separation = n - shift;
+        var l = 0;
+        var r = n;
+
+        int GetNum(int index) =>
+            nums[GetIndex(index)];
+
+        int GetIndex(int index) =>
+            index >= separation
+                ? index - separation
+                : index + shift;
+
+        if (GetNum(l) == target)
+            return GetIndex(l);
+        if (n == 1)
+            return -1;
+        if (GetNum(r) == target)
+            return GetIndex(r);
+
+        while (true)
+        {
+            var m = (l + r) / 2;
+            if (GetNum(m) == target)
+                return GetIndex(m);
+
+            if (GetNum(m) < target)
+                l = m;
+            else
+                r = m;
+
+            if (r - l == 1)
+                return -1;
+        }
+    }
+
+    public int FindShift(int[] nums)
+    {
+        var l = 0;
+        var r = nums.Length - 1;
+        if (nums[l] < nums[r] || l == r)
+            return 0;
+
+        while (true)
+        {
+            var m = (l + r) / 2;
+            if (l == m)
+                return r;
+
+            if (nums[m] > nums[l])
+                l = m;
+            else
+                r = m;
+        }
+    }
+}
---
+++ LeetCode/2. Middle/33. Search in Rotated Sorted Array/Tests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._33._Search_in_Rotated_Sorted_Array;
+
+[TestFixture(TestName = "33. Search in Rotated Sorted Array")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{4,5,6,7,0,1,2}, 0, 4)]
+    [TestCase(new[]{4,5,6,7,0,1,2}, 3, -1)]
+    [TestCase(new[]{1}, 0, -1)]
+    public void Test(int[] input1, int input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.Search(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+
+    [Timeout(1000)]
+    [TestCase(new[]{0,1,2,3}, 0)]
+    [TestCase(new[]{3,0,1,2}, 1)]
+    [TestCase(new[]{2,3,0,1}, 2)]
+    [TestCase(new[]{1,2,3,0}, 3)]
+    [TestCase(new[]{0,1,2}, 0)]
+    [TestCase(new[]{2,0,1}, 1)]
+    [TestCase(new[]{1,2,0}, 2)]
+    public void TestFindShift(int[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.FindShift(input);
+        Assert.AreEqual(output, actual);
+    }
+}
