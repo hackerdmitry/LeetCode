e2069deab8bc5de3d9483f448e2bdc70965b78d3
hackerdmitry <hacker.dmitry@gmail.com>
2024-04-29 12:46:34 +0500

    516. Longest Palindromic Subsequence

---
+++ LeetCode/2. Middle/516. Longest Palindromic Subsequence/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LeetCode._2._Middle._516._Longest_Palindromic_Subsequence;
+
+public class Solution
+{
+    public int LongestPalindromeSubseq(string s)
+    {
+        var dp = new int[s.Length][];
+        for (var i = 0; i < s.Length; i++)
+        {
+            dp[i] = new int[s.Length - i];
+            dp[i][0] = 1;
+        }
+
+        var result = 1;
+        for (var i = 1; i < s.Length; i++)
+        {
+            for (var j = 0; j < s.Length - i; j++)
+            {
+                dp[j][i] = s[j] == s[j + i] ? 2 : 0;
+
+                var max = 0;
+                for (var k = 0; k < i - 1; k++)
+                    if (dp[j + 1][k] > max)
+                        max = dp[j + 1][k];
+
+                dp[j][i] += max;
+                dp[j][i] = Math.Max(dp[j][i], Math.Max(dp[j + 1][i - 1], dp[j][i - 1]));
+
+                if (dp[j][i] > result)
+                    result = dp[j][i];
+            }
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/2. Middle/516. Longest Palindromic Subsequence/Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._516._Longest_Palindromic_Subsequence;
+
+[TestFixture(TestName = "516. Longest Palindromic Subsequence")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("bbbab", 4)]
+    [TestCase("cbbd", 2)]
+    [TestCase("cbbbab", 4)]
+    [TestCase("cbbsbab", 5)]
+    [TestCase("cbbbbab", 5)]
+    [TestCase("a", 1)]
+    [TestCase("euazbipzncptldueeuechubrcourfpftcebikrxhybkymimgvldiwqvkszfycvqyvtiwfckexmowcxztkfyzqovbtmzpxojfofbvwnncajvrvdbvjhcrameamcfmcoxryjukhpljwszknhiypvyskmsujkuggpztltpgoczafmfelahqwjbhxtjmebnymdyxoeodqmvkxittxjnlltmoobsgzdfhismogqfpfhvqnxeuosjqqalvwhsidgiavcatjjgeztrjuoixxxoznklcxolgpuktirmduxdywwlbikaqkqajzbsjvdgjcnbtfksqhquiwnwflkldgdrqrnwmshdpykicozfowmumzeuznolmgjlltypyufpzjpuvucmesnnrwppheizkapovoloneaxpfinaontwtdqsdvzmqlgkdxlbeguackbdkftzbnynmcejtwudocemcfnuzbttcoew", 159)]
+    [TestCase("aaaaaa", 6)]
+    [TestCase("qwertyuiopasdfghjklzxcvbnm", 1)]
+    [TestCase("aiopahaa", 5)]
+    public void Test(string input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.LongestPalindromeSubseq(input);
+        Assert.AreEqual(output, actual);
+    }
+}
