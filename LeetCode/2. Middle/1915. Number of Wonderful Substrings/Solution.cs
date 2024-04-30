e0d987f28667eb8bbed3d9ec48bd9444aeb2e598
hackerdmitry <hacker.dmitry@gmail.com>
2024-04-30 11:35:48 +0500

    1915. Number of Wonderful Substrings

---
+++ LeetCode/2. Middle/1915. Number of Wonderful Substrings/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._2._Middle._1915._Number_of_Wonderful_Substrings;
+
+public class Solution
+{
+    public long WonderfulSubstrings(string word)
+    {
+        var freq = new Dictionary<int, int> { [0] = 1 };
+        var letters = new[] { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
+
+        var result = 0L;
+        var mask = 0;
+
+        foreach (var c in word)
+        {
+            mask ^= 1 << (c - 'a');
+
+            if (freq.ContainsKey(mask))
+            {
+                result += freq[mask];
+                freq[mask]++;
+            }
+            else freq[mask] = 1;
+
+            foreach (var letter in letters)
+                if (freq.ContainsKey(mask ^ letter))
+                    result += freq[mask ^ letter];
+        }
+
+        return result;
+    }
+}
