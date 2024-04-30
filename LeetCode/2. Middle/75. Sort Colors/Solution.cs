1937af7bd2627316112b76a8bafa7e041bda5a92
hackerdmitry <hacker.dmitry@gmail.com>
2024-04-30 15:50:02 +0500

    75. Sort Colors

---
+++ LeetCode/2. Middle/75. Sort Colors/Solution.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode._2._Middle._75._Sort_Colors;
+
+public class Solution
+{
+    public void SortColors(int[] nums)
+    {
+        for (var i = 0; i < nums.Length; i++)
+            for (var j = i + 1; j < nums.Length; j++)
+                if (nums[i] > nums[j])
+                    (nums[i], nums[j]) = (nums[j], nums[i]);
+    }
+}
---
+++ LeetCode/2. Middle/75. Sort Colors/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._75._Sort_Colors;
+
+[TestFixture(TestName = "75. Sort Colors")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{2,0,2,1,1,0}, new[]{0,0,1,1,2,2})]
+    [TestCase(new[]{2,0,1}, new[]{0,1,2})]
+    public void Test(int[] input, int[] output)
+    {
+        var solution = new Solution();
+        solution.SortColors(input);
+        Assert.AreEqual(output, input);
+    }
+}
