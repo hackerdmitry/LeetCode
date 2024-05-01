91f3dbf3d786f45df0519e6e223d39aae3fffdc0
hackerdmitry <hacker.dmitry@gmail.com>
2024-05-01 13:05:58 +0500

    2000. Reverse Prefix of Word

---
+++ LeetCode/1. Easy/2000. Reverse Prefix of Word/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace LeetCode._1._Easy._2000._Reverse_Prefix_of_Word;
+
+public class Solution
+{
+    public string ReversePrefix(string word, char ch)
+    {
+        var index = word.IndexOf(ch);
+        if (index == -1)
+            return word;
+
+        return new string(word.Substring(0, ++index).Reverse().ToArray()) + word.Substring(index);
+    }
+}
---
+++ LeetCode/1. Easy/2000. Reverse Prefix of Word/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._2000._Reverse_Prefix_of_Word;
+
+[TestFixture(TestName = "2000. Reverse Prefix of Word")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("abcdefd", 'd', "dcbaefd")]
+    [TestCase("xyxzxe", 'z', "zxyxxe")]
+    [TestCase("abcd", 'z', "abcd")]
+    public void Test(string input1, char input2, string output)
+    {
+        var solution = new Solution();
+        var actual = solution.ReversePrefix(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
