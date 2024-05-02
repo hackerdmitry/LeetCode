4c67db71996eb1ec17f846b0272372c5ff8d6966
hackerdmitry <hacker.dmitry@gmail.com>
2024-05-02 08:16:51 +0500

    2441. Largest Positive Integer That Exists With Its Negative

---
+++ LeetCode/1. Easy/2441. Largest Positive Integer That Exists With Its Negative/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode._1._Easy._2441._Largest_Positive_Integer_That_Exists_With_Its_Negative;
+
+public class Solution
+{
+    public int FindMaxK(int[] nums)
+    {
+        Array.Sort(nums);
+        var pointer1 = 0;
+        var pointer2 = nums.Length - 1;
+
+        while (nums[pointer1] < 0 && nums[pointer2] > 0)
+        {
+            if (nums[pointer1] == -nums[pointer2])
+                return nums[pointer2];
+            if (nums[pointer1] < -nums[pointer2])
+                pointer1++;
+            else
+                pointer2--;
+        }
+
+        return -1;
+    }
+}
---
+++ LeetCode/1. Easy/2441. Largest Positive Integer That Exists With Its Negative/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._2441._Largest_Positive_Integer_That_Exists_With_Its_Negative;
+
+[TestFixture(TestName = "2441. Largest Positive Integer That Exists With Its Negative")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{-1,2,-3,3}, 3)]
+    [TestCase(new[]{-1,10,6,7,-7,1}, 7)]
+    [TestCase(new[]{-10,8,6,7,-2,-3}, -1)]
+    [TestCase(new[]{1}, -1)]
+    [TestCase(new[]{1,2,3}, -1)]
+    public void Test(int[] input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.FindMaxK(input);
+        Assert.AreEqual(output, actual);
+    }
+}
