f094c18fa33a79255b0dc3e949e6737316c0954f
hackerdmitry <d.butov@skbkontur.ru>
2024-05-03 09:04:23 +0500

    165. Compare Version Numbers

---
+++ LeetCode/2. Middle/165. Compare Version Numbers/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode._2._Middle._165._Compare_Version_Numbers;
+
+public class Solution
+{
+    public int CompareVersion(string version1, string version2)
+    {
+        var revisions1 = Array.ConvertAll(version1.Split('.'), int.Parse);
+        var revisions2 = Array.ConvertAll(version2.Split('.'), int.Parse);
+
+        var minLen = Math.Min(revisions1.Length, revisions2.Length);
+        var maxLen = Math.Max(revisions1.Length, revisions2.Length);
+        for (var i = 0; i < minLen; i++)
+        {
+            if (revisions1[i] < revisions2[i])
+                return -1;
+            if (revisions1[i] > revisions2[i])
+                return 1;
+        }
+
+        for (var i = minLen; i < maxLen; i++)
+        {
+            if (i < revisions1.Length && revisions1[i] > 0)
+                return 1;
+            if (i < revisions2.Length && revisions2[i] > 0)
+                return -1;
+        }
+
+        return 0;
+    }
+}
---
+++ LeetCode/2. Middle/165. Compare Version Numbers/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._165._Compare_Version_Numbers;
+
+[TestFixture(TestName = "165. Compare Version Numbers")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("1.01", "1.001", 0)]
+    [TestCase("1.0", "1.0.0", 0)]
+    [TestCase("0.1", "1.1", -1)]
+    [TestCase("1.0.1", "1", 1)]
+    public void Test(string input1, string input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.CompareVersion(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
