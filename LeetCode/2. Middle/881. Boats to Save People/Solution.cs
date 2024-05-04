33df659e130d290a742cb636c4cf90178b92dcaf
hackerdmitry <hacker.dmitry@gmail.com>
2024-05-04 14:34:18 +0500

    881. Boats to Save People

---
+++ LeetCode/2. Middle/881. Boats to Save People/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode._2._Middle._881._Boats_to_Save_People;
+
+public class Solution
+{
+    public int NumRescueBoats(int[] people, int limit)
+    {
+        Array.Sort(people);
+
+        var result = 0;
+        var leftPointer = 0;
+        var rightPointer = people.Length - 1;
+
+        while (leftPointer < rightPointer)
+        {
+            result++;
+            if (people[leftPointer] + people[rightPointer--] <= limit)
+                leftPointer++;
+        }
+
+        if (leftPointer == rightPointer)
+            result++;
+
+        return result;
+    }
+}
---
+++ LeetCode/2. Middle/881. Boats to Save People/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._881._Boats_to_Save_People;
+
+[TestFixture(TestName = "881. Boats to Save People")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[] {1, 2}, 3, 1)]
+    [TestCase(new[] {3, 2, 2, 1}, 3, 3)]
+    [TestCase(new[] {3, 5, 3, 4}, 5, 4)]
+    [TestCase(new[] {1, 1, 1, 2, 2, 2}, 3, 3)]
+    [TestCase(new[] {3, 2, 3, 2, 2}, 6, 3)]
+    public void Test(int[] input1, int input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.NumRescueBoats(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
