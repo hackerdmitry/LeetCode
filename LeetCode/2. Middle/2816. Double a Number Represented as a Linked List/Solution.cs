9f4e19115ea8a9bbb9046182b9e9d00eea2f2e87
hackerdmitry <hacker.dmitry@gmail.com>
2024-05-07 09:50:42 +0500

    2816. Double a Number Represented as a Linked List

---
+++ LeetCode/2. Middle/2816. Double a Number Represented as a Linked List/Solution.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.__Additional;
+
+namespace LeetCode._2._Middle._2816._Double_a_Number_Represented_as_a_Linked_List;
+
+public class Solution
+{
+    public ListNode DoubleIt(ListNode head)
+    {
+        var array = new ListNode[10_000];
+        array[0] = head;
+        var length = 1;
+        for (; array[length - 1].next != null; length++)
+            array[length] = array[length - 1].next;
+
+        var remainder = 0;
+        for (var i = length - 1; i >= 0; i--)
+        {
+            array[i].val += array[i].val + remainder;
+            remainder = array[i].val / 10;
+            array[i].val %= 10;
+        }
+
+        return remainder == 0 ? head : new ListNode(remainder, head);
+    }
+}
---
+++ LeetCode/2. Middle/2816. Double a Number Represented as a Linked List/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._2816._Double_a_Number_Represented_as_a_Linked_List;
+
+[TestFixture(TestName = "2816. Double a Number Represented as a Linked List")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[] {1, 8, 9}, new[] {3, 7, 8})]
+    [TestCase(new[] {9, 9, 9}, new[] {1, 9, 9, 8})]
+    public void Test(int[] input, int[] output)
+    {
+        var solution = new Solution();
+        var actual = solution.DoubleIt(input).ToArray();
+        Assert.AreEqual(output, actual);
+    }
+}
