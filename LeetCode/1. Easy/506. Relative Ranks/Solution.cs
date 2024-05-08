2b5716101d93011a4cd7ee11fd49959b1dd8790a
hackerdmitry <d.butov@skbkontur.ru>
2024-05-08 09:20:22 +0500

    506. Relative Ranks

---
+++ LeetCode/1. Easy/506. Relative Ranks/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace LeetCode._1._Easy._506._Relative_Ranks;
+
+public class Solution
+{
+    public string[] FindRelativeRanks(int[] score)
+    {
+        return score
+            .Select((x, i) => (x, i))
+            .OrderByDescending(x => x.Item1)
+            .Select((x, i) => ((i + 1) switch
+            {
+                1 => "Gold Medal",
+                2 => "Silver Medal",
+                3 => "Bronze Medal",
+                _ => (i + 1).ToString(),
+            }, x.Item2))
+            .OrderBy(x => x.Item2)
+            .Select(x => x.Item1)
+            .ToArray();
+    }
+}
---
+++ LeetCode/1. Easy/506. Relative Ranks/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCode._1._Easy._506._Relative_Ranks;
+
+[TestFixture(TestName = "506. Relative Ranks")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{5,4,3,2,1}, new[]{"Gold Medal","Silver Medal","Bronze Medal","4","5"})]
+    [TestCase(new[]{10,3,8,9,4}, new[]{"Gold Medal","5","Bronze Medal","Silver Medal","4"})]
+    public void Test(int[] input, string[] output)
+    {
+        var solution = new Solution();
+        var actual = solution.FindRelativeRanks(input);
+        Console.WriteLine($"actual: [{string.Join(", ", actual)}]");
+        Console.WriteLine($"expected: [{string.Join(", ", output)}]");
+        Assert.AreEqual(output, actual);
+    }
+}
