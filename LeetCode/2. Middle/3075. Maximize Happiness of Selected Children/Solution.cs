1c4ebbf5d64b5308a3afb320b98234c3941c63ed
hackerdmitry <hacker.dmitry@gmail.com>
2024-05-09 10:15:48 +0500

    3075. Maximize Happiness of Selected Children

---
+++ LeetCode/2. Middle/3075. Maximize Happiness of Selected Children/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LeetCode._2._Middle._3075._Maximize_Happiness_of_Selected_Children;
+
+public class Solution
+{
+    public long MaximumHappinessSum(int[] happiness, int k)
+    {
+        Array.Sort(happiness);
+        var result = 0L;
+        for (var i = 0; i < happiness.Length && i < k; i++)
+            result += Math.Max(happiness[happiness.Length - 1 - i] - i, 0);
+        return result;
+    }
+}
---
+++ LeetCode/2. Middle/3075. Maximize Happiness of Selected Children/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._3075._Maximize_Happiness_of_Selected_Children;
+
+[TestFixture(TestName = "3075. Maximize Happiness of Selected Children")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[]{1,2,3}, 2, 4)]
+    [TestCase(new[]{1,1,1,1}, 2, 1)]
+    [TestCase(new[]{2,3,4,5}, 1, 5)]
+    public void Test(int[] input1, int input2, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MaximumHappinessSum(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
