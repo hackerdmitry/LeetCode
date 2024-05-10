82a793119744d9ba60ac52cacdabdff5543cb76a
hackerdmitry <hacker.dmitry@gmail.com>
2024-05-10 16:54:54 +0500

    786. K-th Smallest Prime Fraction

---
+++ LeetCode/2. Middle/786. K-th Smallest Prime Fraction/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2._Middle._786._K_th_Smallest_Prime_Fraction;
+
+public class Solution
+{
+    public int[] KthSmallestPrimeFraction(int[] arr, int k)
+    {
+        var list = new List<PrimeFraction>();
+        for (var i = 0; i < arr.Length; i++)
+            for (var j = i + 1; j < arr.Length; j++)
+                list.Add(new PrimeFraction(arr[i], arr[j]));
+
+        var kSmallestPrimeFraction = list.OrderBy(x => x.Value).Skip(k - 1).First();
+        return new[] {kSmallestPrimeFraction.Nominator, kSmallestPrimeFraction.Denominator};
+    }
+
+    class PrimeFraction
+    {
+        public int Nominator { get; set; }
+        public int Denominator { get; set; }
+
+        public decimal Value { get; set; }
+
+        public PrimeFraction(int nominator, int denominator)
+        {
+            Nominator = nominator;
+            Denominator = denominator;
+            Value = decimal.Divide(nominator, denominator);
+        }
+    }
+}
---
+++ LeetCode/2. Middle/786. K-th Smallest Prime Fraction/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._786._K_th_Smallest_Prime_Fraction;
+
+[TestFixture(TestName = "786. K-th Smallest Prime Fraction")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[] {1, 2, 3, 5}, 3, new[] {2, 5})]
+    [TestCase(new[] {1, 7}, 1, new[] {1, 7})]
+    public void Test(int[] input1, int input2, int[] output)
+    {
+        var solution = new Solution();
+        var actual = solution.KthSmallestPrimeFraction(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
