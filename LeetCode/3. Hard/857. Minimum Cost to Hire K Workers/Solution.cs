3e363f54087fcd38d1603979bf4bce4a36f9625d
hackerdmitry <hacker.dmitry@gmail.com>
2024-05-11 14:27:39 +0500

    857. Minimum Cost to Hire K Workers

---
+++ LeetCode/3. Hard/857. Minimum Cost to Hire K Workers/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._3._Hard._857._Minimum_Cost_to_Hire_K_Workers;
+
+public class Solution
+{
+    public double MincostToHireWorkers(int[] quality, int[] wage, int k)
+    {
+        var workers = new Worker[quality.Length];
+        for (var i = 0; i < quality.Length; i++)
+            workers[i] = new Worker(wage[i], quality[i]);
+
+        var minWage = decimal.MaxValue;
+        var qualityQueue = new PriorityQueue<int, int>();
+        var totalQueue = decimal.Zero;
+
+        foreach (var worker in workers.OrderBy(x => x.Value))
+        {
+            totalQueue += worker.Quality;
+            qualityQueue.Enqueue(worker.Quality, -worker.Quality);
+
+            if (qualityQueue.Count > k)
+                totalQueue -= qualityQueue.Dequeue();
+
+            if (qualityQueue.Count == k)
+                minWage = Math.Min(minWage, totalQueue * worker.Value);
+        }
+
+        return (double) minWage;
+    }
+
+    class Worker
+    {
+        public int Wage { get; set; }
+        public int Quality { get; set; }
+
+        public decimal Value { get; set; }
+
+        public Worker(int wage, int quality)
+        {
+            Wage = wage;
+            Quality = quality;
+            Value = decimal.Divide(wage, quality);
+        }
+
+        public override string ToString()
+        {
+            return $"q={Quality}, w={Wage}, v={Value:0.00000}";
+        }
+    }
+}
---
+++ LeetCode/3. Hard/857. Minimum Cost to Hire K Workers/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._3._Hard._857._Minimum_Cost_to_Hire_K_Workers;
+
+[TestFixture(TestName = "857. Minimum Cost to Hire K Workers")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(new[] {10, 20, 5}, new[] {70, 50, 30}, 2, 105.00000)]
+    [TestCase(new[] {3, 1, 10, 10, 1}, new[] {4, 8, 2, 2, 7}, 3, 30.66667)]
+    [TestCase(new[] {5, 5, 5, 1}, new[] {14, 5, 7, 5}, 3, 42.00000)]
+    [TestCase(new[] {2, 1, 5}, new[] {17, 6, 4}, 2, 25.50000)]
+    [TestCase(new[] {2}, new[] {14}, 1, 14.00000)]
+    [TestCase(new[] {14, 56, 59, 89, 39, 26, 86, 76, 3, 36}, new[] {90, 217, 301, 202, 294, 445, 473, 245, 415, 487}, 2, 399.53846)]
+    [TestCase(new[] {32, 43, 66, 9, 94, 57, 25, 44, 99, 19}, new[] {187, 366, 117, 363, 121, 494, 348, 382, 385, 262}, 4, 1528.00000)]
+    public void Test(int[] input1, int[] input2, int input3, double output)
+    {
+        var solution = new Solution();
+        var actual = solution.MincostToHireWorkers(input1, input2, input3);
+        Assert.AreEqual(output, actual, 1e-5);
+    }
+}
