479380d7113cba4bce4df961a783c82000277fd0
hackerdmitry <d.butov@skbkontur.ru>
2024-08-12 12:26:12 +0500

    703. Kth Largest Element in a Stream

---
+++ LeetCode/1. Easy/703. Kth Largest Element in a Stream/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._1._Easy._703._Kth_Largest_Element_in_a_Stream;
+
+public class KthLargest
+{
+    private readonly PriorityQueue<int, int> priorityQueue = new();
+    private readonly int k = 0;
+
+    public KthLargest(int k, int[] nums)
+    {
+        Array.Sort(nums);
+        this.k = k;
+        priorityQueue.EnqueueRange(nums.Skip(nums.Length - k).Select(x => (x, GetPriority(x))));
+    }
+
+    public int Add(int val)
+    {
+        if (priorityQueue.Count < k)
+        {
+            priorityQueue.Enqueue(val, GetPriority(val));
+            return priorityQueue.Peek();
+        }
+
+        if (priorityQueue.Peek() >= val)
+            return priorityQueue.Peek();
+
+        priorityQueue.EnqueueDequeue(val, GetPriority(val));
+        return priorityQueue.Peek();
+    }
+
+    private int GetPriority(int val) => val;
+}
---
+++ LeetCode/1. Easy/703. Kth Largest Element in a Stream/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCode._1._Easy._703._Kth_Largest_Element_in_a_Stream;
+
+[TestFixture(TestName = "703. Kth Largest Element in a Stream")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(3, new[]{4, 5, 8, 2}, new[]{3,5,10,9,4}, new[]{4, 5, 5, 8, 8})]
+    [TestCase(2, new[]{0}, new[]{-1,1,-2,-4,3}, new[]{-1,0,0,0,1})]
+    [TestCase(3, new[]{5,-1}, new[]{2,1,-1,3,4}, new[]{-1,1,1,2,3})]
+    public void Test(int k, int[] startNums, int[] toAddNums, int[] expectedNums)
+    {
+        var kthLargest = new KthLargest(k, startNums);
+
+        for (var i = 0; i < toAddNums.Length; i++)
+        {
+            var expectedNum = expectedNums[i];
+            var actualNum = kthLargest.Add(toAddNums[i]);
+            Console.WriteLine($"exp={expectedNum}, act={actualNum}");
+            Assert.AreEqual(expectedNum, actualNum);
+        }
+    }
+}
