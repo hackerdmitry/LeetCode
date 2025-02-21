83cfb10d0b5c0b1a94e668d58e2868592412eb08
Dmitry Butov <d.butov@2gis.ru>
2025-02-21 08:56:23 +0500

    1261. Find Elements in a Contaminated Binary Tree

---
+++ LeetCode/2. Middle/1261. Find Elements in a Contaminated Binary Tree/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using LeetCode.__Additional;
+
+namespace LeetCode._2._Middle._1261._Find_Elements_in_a_Contaminated_Binary_Tree;
+
+public class FindElements
+{
+    private readonly HashSet<int> hashSet = new();
+    private const int maxValue = 1_000_000;
+
+    public FindElements(TreeNode root)
+    {
+        root.val = 0;
+        hashSet.Add(root.val);
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+
+            if (node.left != null)
+            {
+                var target = node.val * 2 + 1;
+                if (target <= maxValue)
+                {
+                    node.left.val = target;
+                    hashSet.Add(target);
+                    queue.Enqueue(node.left);
+                }
+            }
+
+            if (node.right != null)
+            {
+                var target = node.val * 2 + 2;
+                if (target <= maxValue)
+                {
+                    node.right.val = target;
+                    hashSet.Add(target);
+                    queue.Enqueue(node.right);
+                }
+            }
+        }
+    }
+
+    public bool Find(int target)
+    {
+        return hashSet.Contains(target);
+    }
+}
---
+++ LeetCode/2. Middle/1261. Find Elements in a Contaminated Binary Tree/Tests.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.__Additional;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._1261._Find_Elements_in_a_Contaminated_Binary_Tree;
+
+[Timeout(1000)]
+[TestFixture(TestName = "1261. Find Elements in a Contaminated Binary Tree")]
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        var solution = new FindElements(TreeNode.CreateFromArray(new int?[] {-1, null, -1}));
+        Assert.IsFalse(solution.Find(1));
+        Assert.IsTrue(solution.Find(2));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var solution = new FindElements(TreeNode.CreateFromArray(new int?[] {-1, -1, -1, -1, -1}));
+        Assert.IsTrue(solution.Find(1));
+        Assert.IsTrue(solution.Find(3));
+        Assert.IsFalse(solution.Find(5));
+    }
+
+    [Test]
+    public void Test3()
+    {
+        var solution = new FindElements(TreeNode.CreateFromArray(new int?[] {-1, null, -1, -1, null, -1}));
+        Assert.IsTrue(solution.Find(2));
+        Assert.IsFalse(solution.Find(3));
+        Assert.IsFalse(solution.Find(4));
+        Assert.IsTrue(solution.Find(5));
+    }
+}
