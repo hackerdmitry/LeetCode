12bd00086dbc608cd80e3615a732272338502e9a
hackerdmitry <hacker.dmitry@gmail.com>
2025-10-26 09:14:32 +0500

    2043. Simple Bank System

---
+++ LeetCode/2. Middle/2043. Simple Bank System/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode._2._Middle._2043._Simple_Bank_System;
+
+public class Bank
+{
+    private readonly long[] balance;
+
+    public Bank(long[] balance)
+    {
+        this.balance = balance;
+    }
+
+    public bool Transfer(int account1, int account2, long money)
+    {
+        var valid = IsValidAccount(account1) && IsValidAccount(account2) && balance[account1 - 1] >= money;
+        if (valid)
+        {
+            balance[account1 - 1] -= money;
+            balance[account2 - 1] += money;
+        }
+
+        return valid;
+    }
+
+    public bool Deposit(int account, long money)
+    {
+        var valid = IsValidAccount(account);
+        if (valid)
+            balance[account - 1] += money;
+        return valid;
+    }
+
+    public bool Withdraw(int account, long money)
+    {
+        var valid = IsValidAccount(account) && balance[account - 1] >= money;
+        if (valid)
+            balance[account - 1] -= money;
+        return valid;
+    }
+
+    private bool IsValidAccount(int account) =>
+        account > 0 && account <= balance.Length;
+}
---
+++ LeetCode/2. Middle/2043. Simple Bank System/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._2043._Simple_Bank_System;
+
+[TestFixture(TestName = "2043. Simple Bank System")]
+public class Tests
+{
+    [Timeout(1000)]
+    [Test]
+    public void Test1()
+    {
+        var bank = new Bank(new long[] {10, 100, 20, 50, 30});
+        Assert.True(bank.Withdraw(3, 10));
+        Assert.True(bank.Transfer(5, 1, 20));
+        Assert.True(bank.Deposit(5, 20));
+        Assert.False(bank.Transfer(3, 4, 15));
+        Assert.False(bank.Withdraw(10, 50));
+    }
+}
