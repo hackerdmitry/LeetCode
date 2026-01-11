be2f3632d096e9fce315b4667bd3e0a6e7804f34
hackerdmitry <hacker.dmitry@gmail.com>
2026-01-11 16:04:57 +0500

    933. Number of Recent Calls

---
+++ LeetCode/1. Easy/933. Number of Recent Calls/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._1._Easy._933._Number_of_Recent_Calls;
+
+public class RecentCounter
+{
+    private readonly Queue<int> queue = new();
+
+    public RecentCounter() { }
+
+    public int Ping(int t)
+    {
+        while (queue.Count > 0 && queue.Peek() + 3000 < t)
+            queue.Dequeue();
+        queue.Enqueue(t);
+        return queue.Count;
+    }
+}
---
+++ LeetCode/1. Easy/933. Number of Recent Calls/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._933._Number_of_Recent_Calls;
+
+[TestFixture(TestName = "933. Number of Recent Calls")]
+public class Tests
+{
+    [Timeout(1000)]
+    [Test]
+    public void Test()
+    {
+        var recentCounter = new RecentCounter();
+        Assert.AreEqual(recentCounter.Ping(1), 1);
+        Assert.AreEqual(recentCounter.Ping(100), 2);
+        Assert.AreEqual(recentCounter.Ping(3001), 3);
+        Assert.AreEqual(recentCounter.Ping(3002), 3);
+    }
+}
