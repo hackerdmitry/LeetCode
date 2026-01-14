6353ff3134f078ad45525ce5c049408b0f356f73
Dmitry Butov <d.butov@2gis.ru>
2026-01-14 09:14:47 +0500

    2336. Smallest Number in Infinite Set

---
+++ LeetCode/2. Middle/2336. Smallest Number in Infinite Set/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._2._Middle._2336._Smallest_Number_in_Infinite_Set;
+
+public class SmallestInfiniteSet
+{
+    private readonly HashSet<int> backedNumsBag = new();
+    private readonly PriorityQueue<int, int> backedNums = new();
+    private int smallestNum = 1;
+
+    public int PopSmallest()
+    {
+        return backedNums.Count > 0
+            ? DequeueBackedNum()
+            : smallestNum++;
+    }
+
+    public void AddBack(int num)
+    {
+        if (smallestNum > num && backedNumsBag.Add(num))
+            backedNums.Enqueue(num, num);
+    }
+
+    private int DequeueBackedNum()
+    {
+        var num = backedNums.Dequeue();
+        backedNumsBag.Remove(num);
+        return num;
+    }
+}
---
+++ LeetCode/2. Middle/2336. Smallest Number in Infinite Set/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._2336._Smallest_Number_in_Infinite_Set;
+
+[TestFixture(TestName = "2336. Smallest Number in Infinite Set")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(1, 2, 3)]
+    public void Test(int input1, int input2, int output)
+    {
+        var smallestInfiniteSet = new SmallestInfiniteSet();
+        smallestInfiniteSet.AddBack(2);
+        Assert.AreEqual(1, smallestInfiniteSet.PopSmallest());
+        Assert.AreEqual(2, smallestInfiniteSet.PopSmallest());
+        Assert.AreEqual(3, smallestInfiniteSet.PopSmallest());
+        smallestInfiniteSet.AddBack(1);
+        Assert.AreEqual(1, smallestInfiniteSet.PopSmallest());
+        Assert.AreEqual(4, smallestInfiniteSet.PopSmallest());
+        Assert.AreEqual(5, smallestInfiniteSet.PopSmallest());
+    }
+}
