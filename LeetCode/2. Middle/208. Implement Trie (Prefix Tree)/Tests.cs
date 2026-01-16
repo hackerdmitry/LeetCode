5931069b37b08e2a9a07a95e6bef21b73e3ea636
Dmitry Butov <d.butov@2gis.ru>
2026-01-16 13:35:51 +0500

    208. Implement Trie (Prefix Tree)

---
+++ LeetCode/2. Middle/208. Implement Trie (Prefix Tree)/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._208._Implement_Trie__Prefix_Tree_;
+
+[TestFixture(TestName = "208. Implement Trie (Prefix Tree)")]
+public class Tests
+{
+    [Timeout(1000)]
+    [Test]
+    public void Test()
+    {
+        var trie = new Trie();
+        trie.Insert("apple");
+        Assert.AreEqual(true, trie.Search("apple"));
+        Assert.AreEqual(false, trie.Search("app"));
+        Assert.AreEqual(true, trie.StartsWith("app"));
+        trie.Insert("app");
+        Assert.AreEqual(true, trie.Search("app"));
+    }
+}
---
+++ LeetCode/2. Middle/208. Implement Trie (Prefix Tree)/Trie.cs
@@ -0,0 +1,80 @@
+﻿namespace LeetCode._2._Middle._208._Implement_Trie__Prefix_Tree_;
+
+public class Trie
+{
+    private TrieNode root = new(' ');
+
+    public void Insert(string word)
+    {
+        var node = root;
+        foreach (var c in word)
+            node = node.AddChild(c);
+        node.SetEndOfWord();
+    }
+
+    public bool Search(string word)
+    {
+        var node = root;
+        foreach (var c in word)
+        {
+            node = node.GetChild(c);
+            if (node == null)
+                return false;
+        }
+
+        return node.IsEndOfWord;
+    }
+
+    public bool StartsWith(string prefix)
+    {
+        var node = root;
+        foreach (var c in prefix)
+        {
+            node = node.GetChild(c);
+            if (node == null)
+                return false;
+        }
+
+        return true;
+    }
+
+    private class TrieNode
+    {
+        private TrieNode[] children;
+
+        public char Letter { get; }
+        public bool IsEndOfWord { get; private set; }
+
+        public TrieNode(char letter)
+        {
+            Letter = letter;
+            IsEndOfWord = false;
+        }
+
+        public TrieNode GetChild(char letter)
+        {
+            return children?[letter - 'a'];
+        }
+
+        public TrieNode AddChild(char letter)
+        {
+            children ??= new TrieNode[26];
+
+            var index = letter - 'a';
+            var child = children[index];
+
+            if (child == null)
+            {
+                var childNode = new TrieNode(letter);
+                child = children[index] = childNode;
+            }
+
+            return child;
+        }
+
+        public void SetEndOfWord()
+        {
+            IsEndOfWord = true;
+        }
+    }
+}
