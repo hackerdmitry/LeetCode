d8b44aeabe03485d17f29dd073b18c5419c08d4c
Dmitry Butov <d.butov@2gis.ru>
2026-01-17 21:31:50 +0500

    3811. Number of Alternating XOR Partitions

---
+++ LeetCode/2. Middle/3811. Number of Alternating XOR Partitions/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._2._Middle._3811._Number_of_Alternating_XOR_Partitions;
+
+public class Solution
+{
+    private const int modulo = 1_000_000_007;
+
+    public int AlternatingXOR(int[] nums, int target1, int target2)
+    {
+        var n = nums.Length;
+        var px = new int[n];
+        px[0] = nums[0];
+
+        for (var i = 1; i < n; i++)
+            px[i] = px[i - 1] ^ nums[i];
+
+        var dp0 = new int[n];
+        var dp1 = new int[n];
+
+        var sum0 = new Dictionary<int, int>();
+        var sum1 = new Dictionary<int, int>();
+
+        for (var i = 0; i < n; i++)
+        {
+            var need0 = px[i] ^ target1;
+            var need1 = px[i] ^ target2;
+
+            dp0[i] = sum1.ContainsKey(need0) ? sum1[need0] : 0;
+            dp1[i] = sum0.ContainsKey(need1) ? sum0[need1] : 0;
+
+            if (px[i] == target1)
+                dp0[i] = (dp0[i] + 1) % modulo;
+
+            if (!sum0.ContainsKey(px[i])) sum0[px[i]] = 0;
+            if (!sum1.ContainsKey(px[i])) sum1[px[i]] = 0;
+
+            sum0[px[i]] = (sum0[px[i]] + dp0[i]) % modulo;
+            sum1[px[i]] = (sum1[px[i]] + dp1[i]) % modulo;
+        }
+
+        return (int) (((long) dp0[n - 1] + dp1[n - 1]) % modulo);
+    }
+}
---
+++ LeetCode/2. Middle/3811. Number of Alternating XOR Partitions/Tests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._3811._Number_of_Alternating_XOR_Partitions;
+
+[TestFixture(TestName = "3811. Number of Alternating XOR Partitions")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("[2,3,1,4]", 1, 5, 1)]
+    [TestCase("[1,0,0]", 1, 0, 3)]
+    [TestCase("[7]", 1, 7, 0)]
+    public void Test(string input1, int input2, int input3, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.AlternatingXOR(input1.ParseIntArray(), input2, input3);
+        Assert.AreEqual(output, actual);
+    }
+}
