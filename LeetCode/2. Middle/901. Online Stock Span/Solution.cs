02d1caa4a5dcba75f8782194e281e150e0e5aa18
Dmitry Butov <d.butov@2gis.ru>
2026-01-17 19:29:57 +0500

    901. Online Stock Span

---
+++ LeetCode/2. Middle/901. Online Stock Span/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._2._Middle._901._Online_Stock_Span;
+
+public class StockSpanner
+{
+    private readonly Stack<(int Price, int Streak)> stack = new();
+
+    public int Next(int price)
+    {
+        var streak = 1;
+        while (stack.Count > 0 && stack.Peek().Price <= price)
+            streak += stack.Pop().Streak;
+        stack.Push((price, streak));
+        return streak;
+    }
+}
---
+++ LeetCode/2. Middle/901. Online Stock Span/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._901._Online_Stock_Span;
+
+[TestFixture(TestName = "901. Online Stock Span")]
+public class Tests
+{
+    [Timeout(1000)]
+    [Test]
+    public void Test()
+    {
+        var stockSpanner = new StockSpanner();
+        Assert.AreEqual(1, stockSpanner.Next(100));
+        Assert.AreEqual(1, stockSpanner.Next(80));
+        Assert.AreEqual(1, stockSpanner.Next(60));
+        Assert.AreEqual(2, stockSpanner.Next(70));
+        Assert.AreEqual(1, stockSpanner.Next(60));
+        Assert.AreEqual(4, stockSpanner.Next(75));
+        Assert.AreEqual(6, stockSpanner.Next(85));
+    }
+}
