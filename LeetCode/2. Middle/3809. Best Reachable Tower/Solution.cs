4d11eaf2df153f8fd1f86212c3b97cf860ea7799
Dmitry Butov <d.butov@2gis.ru>
2026-01-17 21:31:22 +0500

    3809. Best Reachable Tower

---
+++ LeetCode/2. Middle/3809. Best Reachable Tower/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._3809._Best_Reachable_Tower;
+
+public class Solution
+{
+    public int[] BestTower(int[][] towers, int[] center, int radius)
+    {
+        var reachableTowers = GetReachableTowers(towers, center, radius);
+        return GetBestTower(reachableTowers);
+    }
+
+    private IEnumerable<int[]> GetReachableTowers(int[][] towers, int[] center, int radius)
+    {
+        return towers.Where(tower =>
+        {
+            var dx = Math.Abs(tower[0] - center[0]);
+            var dy = Math.Abs(tower[1] - center[1]);
+            return dx + dy <= radius;
+        });
+    }
+
+    private int[] GetBestTower(IEnumerable<int[]> reachableTowers)
+    {
+        var bestTower = reachableTowers
+            .OrderByDescending(tower => tower[2])
+            .ThenBy(tower => tower[0])
+            .ThenBy(tower => tower[1])
+            .FirstOrDefault();
+        return bestTower == null
+            ? new[] {-1, -1}
+            : new[] {bestTower[0], bestTower[1]};
+    }
+}
---
+++ LeetCode/2. Middle/3809. Best Reachable Tower/Tests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._3809._Best_Reachable_Tower;
+
+[TestFixture(TestName = "3809. Best Reachable Tower")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("[[1,2,5],[2,1,7],[3,1,9]]", "[1,1]", 2, "[3,1]")]
+    [TestCase("[[1,3,4],[2,2,4],[4,4,7]]", "[0,0]", 5, "[1,3]")]
+    [TestCase("[[5,6,8],[0,3,5]]", "[1,2]", 1, "[-1,-1]")]
+    [TestCase("[[3,2,92065],[5,2,50137]]", "[51252,81840]", 32957, "[-1,-1]")]
+    public void Test(string input1, string input2, int input3, string output)
+    {
+        var solution = new Solution();
+        var actual = solution.BestTower(input1.ParseIntArray2d(), input2.ParseIntArray(), input3);
+        var expected = output.ParseIntArray();
+        actual.WriteLine("actual");
+        expected.WriteLine("expected");
+        Assert.AreEqual(expected, actual);
+    }
+}
