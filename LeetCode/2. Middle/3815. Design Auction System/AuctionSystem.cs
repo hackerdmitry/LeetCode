36cfa2d6df61a5e50284320a3cc6526bcf11509f
Dmitry Butov <d.butov@2gis.ru>
2026-01-18 09:18:12 +0500

    3815. Design Auction System

---
+++ LeetCode/2. Middle/3815. Design Auction System/AuctionSystem.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._2._Middle._3815._Design_Auction_System;
+
+public class AuctionSystem
+{
+    private readonly Dictionary<int, ItemAuction> itemAuctions = new();
+
+    public void AddBid(int userId, int itemId, int bidAmount)
+    {
+        var itemAuction = itemAuctions.GetValueOrDefault(itemId);
+        if (itemAuction == null)
+        {
+            itemAuction = new ItemAuction();
+            itemAuctions[itemId] = itemAuction;
+        }
+
+        itemAuction.AddOrUpdateBid(userId, bidAmount);
+    }
+
+    public void UpdateBid(int userId, int itemId, int newAmount)
+    {
+        AddBid(userId, itemId, newAmount);
+    }
+
+    public void RemoveBid(int userId, int itemId)
+    {
+        itemAuctions[itemId].RemoveBid(userId);
+    }
+
+    public int GetHighestBidder(int itemId)
+    {
+        var itemAuction = itemAuctions.GetValueOrDefault(itemId);
+        return itemAuction?.GetHighestBidder() ?? -1;
+    }
+
+    private class ItemAuction
+    {
+        private readonly PriorityQueue<(int UserId, int Bid), long> bids = new();
+        private readonly Dictionary<int, int> users = new();
+
+        public void AddOrUpdateBid(int userId, int bidAmount)
+        {
+            users[userId] = bidAmount;
+            bids.Enqueue((userId, bidAmount), -100_000L * bidAmount - userId);
+        }
+
+        public void RemoveBid(int userId)
+        {
+            users.Remove(userId);
+        }
+
+        public int GetHighestBidder()
+        {
+            while (bids.Count > 0)
+            {
+                var (userId, bid) = bids.Peek();
+                if (users.TryGetValue(userId, out var currentBid) && currentBid == bid)
+                    return userId;
+
+                bids.Dequeue();
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode/2. Middle/3815. Design Auction System/Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._3815._Design_Auction_System;
+
+[TestFixture(TestName = "3815. Design Auction System")]
+public class Tests
+{
+    [Timeout(1000)]
+    [Test]
+    public void Test1()
+    {
+        var auctionSystem = new AuctionSystem();
+        auctionSystem.AddBid(1, 7, 5);
+        auctionSystem.AddBid(2, 7, 6);
+        Assert.AreEqual(2, auctionSystem.GetHighestBidder(7));
+        auctionSystem.UpdateBid(1, 7, 8);
+        Assert.AreEqual(1, auctionSystem.GetHighestBidder(7));
+        auctionSystem.RemoveBid(2, 7);
+        Assert.AreEqual(1, auctionSystem.GetHighestBidder(7));
+        Assert.AreEqual(-1, auctionSystem.GetHighestBidder(3));
+    }
+    [Test]
+    public void Test2()
+    {
+        var auctionSystem = new AuctionSystem();
+        auctionSystem.AddBid(10,1,1000);
+        auctionSystem.AddBid(20,1,1000);
+        Assert.AreEqual(20, auctionSystem.GetHighestBidder(1));
+    }
+}
