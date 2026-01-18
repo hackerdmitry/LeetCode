4b52b03aac1b75059b9f69b7cdca84bc204190e8
Dmitry Butov <d.butov@2gis.ru>
2026-01-18 09:23:12 +0500

    3813. Vowel-Consonant Score

---
+++ LeetCode/1. Easy/3813. Vowel-Consonant Score/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._1._Easy._3813._Vowel_Consonant_Score;
+
+public class Solution
+{
+    private static readonly HashSet<char> vowels = new() {'a', 'e', 'i', 'o', 'u'};
+
+    public int VowelConsonantScore(string s)
+    {
+        var v = s.Count(x => char.IsLetter(x) && vowels.Contains(x));
+        var c = s.Count(x => char.IsLetter(x) && !vowels.Contains(x));
+        return c > 0 ? v / c : 0;
+    }
+}
---
+++ LeetCode/1. Easy/3813. Vowel-Consonant Score/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._3813._Vowel_Consonant_Score;
+
+[TestFixture(TestName = "3813. Vowel-Consonant Score")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("cooear", 2)]
+    [TestCase("axeyizou", 1)]
+    [TestCase("au 123", 0)]
+    [TestCase("i3", 0)]
+    public void Test(string input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.VowelConsonantScore(input);
+        Assert.AreEqual(output, actual);
+    }
+}
