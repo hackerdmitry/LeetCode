6ca5da9d64d09556bda1ad6675fd8e317ced5f6e
Dmitry Butov <d.butov@2gis.ru>
2026-01-19 12:39:53 +0500

    3814. Maximum Capacity Within Budget

---
+++ LeetCode/2. Middle/3814. Maximum Capacity Within Budget/Solution.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2._Middle._3814._Maximum_Capacity_Within_Budget;
+
+public class Solution
+{
+    public int MaxCapacity(int[] costs, int[] capacities, int budget)
+    {
+        budget--;
+
+        var maxCapacity = 0;
+
+        var n = costs.Length;
+        var bestCapacities = new IncreasingSortedDictionary();
+        foreach (var (cost, capacity) in Enumerable.Range(0, n).Select(i => (Cost: costs[i], Capacity: capacities[i])).OrderBy(x => x.Cost).ThenByDescending(x => x.Capacity))
+            if (cost <= budget)
+            {
+                var bestCapacity = capacity + (bestCapacities.FindPrevOrEqual(budget - cost)?.Value ?? 0);
+                maxCapacity = Math.Max(maxCapacity, bestCapacity);
+                bestCapacities.Add(cost, capacity);
+            }
+
+        return maxCapacity;
+    }
+
+    private class IncreasingSortedDictionary
+    {
+        private List<int> keys = new();
+        private Dictionary<int, int> dictionary = new();
+
+        public void Add(int key, int value)
+        {
+            if (keys.Count > 0 && dictionary[keys[^1]] >= value)
+                return;
+
+            dictionary[key] = value;
+            if (keys.Count == 0 || keys[^1] != key)
+                keys.Add(key);
+        }
+
+        public (int Key, int Value)? FindPrevOrEqual(int key)
+        {
+            if (keys.Count == 0 || keys[0] > key)
+                return null;
+
+            var index = BinarySearchMax(0, keys.Count - 1, mid => keys[mid] <= key);
+            return (keys[index], dictionary[keys[index]]);
+        }
+
+        private int BinarySearchMax(int left, int right, Func<int, bool> canBeLeft)
+        {
+            while (left < right)
+            {
+                var mid = (left + right) / 2;
+                if (left == mid)
+                    mid++;
+                var isLeft = canBeLeft(mid);
+                if (isLeft)
+                    left = mid;
+                else if (right == mid)
+                    right--;
+                else
+                    right = mid;
+            }
+
+            return left;
+        }
+    }
+}
---
+++ LeetCode/2. Middle/3814. Maximum Capacity Within Budget/Tests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._3814._Maximum_Capacity_Within_Budget;
+
+[TestFixture(TestName = "3814. Maximum Capacity Within Budget")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("[1,2,4]", "[1,8,5]", 11, 13)]
+    [TestCase("[8,7,1]", "[7,4,2]", 11, 9)]
+    [TestCase("[3,5,7,4]", "[2,4,3,6]", 7, 6)]
+    [TestCase("[4,8,5,3]", "[1,5,2,7]", 8, 8)]
+    [TestCase("[1,7,3]", "[7,3,5]", 13, 12)]
+    [TestCase("[1,12,2,4,1,11,8]", "[2,11,3,7,7,12,3]", 14, 19)]
+    [TestCase("[2,2,2]", "[3,5,4]", 5, 9)]
+    [TestCase("[4,6]", "[5,3]", 3, 0)]
+    public void Test(string input1, string input2, int input3, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MaxCapacity(input1.ParseIntArray(), input2.ParseIntArray(), input3);
+        Assert.AreEqual(output, actual);
+    }
+}
