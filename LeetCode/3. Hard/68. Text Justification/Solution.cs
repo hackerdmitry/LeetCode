044ec5db29408847d96146fca9c02677e218ef49
Dmitry Butov <d.butov@2gis.ru>
2026-01-20 21:47:51 +0500

    68. Text Justification

---
+++ LeetCode/3. Hard/68. Text Justification/Solution.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode._3._Hard._68._Text_Justification;
+
+public class Solution
+{
+    public IList<string> FullJustify(string[] words, int maxWidth)
+    {
+        return CreateJustifiedLines(words, maxWidth).ToArray();
+    }
+
+    private IEnumerable<string> CreateJustifiedLines(string[] words, int maxWidth)
+    {
+        var lineWords = new List<string>();
+        var wordsLength = 0;
+        foreach (var word in words)
+        {
+            if (wordsLength + lineWords.Count + word.Length > maxWidth)
+            {
+                yield return JustifyLine(lineWords, maxWidth, wordsLength);
+                lineWords.Clear();
+                wordsLength = 0;
+            }
+
+            wordsLength += word.Length;
+            lineWords.Add(word);
+        }
+
+        yield return JustifyLine(lineWords, maxWidth, wordsLength, true);
+    }
+
+    private string JustifyLine(IList<string> lineWords, int maxWidth, int wordsLength, bool isLeftJustified = false)
+    {
+        var stringBuilder = new StringBuilder();
+        var totalSpaces = maxWidth - wordsLength;
+        if (lineWords.Count > 1)
+        {
+            var gap = isLeftJustified ? " " : new string(' ', totalSpaces / (lineWords.Count - 1));
+            var wordsWithExtraGaps = isLeftJustified ? 0 : totalSpaces % (lineWords.Count - 1);
+            foreach (var word in lineWords)
+            {
+                stringBuilder.Append(word);
+                if (stringBuilder.Length < maxWidth)
+                {
+                    stringBuilder.Append(gap);
+                    if (wordsWithExtraGaps > 0)
+                    {
+                        stringBuilder.Append(' ');
+                        wordsWithExtraGaps--;
+                    }
+                }
+            }
+        }
+        else
+            stringBuilder.Append(lineWords[0]);
+
+        if (stringBuilder.Length < maxWidth)
+            stringBuilder.Append(' ', maxWidth - stringBuilder.Length);
+
+        return stringBuilder.ToString();
+    }
+}
---
+++ LeetCode/3. Hard/68. Text Justification/Tests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._3._Hard._68._Text_Justification;
+
+[TestFixture(TestName = "68. Text Justification")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("[\"This\", \"is\", \"an\", \"example\", \"of\", \"text\", \"justification.\"]", 16, "[\"This    is    an\",\"example  of text\",\"justification.  \"]")]
+    [TestCase("[\"What\",\"must\",\"be\",\"acknowledgment\",\"shall\",\"be\"]", 16, "[\"What   must   be\",\"acknowledgment  \",\"shall be        \"]")]
+    [TestCase("[\"Science\",\"is\",\"what\",\"we\",\"understand\",\"well\",\"enough\",\"to\",\"explain\",\"to\",\"a\",\"computer.\",\"Art\",\"is\",\"everything\",\"else\",\"we\",\"do\"]", 20, "[\"Science  is  what we\",\"understand      well\",\"enough to explain to\",\"a  computer.  Art is\",\"everything  else  we\",\"do                  \"]")]
+    public void Test(string input1, int input2, string output)
+    {
+        var solution = new Solution();
+        var actual = solution.FullJustify(input1.ParseStringArray(), input2);
+        var expected = output.ParseStringArray();
+        expected.WriteLine("expected");
+        actual.WriteLine("actual");
+        Assert.AreEqual(expected, actual);
+    }
+}
