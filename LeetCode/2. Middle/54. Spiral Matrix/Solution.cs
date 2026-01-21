19ff7433a8ad5c7df33d50e6d7f94a7f84cd4870
Dmitry Butov <d.butov@2gis.ru>
2026-01-21 13:58:37 +0500

    54. Spiral Matrix

---
+++ LeetCode/2. Middle/54. Spiral Matrix/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2._Middle._54._Spiral_Matrix;
+
+public class Solution
+{
+    private readonly int[] directions = {0, 1, 0, -1, 0};
+
+    public IList<int> SpiralOrder(int[][] matrix)
+    {
+        var n = matrix.Length;
+        var m = matrix[0].Length;
+        return SpiralEnumerable(matrix, m, n).ToArray();
+    }
+
+    private IEnumerable<int> SpiralEnumerable(int[][] matrix, int m, int n)
+    {
+        var countDirection = new[] {m - 1, n - 1, m - 1, n - 2};
+        var isFirst = true;
+        var first = countDirection[0] > 0 ? 0 : 1;
+
+        var y = 0;
+        var x = 0;
+
+        yield return matrix[0][0];
+        while (true)
+        {
+            for (var i = isFirst ? first : 0; i < countDirection.Length; i++)
+            {
+                if (countDirection[i] <= 0)
+                    yield break;
+                for (var j = 0; j < countDirection[i]; j++)
+                    yield return Step(matrix, ref y, ref x, i);
+                if (i == 0 && countDirection[0] == countDirection[2])
+                    countDirection[i]--;
+                else
+                    countDirection[i] -= 2;
+            }
+
+            isFirst = false;
+        }
+    }
+
+    private int Step(int[][] matrix, ref int y, ref int x, int direction)
+    {
+        y += directions[direction];
+        x += directions[direction + 1];
+        return matrix[y][x];
+    }
+}
---
+++ LeetCode/2. Middle/54. Spiral Matrix/Tests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._54._Spiral_Matrix;
+
+[TestFixture(TestName = "54. Spiral Matrix")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("[[1,2,3,4],[5,6,7,8],[9,10,11,12]]", "[1,2,3,4,8,12,11,10,9,5,6,7]")]
+    [TestCase("[[1,2,3],[4,5,6],[7,8,9],[10,11,12]]", "[1,2,3,6,9,12,11,10,7,4,5,8]")]
+    [TestCase("[[3],[2]]", "[3,2]")]
+    [TestCase("[[1,2,3],[4,5,6],[7,8,9]]", "[1,2,3,6,9,8,7,4,5]")]
+    public void Test(string input, string output)
+    {
+        var solution = new Solution();
+        var actual = solution.SpiralOrder(input.ParseIntArray2d());
+        var expected = output.ParseIntArray();
+        expected.WriteLine("expected");
+        actual.WriteLine("actual");
+        Assert.AreEqual(expected, actual);
+    }
+}
