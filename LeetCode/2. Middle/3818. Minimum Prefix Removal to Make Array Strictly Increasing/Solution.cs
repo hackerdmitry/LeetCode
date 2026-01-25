69b2b15c16c438322783627d30164752e2497225
Dmitry Butov <d.butov@2gis.ru>
2026-01-25 10:19:28 +0500

    3818. Minimum Prefix Removal to Make Array Strictly Increasing

---
+++ LeetCode/2. Middle/3818. Minimum Prefix Removal to Make Array Strictly Increasing/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode._2._Middle._3818._Minimum_Prefix_Removal_to_Make_Array_Strictly_Increasing;
+
+public class Solution
+{
+    public int MinimumPrefixLength(int[] nums)
+    {
+        var result = 1;
+        for (var i = nums.Length - 2; i >= 0; i--)
+            if (nums[i + 1] <= nums[i])
+                break;
+            else
+                result++;
+        return nums.Length - result;
+    }
+}
---
+++ LeetCode/2. Middle/3818. Minimum Prefix Removal to Make Array Strictly Increasing/Tests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._3818._Minimum_Prefix_Removal_to_Make_Array_Strictly_Increasing;
+
+[TestFixture(TestName = "3818. Minimum Prefix Removal to Make Array Strictly Increasing")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("[1,-1,2,3,3,4,5]", 4)]
+    [TestCase("[4,3,-2,-5]", 3)]
+    [TestCase("[1,2,3,4]", 0)]
+    public void Test(string input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.MinimumPrefixLength(input.ParseIntArray());
+        Assert.AreEqual(output, actual);
+    }
+}
