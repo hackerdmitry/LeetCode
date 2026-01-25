15ceefd8ef82d40bf379e84759b3a8c086f45935
Dmitry Butov <d.butov@2gis.ru>
2026-01-25 10:18:56 +0500

    3819. Rotate Non Negative Elements

---
+++ LeetCode/2. Middle/3819. Rotate Non Negative Elements/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2._Middle._3819._Rotate_Non_Negative_Elements;
+
+public class Solution
+{
+    public int[] RotateElements(int[] nums, int k)
+    {
+        var countPositive = nums.Count(x => x >= 0);
+        if (countPositive == 0)
+            return nums;
+
+        k %= countPositive;
+
+        var firstPositiveIndex = -1;
+
+        var queue = new Queue<int>();
+        for (var i = nums.Length - 1; i >= 0; i--)
+            if (nums[i] >= 0)
+            {
+                if (firstPositiveIndex == -1)
+                    firstPositiveIndex = i;
+                queue.Enqueue(nums[i]);
+                if (queue.Count > k)
+                    nums[i] = queue.Dequeue();
+            }
+        for (var i = nums.Length - 1; i >= 0 && queue.Count > 0; i--)
+            if (nums[i] >= 0)
+                nums[i] = queue.Dequeue();
+
+        return nums;
+    }
+}
---
+++ LeetCode/2. Middle/3819. Rotate Non Negative Elements/Tests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._3819._Rotate_Non_Negative_Elements;
+
+[TestFixture(TestName = "3819. Rotate Non Negative Elements")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("[1,-2,3,-4]", 3, "[3,-2,1,-4]")]
+    [TestCase("[-3,-2,7]", 1, "[-3,-2,7]")]
+    [TestCase("[5,4,-9,6]", 2, "[6,5,-9,4]")]
+    [TestCase("[-6,-2]", 18866, "[-6,-2]")]
+    public void Test(string input1, int input2, string output)
+    {
+        var solution = new Solution();
+        var actual = solution.RotateElements(input1.ParseIntArray(), input2);
+        var expected = output.ParseIntArray();
+        expected.WriteLine("expected");
+        actual.WriteLine("actual");
+        Assert.AreEqual(expected, actual);
+    }
+}
