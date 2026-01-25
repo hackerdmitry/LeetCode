4e4ad12cbc69e78fcc016bca550e4fe25016871a
Dmitry Butov <d.butov@2gis.ru>
2026-01-25 10:18:19 +0500

    3821. Find Nth Smallest Integer With K One Bits

---
+++ LeetCode/3. Hard/3821. Find Nth Smallest Integer With K One Bits/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LeetCode._3._Hard._3821._Find_Nth_Smallest_Integer_With_K_One_Bits;
+
+public class Solution
+{
+    public long NthSmallest(long n, int k)
+    {
+        var result = 0L;
+        for (var i = 50; i >= 0 && k > 0; i--)
+        {
+            var count = Combinations(i, k);
+            if (n > count)
+            {
+                n -= count;
+                result |= 1L << i;
+                k--;
+            }
+        }
+
+        return result;
+    }
+
+    private static long Combinations(int positions, int ones)
+    {
+        if (ones > positions || ones < 0)
+            return 0;
+
+        if (positions == ones || ones == 0)
+            return 1;
+
+        var k = Math.Min(ones, positions - ones);
+        var result = 1L;
+
+        for (var i = 1; i <= k; i++)
+            result = result * (positions - k + i) / i;
+
+        return result;
+    }
+}
---
+++ LeetCode/3. Hard/3821. Find Nth Smallest Integer With K One Bits/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._3._Hard._3821._Find_Nth_Smallest_Integer_With_K_One_Bits;
+
+[TestFixture(TestName = "3821. Find Nth Smallest Integer With K One Bits")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(9, 2, 20)]
+    [TestCase(4, 2, 9)]
+    [TestCase(3, 1, 4)]
+    public void Test(long input1, int input2, long output)
+    {
+        var solution = new Solution();
+        var actual = solution.NthSmallest(input1, input2);
+        Assert.AreEqual(output, actual);
+    }
+}
