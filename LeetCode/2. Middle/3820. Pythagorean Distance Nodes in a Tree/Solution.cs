4902f42eaf84ac5a4370911c038705c4f56336cf
Dmitry Butov <d.butov@2gis.ru>
2026-01-25 10:18:02 +0500

    3820. Pythagorean Distance Nodes in a Tree

---
+++ LeetCode/2. Middle/3820. Pythagorean Distance Nodes in a Tree/Solution.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode._2._Middle._3820._Pythagorean_Distance_Nodes_in_a_Tree;
+
+public class Solution
+{
+    public int SpecialNodes(int n, int[][] edges, int x, int y, int z)
+    {
+        var tree = CreateTree(n, edges);
+        CalculateDeltas(tree[x], (prev, node) => node.DX = prev.DX + 1);
+        CalculateDeltas(tree[y], (prev, node) => node.DY = prev.DY + 1);
+        CalculateDeltas(tree[z], (prev, node) => node.DZ = prev.DZ + 1);
+        return tree.Count(node => node.IsSpecial());
+    }
+
+    private Node[] CreateTree(int n, int[][] edges)
+    {
+        var tree = new Node[n];
+        for (var i = 0; i < n; i++)
+            tree[i] = new Node(i);
+
+        foreach (var edge in edges)
+        {
+            var u = edge[0];
+            var v = edge[1];
+            tree[u].AddNeighbour(tree[v]);
+            tree[v].AddNeighbour(tree[u]);
+        }
+
+        return tree;
+    }
+
+    private void CalculateDeltas(Node node, Action<Node, Node> incrementDelta)
+    {
+        var queue = new Queue<(Node prev, Node curr)>();
+        queue.Enqueue((node, node));
+
+        while (queue.Count > 0)
+        {
+            var (prev, curr) = queue.Dequeue();
+            foreach (var neighbour in curr.Neighbours)
+                if (neighbour != prev)
+                {
+                    incrementDelta(curr, neighbour);
+                    queue.Enqueue((curr, neighbour));
+                }
+        }
+    }
+
+    private class Node
+    {
+        private int Index { get; }
+
+        public int DX { get; set; }
+        public int DY { get; set; }
+        public int DZ { get; set; }
+
+        public LinkedList<Node> Neighbours { get; set; } = new();
+
+        public Node(int index)
+        {
+            Index = index;
+        }
+
+        public void AddNeighbour(Node child)
+        {
+            Neighbours.AddLast(child);
+        }
+
+        public bool IsSpecial()
+        {
+            if (DX >= DY && DX >= DZ)
+                return DX * DX == DY * DY + DZ * DZ;
+
+            if (DY >= DX && DY >= DZ)
+                return DY * DY == DX * DX + DZ * DZ;
+
+            return DZ * DZ == DX * DX + DY * DY;
+        }
+
+        public override string ToString()
+        {
+            return Index.ToString();
+        }
+    }
+}
---
+++ LeetCode/2. Middle/3820. Pythagorean Distance Nodes in a Tree/Tests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._3820._Pythagorean_Distance_Nodes_in_a_Tree;
+
+[TestFixture(TestName = "3820. Pythagorean Distance Nodes in a Tree")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(4, "[[0,1],[1,2],[2,3]]", 0, 3, 2, 0)]
+    [TestCase(4, "[[0,1],[0,2],[0,3]]", 1, 2, 3, 3)]
+    [TestCase(4, "[[0,1],[1,2],[1,3]]", 1, 3, 0, 1)]
+    public void Test(int input1, string input2, int input3, int input4, int input5, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.SpecialNodes(input1, input2.ParseIntArray2d(), input3, input4, input5);
+        Assert.AreEqual(output, actual);
+    }
+}
