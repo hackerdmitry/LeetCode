273e8aba8cc338a713b2075b3526a9bfc003328b
Dmitry Butov <d.butov@2gis.ru>
2026-01-26 21:53:55 +0500

    155. Min Stack

---
+++ LeetCode/2. Middle/155. Min Stack/MinStack.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode._2._Middle._155._Min_Stack;
+
+public class MinStack
+{
+    private Node top;
+    private Node minTop;
+
+    public void Push(int val)
+    {
+        top = new Node(val, top);
+
+        if (minTop == null || val <= minTop.Value)
+            minTop = new Node(val, minTop);
+    }
+
+    public void Pop()
+    {
+        if (minTop != null && top.Value <= minTop.Value)
+            minTop = minTop.Prev;
+        top = top?.Prev;
+    }
+
+    public int Top()
+    {
+        return top.Value;
+    }
+
+    public int GetMin()
+    {
+        return minTop.Value;
+    }
+
+    private class Node
+    {
+        public Node Prev { get; }
+        public int Value { get; }
+
+        public Node(int value, Node prev)
+        {
+            Value = value;
+            Prev = prev;
+        }
+    }
+}
---
+++ LeetCode/2. Middle/155. Min Stack/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._2._Middle._155._Min_Stack;
+
+[TestFixture(TestName = "155. Min Stack")]
+public class Tests
+{
+    [Timeout(1000)]
+    [Test]
+    public void Test()
+    {
+        var minStack = new MinStack();
+        minStack.Push(-2);
+        minStack.Push(0);
+        minStack.Push(-3);
+        Assert.AreEqual(-3, minStack.GetMin());
+        minStack.Pop();
+        Assert.AreEqual(0, minStack.Top());
+        Assert.AreEqual(-2, minStack.GetMin());
+    }
+}
