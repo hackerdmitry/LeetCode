7891ee1310a19e740f4f750b70c775bd89021086
Dmitry Butov <d.butov@2gis.ru>
2026-02-02 08:47:47 +0500

    3828. Final Element After Subarray Deletions

---
+++ LeetCode/2. Middle/3828. Final Element After Subarray Deletions/Solution.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LeetCode._2._Middle._3828._Final_Element_After_Subarray_Deletions;
+
+public class Solution
+{
+    public int FinalElement(int[] nums)
+    {
+        return Math.Max(nums[0], nums[^1]);
+    }
+}
---
+++ LeetCode/2. Middle/3828. Final Element After Subarray Deletions/Tests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._3828._Final_Element_After_Subarray_Deletions;
+
+[TestFixture(TestName = "3828. Final Element After Subarray Deletions")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase("[1,5,2]", 2)]
+    [TestCase("[3,7]", 7)]
+    [TestCase("[10,6,10,7]", 10)]
+    [TestCase("[7,10,6,10]", 10)]
+    [TestCase("[6,10,6,7]", 7)]
+    [TestCase("[7,6,10,6]", 7)]
+    [TestCase("[6,10,7,10,6]", 6)]
+    public void Test(string input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.FinalElement(input.ParseIntArray());
+        Assert.AreEqual(output, actual);
+    }
+}
