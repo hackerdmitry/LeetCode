6611d1248832d14a8d5b3ef79ea729f0b1394dd4
Dmitry Butov <d.butov@2gis.ru>
2026-02-02 08:48:33 +0500

    3829. Design Ride Sharing System

---
+++ LeetCode/2. Middle/3829. Design Ride Sharing System/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode._2._Middle._3829._Design_Ride_Sharing_System;
+
+public class RideSharingSystem
+{
+    private readonly int[] unavailableMatch = {-1, -1};
+
+    private readonly Dictionary<int, int> cancelledRiders = new();
+    private readonly Dictionary<int, int> ridersDict = new();
+
+    private readonly Queue<int> riders = new();
+    private readonly Queue<int> drivers = new();
+
+    public void AddRider(int riderId)
+    {
+        if (!ridersDict.TryAdd(riderId, 1))
+            ridersDict[riderId]++;
+        riders.Enqueue(riderId);
+    }
+
+    public void AddDriver(int driverId)
+    {
+        drivers.Enqueue(driverId);
+    }
+
+    public int[] MatchDriverWithRider()
+    {
+        while (riders.Count > 0 && cancelledRiders.TryGetValue(riders.Peek(), out var cancelledRider) && cancelledRider > 0)
+            riders.Dequeue();
+
+        if (riders.Count > 0 && drivers.Count > 0)
+        {
+            var riderId = riders.Dequeue();
+            var driverId = drivers.Dequeue();
+            ridersDict[riderId]--;
+            return new[] {driverId, riderId};
+        }
+
+        return unavailableMatch;
+    }
+
+    public void CancelRider(int riderId)
+    {
+        if (ridersDict.TryGetValue(riderId, out var ridersCount))
+            cancelledRiders[riderId] = ridersCount;
+    }
+}
---
+++ LeetCode/2. Middle/3829. Design Ride Sharing System/Tests.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Extensions;
+using NUnit.Framework;
+
+namespace LeetCode._2._Middle._3829._Design_Ride_Sharing_System;
+
+[TestFixture(TestName = "3829. Design Ride Sharing System")]
+public class Tests
+{
+    [Timeout(1000)]
+    [Test]
+    public void Test1()
+    {
+        var rideSharingSystem = new RideSharingSystem();
+        rideSharingSystem.AddRider(3);
+        rideSharingSystem.AddDriver(2);
+        rideSharingSystem.AddRider(1);
+        Assert.AreEqual("[2, 3]".ParseIntArray(), rideSharingSystem.MatchDriverWithRider());
+        rideSharingSystem.AddDriver(5);
+        rideSharingSystem.CancelRider(3);
+        Assert.AreEqual("[5, 1]".ParseIntArray(), rideSharingSystem.MatchDriverWithRider());
+        Assert.AreEqual("[-1, -1]".ParseIntArray(), rideSharingSystem.MatchDriverWithRider());
+    }
+
+    [Timeout(1000)]
+    [Test]
+    public void Test2()
+    {
+        var rideSharingSystem = new RideSharingSystem();
+        rideSharingSystem.AddRider(8);
+        rideSharingSystem.AddDriver(8);
+        rideSharingSystem.AddDriver(6);
+        Assert.AreEqual("[8, 8]".ParseIntArray(), rideSharingSystem.MatchDriverWithRider());
+        rideSharingSystem.AddRider(2);
+        rideSharingSystem.CancelRider(2);
+        Assert.AreEqual("[-1, -1]".ParseIntArray(), rideSharingSystem.MatchDriverWithRider());
+    }
+}
