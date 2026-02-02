a04d43e8f0f8ec2bfa2f4c4261f8591e6aacd4dc
Dmitry Butov <d.butov@2gis.ru>
2026-02-02 08:47:07 +0500

    3827. Count Monobit Integers

---
+++ LeetCode/1. Easy/3827. Count Monobit Integers/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode._1._Easy._3827._Count_Monobit_Integers;
+
+public class Solution
+{
+    public int CountMonobit(int n)
+    {
+        var a = 0;
+        var result = 0;
+        while (a <= n)
+        {
+            result++;
+            a = (a << 1) | 1;
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/1. Easy/3827. Count Monobit Integers/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._1._Easy._3827._Count_Monobit_Integers;
+
+[TestFixture(TestName = "3827. Count Monobit Integers")]
+public class Tests
+{
+    [Timeout(1000)]
+    [TestCase(1, 2)]
+    [TestCase(4, 3)]
+    public void Test(int input, int output)
+    {
+        var solution = new Solution();
+        var actual = solution.CountMonobit(input);
+        Assert.AreEqual(output, actual);
+    }
+}
